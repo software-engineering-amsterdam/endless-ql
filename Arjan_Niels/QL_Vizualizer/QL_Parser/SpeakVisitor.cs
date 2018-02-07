a1f3fd162e825fe66f37d1c6e1362ef6f31da9ca
Niels Boerkamp <n.boerkamp@outlook.com>
2018-02-07 16:36:33 +0100

    Added Antlr4 to the project.

---
+++ Arjan_Niels/QL_Vizualizer/QL_Parser/SpeakVisitor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL_Parser
+{
+    public class SpeakVisitor : SpeakBaseVisitor<object>
+    {
+        public List<SpeakLine> Lines = new List<SpeakLine>();
+
+        public override object VisitLine(SpeakParser.LineContext context)
+        {
+            NameContext name = context.name();
+            OpinionContext opinion = context.opinion();
+
+            SpeakLine line = new SpeakLine() { Person = name.GetText(), Text = opinion.GetText().Trim('"') };
+            Lines.Add(line);
+
+            return line;
+        }
+    }
+}

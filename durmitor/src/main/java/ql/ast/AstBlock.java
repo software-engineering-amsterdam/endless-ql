829d9180f8781ba813489088ef772e2f2cb4b459
Stevan Pavlicic <stevan.pavlicic@tno.nl>
2018-02-07 20:19:47 +0100

    fixed issue in grammar

--- durmitor/src/main/java/ql/ast/AstBlock.java
+++ durmitor/src/main/java/ql/ast/AstBlock.java
@@ -4,26 +4,26 @@ import java.util.ArrayList;
 
 public class AstBlock extends AstNode {
 
-	private ArrayList<Object> store = new ArrayList<Object>(0);
-	
-	AstBlock(Ast ast) {
-		super(ast);
-	}
+    private ArrayList<Object> store = new ArrayList<Object>(0);
 
-	@Override
-	int getNodeType0() {
-		return AstNode.BLOCK;
-	}
-	
-	public void addChild(AstNode node) {
-		this.store.add(node);
-	}
-	
-	public int size() {
-		return this.store.size();
-	}
-	
-	public AstNode get(int index) {
-		return (AstNode) this.store.get(index);
-	}
+    AstBlock(Ast ast) {
+        super(ast);
+    }
+
+    @Override
+    int getNodeType0() {
+        return AstNode.BLOCK;
+    }
+
+    public void addChild(AstNode node) {
+        this.store.add(node);
+    }
+
+    public int size() {
+        return this.store.size();
+    }
+
+    public AstNode get(int index) {
+        return (AstNode) this.store.get(index);
+    }
 }

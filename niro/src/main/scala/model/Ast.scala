b06ef259c68ac9fb1af42784531c4718778cdf6e
rmathijn <rocco.mathijnandela@dhl.com>
2018-02-08 14:45:19 +0100

    Extend AST with more useful AnswerType

---
+++ niro/src/main/scala/model/Ast.scala
@@ -0,0 +1,25 @@
+package model
+
+object Ast {
+
+  case class QLForm(formName: String, statements: Seq[Statement])
+
+  sealed trait Statement
+  case class Question(id: String, label: String, answerType: AnswerType) extends Statement
+
+  sealed trait AnswerType
+  object AnswerType {
+    case object StringAnswerType extends AnswerType
+    case object IntAnswerType extends AnswerType
+    case object BooleanAnswerType extends AnswerType
+
+    def apply(answerTypeText: String): AnswerType = {
+      answerTypeText match {
+        case "boolean" => BooleanAnswerType
+        case "text" => StringAnswerType
+        case "integer" => IntAnswerType
+        case other => throw new IllegalArgumentException(s"Unsupported type: $other")
+      }
+    }
+  }
+}

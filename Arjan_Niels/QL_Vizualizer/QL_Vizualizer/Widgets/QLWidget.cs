1fcf095428100fb040cab4851f0acb21d488dac7
GrimGerbil <meijerarjan@live.nl>
2018-02-08 11:15:17 +0100

    Base QLWidget
    Implemented the basis of the QLWidget class

---
+++ Arjan_Niels/QL_Vizualizer/QL_Vizualizer/Widgets/QLWidget.cs
@@ -0,0 +1,32 @@
+﻿namespace QL_Vizualizer.Widgets
+{
+    public abstract class QLWidget<T>
+    {
+        /// <summary>
+        /// Contains given answer, if not answered contains default value for T
+        /// </summary>
+        public T AnswerValue { get; private set; }
+
+        /// <summary>
+        /// Indication if user gave an answer for this QLWidget
+        /// </summary>
+        public bool IsAnswered { get; private set; }
+
+        public QLWidget(string identifyer, string question)
+        {
+            AnswerValue = default(T);
+            IsAnswered = false;
+        }
+
+        /// <summary>
+        /// Set the value of the AnswerValue
+        /// </summary>
+        /// <param name="answer"></param>
+        public void SetAnswer(T answer)
+        {
+            AnswerValue = answer;
+            IsAnswered = true;
+        }
+
+    }
+}

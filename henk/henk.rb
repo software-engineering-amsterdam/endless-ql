cc7a1305cd9aa9951b38a5dc4ecabcba661ca747
Jovan Maric <jovanmaric777@gmail.com>
2018-02-08 16:23:01 +0100

    feat(rltk): add simple tester

--- henk/henk.rb
+++ henk/henk.rb
@@ -1,5 +1,12 @@
 require_relative 'lib/hello_world'
+require_relative 'lib/parser'
+require_relative 'lib/ast'
+require_relative 'lib/lexer'
+
 require 'pry'
 
 # Project base class for autoloading
 module Henk; end
+
+a = Henk::Parser.parse(Henk::Lexer.lex('1+1;'))
+binding.pry

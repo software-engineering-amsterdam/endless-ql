e39acf7b3b518ee3a79e999cf7bd37895e6864e6
rmathijn <rocco.mathijnandela@dhl.com>
2018-02-08 14:55:47 +0100

    Build Ast in ListeningCompiler

--- niro/src/main/java/nl/uva/se/sc/niro/ListeningCompiler.java
+++ niro/src/main/java/nl/uva/se/sc/niro/ListeningCompiler.java
@@ -1,6 +1,14 @@
 package nl.uva.se.sc.niro;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Ast.AnswerType;
+import model.Ast.AnswerType$;
+import model.Ast.QLForm;
+import model.Ast.Question;
+import model.Ast.Statement;
 
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -11,12 +19,16 @@ import ql.QLLexer;
 import ql.QLParser;
 import ql.QLParser.ExpressionContext;
 import ql.QLParser.FormContext;
+import scala.collection.JavaConverters;
+import scala.collection.Seq;
 
 /**
  * Just for testing the ANTLR integration. Acts mainly for Proof Of Concept. 
  * @author Nico Tromp
  */
 public class ListeningCompiler extends QLBaseListener {
+
+	private QLForm qlForm;
 
 	public Object compileScriptFile(CharStream source) throws IOException {
 		QLLexer lexer = new QLLexer(source);
@@ -46,5 +58,29 @@ public class ListeningCompiler extends QLBaseListener {
 			break;
 		}
 	}
-		
+
+	@Override
+	public void enterForm(FormContext ctx) {
+		List<Statement> jList = new ArrayList<>();
+		Seq<Statement> sList = JavaConverters.asScalaBufferConverter(jList).asScala().toSeq();
+
+		qlForm = new QLForm(ctx.name().getText(), sList);
+		System.out.printf("%nform %s {%n", ctx.name().getText());
+	}
+
+	@Override
+	public void enterQuestion(QLParser.QuestionContext ctx) {
+		String questionId = ctx.name().getText();
+		String questionLabel = ctx.children.get(2).getText();
+		String answerTypeText = ctx.answer_type().getText();
+		AnswerType answerType = AnswerType$.MODULE$.apply(answerTypeText);
+
+		Question question = new Question(questionId, questionLabel, answerType);
+		List<Statement> questions = JavaConverters.seqAsJavaList(qlForm.statements());
+		questions.add(question);
+		Seq<Statement> sList = JavaConverters.asScalaBufferConverter(questions).asScala().toSeq();
+
+		qlForm = new QLForm(qlForm.formName(), sList);
+		System.out.printf("\t%s : %s %s%n", ctx.name().getText(), ctx.TEXT().getText(), ctx.answer_type().getText());
+	}
 }

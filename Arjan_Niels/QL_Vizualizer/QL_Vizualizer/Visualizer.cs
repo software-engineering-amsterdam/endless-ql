62d7cbcd8c04cf1df9719c1c7a7610a2290a28bb
GrimGerbil <meijerarjan@live.nl>
2018-02-08 15:17:03 +0100

    Basic widget
    Implementation of the basic widget, using an Expression

--- Arjan_Niels/QL_Vizualizer/QL_Vizualizer/Visualizer.cs
+++ Arjan_Niels/QL_Vizualizer/QL_Vizualizer/Visualizer.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using QL_Vizualizer.Controllers;
+using QL_Vizualizer.Controllers.Display;
+using QL_Vizualizer.Widgets;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +18,17 @@ namespace QL_Vizualizer
         public Visualizer()
         {
             InitializeComponent();
+            WidgetController.Initialize(new WidgetDisplayControllerWindows(10, panel1));
+            WidgetController.Instance.SetWidgets(new List<QLWidget>()
+            {
+                new QLWidgetInt("a", "wat is 10 + 1?", new Expression<bool>(() => {return true; }), null),
+                new QLWidgetInt("b", "wat is 5 + 3?", new Expression<bool>(() => {return true; }), null),
+                new QLWidgetInt("c", "som:", new Expression<bool>(() => {return true; }), new Expression<int>(() => {
+                    return (WidgetController.Instance.GetWidget("a") as QLWidgetInt).AnswerValue + (WidgetController.Instance.GetWidget("b") as QLWidgetInt).AnswerValue;
+                }, "a", "b"))
+            });
+
+            WidgetController.Instance.Show();
         }
     }
 }

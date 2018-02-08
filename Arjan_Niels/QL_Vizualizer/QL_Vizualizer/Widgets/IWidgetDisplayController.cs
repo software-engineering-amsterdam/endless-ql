01dc76ffcc209a300b6e88e9a3c1557c33b5ded0
GrimGerbil <meijerarjan@live.nl>
2018-02-08 11:20:22 +0100

    IWidgetDisplayController implementation
    Base implementation of the IWidgetDisplayController

---
+++ Arjan_Niels/QL_Vizualizer/QL_Vizualizer/Widgets/IWidgetDisplayController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace QL_Vizualizer.Widgets
+{
+    public interface IWidgetDisplayController
+    {
+        /// <summary>
+        /// Shows widget on the specified position
+        /// </summary>
+        /// <typeparam name="T">Type of widget</typeparam>
+        /// <param name="widget">Widget to be displayed</param>
+        /// <param name="position">Top-left position of widget</param>
+        void Show<T>(QLWidget<T> widget, Tuple<float, float> position);
+    }
+}

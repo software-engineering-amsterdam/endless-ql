62a775b97490c47fe5041c1dd73e86eebcc49516
Mihai Onofrei <mihai303@gmail.com>
2018-02-12 14:42:44 +0200

    update StmtVisitor in order to support If staments contruction

--- Jorick_Mihai/src/org/uva/jomi/ql/ast/StmtVisitor.java
+++ Jorick_Mihai/src/org/uva/jomi/ql/ast/StmtVisitor.java
@@ -7,12 +7,15 @@ import org.uva.jomi.ql.parser.antlr.QLParser.CommandContext;
 
 public class StmtVisitor extends QLBaseVisitor<Stmt> {
 	
+	// An expression visitor is needed in order to visit the expression nodes in the Ast.
 	private final ExprVisitor exprVisitor;
 	
+	// The expression visitor is initialized in the default constructor
 	public StmtVisitor() {
 		this.exprVisitor = new ExprVisitor();
 	}
-
+	
+	// Builds a Form statement using the parser context. 
 	@Override public Stmt visitFormStmt(QLParser.FormStmtContext ctx) {
 		QLToken token = new QLToken(ctx.IDENTIFIER().getSymbol());
 		IndentifierExpr identifier = new IndentifierExpr(token);
@@ -20,9 +23,11 @@ public class StmtVisitor extends QLBaseVisitor<Stmt> {
 		return new FormStmt(identifier, blockStmt);
 	}
 	
+	// Builds a Block statement using the parser context.
 	@Override public Stmt visitBlockStmt(QLParser.BlockStmtContext ctx) {
 		List<Stmt> statements = new ArrayList<>(ctx.command().size());
 		
+		// Visit every statement in the block and add it to the statements array.
 		for (CommandContext statement : ctx.command()) {
 			statements.add(visit(statement));
 		}
@@ -30,12 +35,15 @@ public class StmtVisitor extends QLBaseVisitor<Stmt> {
 		return new BlockStmt(statements);
 	}
 	
+	// Builds a Question statement using the parser context.
 	@Override public Stmt visitQuestionStmt(QLParser.QuestionStmtContext ctx) {
 		QLToken token = new QLToken(ctx.IDENTIFIER().getSymbol());
 		String label = ctx.LABEL().getText();
 		QLType type = QLType.getType(ctx.TYPE().getText());
-		// Set the token and type of the identifier in order for it to match the question type
+		
+		// Set the token and type of the identifier in order for it to match the question type.
 		IndentifierExpr identifier = new IndentifierExpr(token, type);
+		
 		// Check if the question has an expression
 		if (ctx.expression() != null) {
 			Expr expression = ctx.expression().accept(exprVisitor);
@@ -43,5 +51,12 @@ public class StmtVisitor extends QLBaseVisitor<Stmt> {
 		}
 		
 		return new QuestionStmt(identifier, label, type);
+	}
+	
+	// Builds an If statement using the parser context.
+	@Override public Stmt visitIfStmt(QLParser.IfStmtContext ctx) {
+		Expr expression = ctx.expression().accept(exprVisitor);
+		BlockStmt blockStmt = (BlockStmt) visitBlockStmt(ctx.blockStmt());
+		return new IfStmt(expression, blockStmt);
 	}
 }

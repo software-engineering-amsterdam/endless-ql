74f3976206262c88a04eff7a7d17b9ba09b7cf89
Joana Correia Magalhães Sousa <jsousa039@gmail.com>
2018-02-12 12:34:21 +0100

    Added new methods on Question class

--- ForcePush/src/questions/Question.java
+++ ForcePush/src/questions/Question.java
@@ -6,17 +6,45 @@ private boolean mandatory;
 private String question;
 private String answer;
 
-public Question(String question)
-{
-	mandatory = false;
-	this.question = question;
-	answer = "";
-}
+	public Question(String question)
+	{
+		mandatory = false;
+		this.question = question;
+		answer = "";
+	}
 
-public void mandatory()
-{
-	mandatory = true;
+	public void mandatory()
+	{
+		if(!mandatory)
+			mandatory = true;
+		
+		else
+			mandatory = false;
 	
-}
+	}
+
+	public boolean isMandatory()
+	{
+		return mandatory;
+	}
 	
+	public void editQuestion(String newQuestion)
+	{
+		question = newQuestion;
+	}
+	
+	public String writtenQuestion()
+	{
+		return question;
+	}
+	
+	public void givenAnswer(String newAnswer)
+	{
+		answer = newAnswer;
+	}
+	
+	public String answerValue()
+	{
+		return answer;
+	}
 }

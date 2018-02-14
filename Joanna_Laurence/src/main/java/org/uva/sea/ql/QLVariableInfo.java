66a46be517deb837e42da328cf1d8d3744193e40
lauwdude <l.saes@live.nl>
2018-02-14 16:16:26 +0100

    Add variable information class that is required by type checker. Implementation still has to be made

---
+++ Joanna_Laurence/src/main/java/org/uva/sea/ql/QLVariableInfo.java
@@ -0,0 +1,11 @@
+package org.uva.sea.ql;
+
+/**
+ * Iterates over the AST and add links between variables and questions
+ * Checks if variables are not used before declared
+ */
+public class QLVariableInfo {
+
+
+
+}

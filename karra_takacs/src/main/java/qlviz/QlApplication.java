182bd5556e71a90d06a2fe46c8d0aa1021721501
Peter Takacs <hhike@outlook.com>
2018-02-14 09:48:10 +0100

    Removes comments that no longer apply

--- karra_takacs/src/main/java/qlviz/QlApplication.java
+++ karra_takacs/src/main/java/qlviz/QlApplication.java
@@ -11,21 +11,18 @@ public class QlApplication {
 		ICharStreamProvider charStreamProvider = new FileReader(args[0]);
 		CharStream charStream;
 		try {
-			// reads the file into a character stream
 			charStream = charStreamProvider.getStream();
 		} catch (IOException e) {
 			e.printStackTrace();
 			return;
 		}
-		// creates a lexer object
+
 		qlLexer lexer = new qlLexer(charStream);
-		// List<? extends Token> allTokens = lexer.getAllTokens();
-		// gets the tokens which match the lexer.
+
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
-		// pass the list of tokens to the parsers.
 		qlParser parser = new qlParser(tokens);
 
-		// Attaching the walk to the listener
+
 		ParseTreeWalker TreeWalker = new ParseTreeWalker();
 		TaxFormBaseListener listener = new TaxFormBaseListener();
 		TreeWalker.walk(listener, parser.questionName());

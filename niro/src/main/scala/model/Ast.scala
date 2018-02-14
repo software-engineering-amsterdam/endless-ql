efb29eddf4c872014fdc286cdd3f6515788b8281
rmathijn <rocco.mathijnandela@dhl.com>
2018-02-14 15:44:09 +0100

    Implement draft of Expression type

--- niro/src/main/scala/model/Ast.scala
+++ niro/src/main/scala/model/Ast.scala
@@ -5,8 +5,9 @@ object Ast {
   case class QLForm(formName: String, statements: Seq[Statement])
 
   sealed trait Statement
-  case class Question(id: String, label: String, answerType: AnswerType) extends Statement
-  case class Conditional() extends Statement
+  import model.Ast.Expression.Ident
+  case class Question(id: Ident, label: String, answerType: AnswerType) extends Statement
+  case class Conditional(condition: Expression, ifStatements: Seq[Statement], elseStatements: Seq[Statement]) extends Statement
 
   sealed trait AnswerType
   object AnswerType {
@@ -20,6 +21,48 @@ object Ast {
         case "text" => StringAnswerType
         case "string" => IntAnswerType
         case other => throw new IllegalArgumentException(s"Unsupported type: $other")
+      }
+    }
+  }
+
+  sealed trait Expression
+  object Expression {
+    case class IntConst(value: Int) extends Expression
+    case class BoolConst(value: Boolean) extends Expression
+    case class Ident(value: String) extends Expression
+
+    sealed trait Operator extends Expression
+    object Operator {
+      sealed trait UnaryExpr extends Operator
+      sealed trait ArithmExpr extends Operator
+      sealed trait CompExpr extends Operator
+      sealed trait LogicalExpr extends Operator
+
+      object UnaryExpr {
+        case class Sub(value: Expression) extends UnaryExpr
+        case class Not(value: Expression) extends UnaryExpr
+      }
+
+      object ArithmExpr {
+        case class Sub(left: Expression, right: Expression) extends ArithmExpr
+        case class Add(left: Expression, right: Expression) extends ArithmExpr
+        case class Div(left: Expression, right: Expression) extends ArithmExpr
+        case class Mul(left: Expression, right: Expression) extends ArithmExpr
+      }
+
+      object CompExpr {
+        case class Lt(left: Expression, right: Expression) extends CompExpr
+        case class LTe(left: Expression, right: Expression) extends CompExpr
+        case class GTe(left: Expression, right: Expression) extends CompExpr
+        case class Gt(left: Expression, right: Expression) extends CompExpr
+        case class Ne(left: Expression, right: Expression) extends CompExpr
+        case class Eq(left: Expression, right: Expression) extends CompExpr
+      }
+
+      object LogicalOp {
+        case class Or(left: Expression, right: Expression) extends CompExpr
+        case class And(left: Expression, right: Expression) extends CompExpr
+        case class Not(left: Expression, right: Expression) extends CompExpr
       }
     }
   }

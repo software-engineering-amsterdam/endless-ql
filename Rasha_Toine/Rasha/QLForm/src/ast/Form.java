f3f467953b4f746329c8bd64c285e211e62da0b9
rashadaoud <31595092+rashadaoud@users.noreply.github.com>
2018-02-14 14:55:02 +0100

    Change Form class on grammar change

--- Rasha_Toine/Rasha/QLForm/src/ast/Form.java
+++ Rasha_Toine/Rasha/QLForm/src/ast/Form.java
@@ -3,23 +3,27 @@ package ast;
 import java.util.List;
 import java.util.Set;
 
+import org.antlr.runtime.tree.TreeWizard.Visitor;
+
 import ast.AstNode;
 
 public class Form extends AstNode{
 
 	private final String id;
-	private final List<Block> blocks;
+	private final Block block;
 
-	public Form(String id, List<Block> blocks) {
+	public Form(String id, Block block) {
 		this.id = id;
-		this.blocks = blocks;
+		this.block = block;
 	}
 
 	public String getId() {
 		return id;
 	}
 
-	public List<Block> getBlocks() {
-		return this.blocks;
+	public Block getBlock() {
+		return this.block;
 	}
+	
+
 }

b3c7619e5e852d3520a954b1a48c43c4a1f95809
Simon Schneider <simon.sch@outlook.com>
2018-02-15 14:47:01 +0100

    FIX set correct path for expression mocks

--- schneider_kalf/src/App.tsx
+++ schneider_kalf/src/App.tsx
@@ -2,8 +2,7 @@ import * as React from 'react';
 import { FormComponent } from './rendering/components/form_component/FormComponent';
 import { sampleForm } from "./mock/sampleForm";
 import 'bootstrap/dist/css/bootstrap.css';
-import { testExpressionStuff } from "./form/nodes/test";
-
+import { testExpressionStuff } from "./mock/evaluationExamples";
 
 const qlParser = require("./parsing/parsers/ql_parser");
 
@@ -14,11 +13,6 @@ class App extends React.Component {
     const result = qlParser.parse(gl);
     console.log("Parser source:");
     console.log(result);
-
-    // const javascriptGrammar = require('!raw-loader!./parsing/grammars/javascript.pegjs');
-    // const javascriptParser = peg.generate(javascriptGrammar);
-    // const ast = javascriptParser.parse("if(true && var1){alert(\"asd\");alert(\"fgh\");}else{return null;}");
-    // console.log(ast); // OUTPUT: AST
 
     testExpressionStuff();
   }

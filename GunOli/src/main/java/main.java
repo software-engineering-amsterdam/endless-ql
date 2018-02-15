a92dcd9d3be45eefcab186faec15932c208f51e1
Leó Gunnar vidisson <leo@tolvugerdi.is>
2018-02-15 14:45:00 +0100

    First runnig vis

--- GunOli/src/main/java/main.java
+++ GunOli/src/main/java/main.java
@@ -1,18 +1,21 @@
 import ParseObjects.Form;
 
-import java.io.InputStream;
+import org.antlr.v4.gui.Trees;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
 
+
+import java.io.InputStream;
 public class main {
 
     public Form parseForm(InputStream stream){
         try{
-            //Todo: Fix project structure
-           /*
+
            QLLexer lexer = new QLLexer(CharStreams.fromStream(stream));
            CommonTokenStream tokens = new CommonTokenStream(lexer);
            QLParser parser = new QLParser(tokens);
            Trees.inspect(parser.root(), parser);
-           */
+
             return null;
         }catch(Exception e){
             System.out.println(e);

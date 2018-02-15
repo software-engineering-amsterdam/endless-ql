0527a048cb3b7df885e8137e536618068211c547
Rob Kunst <rob.kunst@student.uva.nl>
2018-02-15 12:03:51 +0100

    visitor updates

--- Rob_Jaap/java/src/main/java/com/chariotit/uva/sc/qdsl/FormsVisitor.java
+++ Rob_Jaap/java/src/main/java/com/chariotit/uva/sc/qdsl/FormsVisitor.java
@@ -5,10 +5,30 @@ public class FormsVisitor<T> extends GrammarBaseVisitor <T>{
 
     @Override public T visitForm(GrammarParser.FormContext ctx) {
 
-        System.out.println("Visiting form test");
+        System.out.println("Visiting form");
 
         return visitChildren(ctx);
 
     }
+
+
+    @Override public T visitQuestion(GrammarParser.QuestionContext ctx) {
+
+        System.out.println("Visiting question");
+
+        return visitChildren(ctx);
+
+    }
+
+    @Override public T visitLabel(GrammarParser.LabelContext ctx){
+
+        System.out.println("Visiting label");
+
+        System.out.println(ctx);
+
+        return visitChildren(ctx);
+
+    }
+
 
 }

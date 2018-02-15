6267e9e9fe827b34334e117b561a225eb1148291
hasan <hasanbilen38@gmail.com>
2018-02-15 11:45:37 +0100

    Refactor of GUI to use new refactored expressions.

--- Remi_Hasan/src/main/java/model/Condition.java
+++ Remi_Hasan/src/main/java/model/Condition.java
@@ -6,12 +6,17 @@ import java.util.ArrayList;
 
 // TODO rename to something like conditionblock for consistency
 public class Condition extends BlockElement {
-    Expression condition;
-    ArrayList<BlockElement> elements;
+    public Expression condition;
+    public ArrayList<BlockElement> elements;
 
     // TODO force condition to be of evaluated type ExpressionVariableBoolean
     public Condition(Expression condition, ArrayList<BlockElement> elements){
         this.condition = condition;
         this.elements = elements;
+    }
+
+    @Override
+    public boolean isCondition() {
+        return true;
     }
 }

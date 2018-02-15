3e83c7ff4be6313febf7dd223c043fcaa1065289
sangamm <sangamgupta10@gmail.com>
2018-02-15 12:16:38 +0100

    Rename Symbol to question Extract innerclass from Symbol

--- DogeQL/src/main/kotlin/FormListener.kt
+++ DogeQL/src/main/kotlin/FormListener.kt
@@ -1,46 +1,36 @@
-import symbol.BooleanValue
-import symbol.IntegerValue
-import symbol.SymbolTable
-import symbol.SymbolValue
+import Tree.ExpressionNode
+import data.*
 import java.util.*
 
 class FormListener : QuestionareLanguageParserBaseListener() {
 
-    val table = SymbolTable()
-    val stack = ArrayDeque<ExprNode>()
+    val table = QuestionTable()
+    val stack = ArrayDeque<ExpressionNode>()
 
     override fun exitForm(ctx: QuestionareLanguageParser.FormContext?) {
         stack.forEach {
             it.print()
             println()
         }
+
+        table.print()
     }
 
     override fun exitQuestion(ctx: QuestionareLanguageParser.QuestionContext) {
         val text = ctx.STRING_LITERAL().text
         val type = ctx.TYPE().text
         val identifier = ctx.IDENTIFIER().text
-        val question = Question(text, identifier, type)
 
-        table.register(identifier, factory(type))
-    }
+        val question = Question(text, typeParser(type))
 
-    fun factory(type: String): SymbolValue = when (type) {
-        "boolean" -> BooleanValue(false)
-        "integer" -> IntegerValue(1)
-        else      -> throw IllegalArgumentException("Fuck")
-    }
-
-
-    override fun enterExpr(ctx: QuestionareLanguageParser.ExprContext?) {
-        println("Enter: ${ctx.toString()}")
+        table.register(identifier, question)
     }
 
     override fun exitExpr(ctx: QuestionareLanguageParser.ExprContext?) {
-        println("Exit: $ctx")
 
         if(ctx?.childCount == 1){
-            val node = ExprNode(ctx.IDENTIFIER().text)
+            val identifier = ctx.IDENTIFIER().text
+            val node = ExpressionNode(identifier)
             stack.push(node)
         }else if (ctx?.childCount == 3){
             if (ctx.getChild(0).text == "(") {
@@ -49,25 +39,16 @@ class FormListener : QuestionareLanguageParserBaseListener() {
 
             val right = stack.pop()
             val left = stack.pop()
-            val node = ExprNode(ctx.getChild(1).text, left, right)
+            val node = ExpressionNode(ctx.getChild(1).text, left, right)
             stack.push(node)
-
-            val a = 1
         }
-
     }
-
-
 }
 
-data class ExprNode(val identifier: String, val leftNode: ExprNode? = null, val rightNode: ExprNode? = null) {
 
-    fun print() {
-        print("(")
-        leftNode?.print()
-        print(identifier)
-        rightNode?.print()
-        print(")")
-    }
-
+fun typeParser(type: String): BaseSymbolValue = when (type) {
+    "boolean" -> BooleanValue(false)
+    "integer" -> IntegerValue(1)
+    else      -> throw IllegalArgumentException("Fuck")
 }
+

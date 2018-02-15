56b4cfbc8c5fb7c8845f22890e22979c33d95a63
Joanna Roczniak <roczniak.joanna@gmail.com>
2018-02-15 18:11:30 +0100

    Update pom.xml with javafx plugin. Add first inputs in a gui.

---
+++ Joanna_Laurence/src/main/java/org/uva/sea/ql/gui/FormController.java
@@ -0,0 +1,13 @@
+package org.uva.sea.ql.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class FormController {
+
+    @FXML
+    private TextField question;
+
+    @FXML
+    private TextField condition;
+}
--- Joanna_Laurence/src/main/java/org/uva/sea/ql/gui/Main.java
+++ Joanna_Laurence/src/main/java/org/uva/sea/ql/gui/Main.java
@@ -1,17 +1,23 @@
 package org.uva.sea.ql.gui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/gui/demo-css.fxml"));
+        Scene scene = new Scene(root, 800, 500);
         primaryStage.setTitle("Form");
+        primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }

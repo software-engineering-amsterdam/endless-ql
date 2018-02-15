944ab94869dc9e7f3e7ba89875fbf98cb390a88f
TerryvanWalen <github@terryvanwalen.nl>
2018-02-15 13:43:30 +0100

    Add on AST form structure

--- nitpickers/pyql/pyql/ast/ast_node.py
+++ nitpickers/pyql/pyql/ast/ast_node.py
@@ -1,19 +1,15 @@
-from pyql.ast.code_location import CodeLocation
+from pyql.ast import code_location
 
 
 class ASTNode:
 
-    def __init__(self, loc: CodeLocation):
-        self.location = loc
+    def __init__(self, location: code_location.CodeLocation):
+        self._location = location
 
-    def get_code_location(self):
-        return self.location
+    @property
+    def location(self):
+        return self._location
 
     def __repr__(self):
-        return "AST node at: " + str(self.location)
+        return "AST node at: " + str(self._location)
 
-
-if __name__ == '__main__':
-    location = code_location.CodeLocation(2, 3)
-    node = ASTNode(location)
-    print(node)

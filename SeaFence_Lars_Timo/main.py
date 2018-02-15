a68a85734f318ac8a5b07d6e9356d44d5913ce6a
Lars Lokhoff <Lars@Lars.local>
2018-02-15 14:27:48 +0100

    add antlr4 for python2

--- SeaFence_Lars_Timo/main.py
+++ SeaFence_Lars_Timo/main.py
@@ -1,16 +1,26 @@
 import sys
 from antlr4 import *
-from antlr_files.QLLexer import QLLexer
-from antlr_files.QLParser import QLParser
+from antlr_files_2.QLLexer import QLLexer
+from antlr_files_2.QLParser import QLParser
+from antlr_files_2.QLVisitor import QLVisitor
+try:
+    from Tkinter import *
+except ImportError:
+    from _tkinter import *
 
 def main(argv):
-    input = FileStream(argv[1])
-    lexer = QLLexer(input)
-    stream = CommonTokenStream(lexer)
-    parser = QLParser(stream)
+    # input = FileStream(argv[1])
+    # lexer = QLLexer(input)
+    # stream = CommonTokenStream(lexer)
+    # parser = QLParser(stream)
 
-    tree = parser.form()
-    print(tree.toStringTree(recog=parser))
+    # tree = parser.form()
+    # print(tree.toStringTree(recog=parser))
+
+    root = Tk()
+    w = Label(root, text="Hello, world!")
+    w.pack()
+    root.mainloop()
 
 if __name__ == '__main__':
     main(sys.argv)

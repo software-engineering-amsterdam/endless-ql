5d08c5cc5e5045cfa208d35a205b91fb76694bf0
Hector Stenger <github@orti.works>
2018-02-16 16:38:25 +0100

    remove ruby based project files

--- henk/lib/hello_world.rb
+++
@@ -1,8 +0,0 @@
-module Henk
-  # Temporary test class
-  class HelloWorld
-    def self.main
-      'Hello World'
-    end
-  end
-end
--- henk/spec/hello_world_spec.rb
+++
@@ -1,5 +0,0 @@
-RSpec.describe Henk::HelloWorld do
-  it 'puts Hello World' do
-    expect(Henk::HelloWorld.main).to eq('Hello World')
-  end
-end

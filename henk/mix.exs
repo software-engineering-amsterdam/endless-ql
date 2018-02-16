2288a8981b657efd58682fbf66b64f044c6bd8de
Jovan Maric <jovanmaric777@gmail.com>
2018-02-16 20:56:46 +0100

    feat(espec): add support map compilation

--- henk/mix.exs
+++ henk/mix.exs
@@ -26,7 +26,7 @@ defmodule Henk.Mixfile do
   end
 
   # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(:test), do: ["lib", "spec/support"]
   defp elixirc_paths(_),     do: ["lib"]
 
   # Specifies your project dependencies.

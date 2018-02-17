4a8033815c026606c6930330f25b1cf94d25fda3
Peter Takacs <hhike@outlook.com>
2018-02-17 14:22:34 +0100

    Adds ConditionalBlockVisitor

--- karra_takacs/src/main/java/qlviz/interpreter/ConditionalBlockVisitor.java
+++ karra_takacs/src/main/java/qlviz/interpreter/ConditionalBlockVisitor.java
@@ -1,7 +1,27 @@
 package qlviz.interpreter;
 
 import qlviz.QLBaseVisitor;
+import qlviz.QLParser;
+import qlviz.model.BooleanExpression;
 import qlviz.model.ConditionalBlock;
+import qlviz.model.QuestionBlock;
+
+import java.util.stream.Collectors;
 
 public class ConditionalBlockVisitor extends QLBaseVisitor<ConditionalBlock> {
+
+    private final QLBaseVisitor<BooleanExpression> booleanExpressionVisitor;
+    private final QLBaseVisitor<QuestionBlock> questionBlockVisitor;
+
+    public ConditionalBlockVisitor(QLBaseVisitor<BooleanExpression> booleanExpressionVisitor, QLBaseVisitor<QuestionBlock> questionBlockVisitor) {
+        this.booleanExpressionVisitor = booleanExpressionVisitor;
+        this.questionBlockVisitor = questionBlockVisitor;
+    }
+
+    @Override
+    public ConditionalBlock visitConditionalBlock(QLParser.ConditionalBlockContext ctx) {
+        return new ConditionalBlock(
+                booleanExpressionVisitor.visitBooleanExpression(ctx.booleanExpression()),
+                ctx.questionBlock().stream().map(questionBlockVisitor::visitQuestionBlock).collect(Collectors.toList()));
+    }
 }

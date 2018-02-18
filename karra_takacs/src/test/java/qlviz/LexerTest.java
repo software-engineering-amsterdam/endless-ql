11f2348001d019f06615306740ac0d9ec9ff525b
Deepa Karra <deepa.karra@student.uva.nl>
2018-02-18 11:10:53 +0100

    Checks the type of tokens in the expected order

--- karra_takacs/src/test/java/qlviz/LexerTest.java
+++ karra_takacs/src/test/java/qlviz/LexerTest.java
@@ -4,29 +4,37 @@ import org.antlr.v4.runtime.*;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
-
 
 public class LexerTest {
 
-    @Test
-    public void testEmptyForm() {
-        QLLexer lexer = new QLLexer(new ANTLRInputStream("form { }"));
-        List<? extends Token> tokens = lexer.getAllTokens();
-        Assert.assertEquals(0, tokens.size());
-    }
+	@Test
+	public void testEmptyForm() {
+		QLLexer lexer = new QLLexer(new ANTLRInputStream("form { }"));
+		List<? extends Token> tokens = lexer.getAllTokens();
+		Assert.assertEquals(0, tokens.size());
+	}
 
-    @Test
-    public void testSingleQuestion() {
-        QLLexer lexer = new QLLexer(new ANTLRInputStream(
-                "form { " +
-                        "hasSoldHouse: \"Did you sell a house in 2010?\" boolean" +
-                        "}"));
-        List<? extends Token> tokens = lexer.getAllTokens();
-        Assert.assertEquals(3, tokens.size());
-        Assert.assertTrue(tokens.stream().anyMatch(t -> t.getType() == QLLexer.IDENTIFIER));
-        Assert.assertTrue(tokens.stream().anyMatch(t -> t.getType() == QLLexer.STRING));
-        Assert.assertTrue(tokens.stream().anyMatch(t -> t.getType() == QLLexer.TYPE));
-    }
+	@Test
+	public void testSingleQuestion() {
+		int j = 0;
+		List<Integer> tokenList = new ArrayList<Integer>();
+		QLLexer lexer = new QLLexer(
+				new ANTLRInputStream("form { " + "\"Did you sell a house in 2010?\" hasSoldHouse: boolean" + "}"));
+		tokenList.add(QLLexer.STRING);
+		tokenList.add(QLLexer.IDENTIFIER);
+		tokenList.add(QLLexer.TYPE);
+		List<? extends Token> tokens = lexer.getAllTokens();
+		Assert.assertEquals(3, tokens.size());
+		//matches the type of tokens
+		for (Token token : tokens) {
+			while (j < tokenList.size()) {
+				Assert.assertEquals(token.getType(), tokenList.get(j).intValue());
+				break;
+			}
+			j++;
+		}
+	}
 
 }

28bd0e553c96fc5166b01044698a0bd9442a06cb
GrimGerbil <meijerarjan@live.nl>
2018-02-19 19:21:33 +0100

    Unittest WidgetDisplayController WIP
    Work in progress - unittest widgetdisplaycontroller

---
+++ Arjan_Niels/QL_Vizualizer/QL_Visualizer.Tests/Display/WidgetDisplayControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QL_Vizualizer.Controllers;
+using QL_Vizualizer.Controllers.Display;
+using QL_Vizualizer.Factories;
+using QL_Vizualizer.Widgets.Types;
+
+namespace QL_Visualizer.Tests.Display
+{
+    [TestClass]
+    public abstract class WidgetDisplayControllerTest<T>
+    {
+        protected WidgetController _widgetController;
+        protected WidgetDisplayController<T> _widgetDisplayController;
+        protected ElementFactory<T> _elementFactory;
+
+        private QLWidgetInt _intWidget;
+        private QLWidgetBool _boolWidget;
+        private QLWidgetString _stringWidget;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _intWidget = new QLWidgetInt("a", "q1");
+            _boolWidget = new QLWidgetBool("b", "q2");
+            _stringWidget = new QLWidgetString("c", "q3");
+        }
+
+
+        [TestMethod]
+        public void CreateElementTest()
+        {
+            //_widgetController.
+        }
+
+        [TestMethod]
+        public abstract void UpdateElementTest();
+    }
+}
---
+++ Arjan_Niels/QL_Vizualizer/QL_Visualizer.Tests/Display/WidgetDisplayControllerWindowsTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QL_Visualizer.Tests.Display
+{
+    [TestClass]
+    public class WidgetDisplayControllerWindowsTest : WidgetDisplayControllerTest<Control>
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        public override void UpdateElementTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

1319bf51ecdbcdfc732ca7222ca40caa94fe2724
V-Jong <vince-ny@hotmail.com>
2018-02-20 23:02:26 +0100

    Moved tests to separate test project

--- Erik_Vincent/Assignment1/Assignment1/QuestionForm.cs
+++ Erik_Vincent/Assignment1/Assignment1/QuestionForm.cs
@@ -3,8 +3,11 @@ using Antlr4.Runtime.Tree;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo("Assignment1Tests")]
 
 namespace Assignment1
 {
---
+++ Erik_Vincent/Assignment1/Assignment1Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Assignment1Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Assignment1Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f1854c69-e55a-4c71-80a8-da6ac4f91a55")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

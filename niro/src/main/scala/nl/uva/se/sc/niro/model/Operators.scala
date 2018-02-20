4c676dc6363cebc186c4bc538030d4be8abf0259
rmathijn <rocco.mathijnandela@dhl.com>
2018-02-20 01:00:12 +0100

    Make factory method for Operator more specific

--- niro/src/main/scala/nl/uva/se/sc/niro/model/Operators.scala
+++ niro/src/main/scala/nl/uva/se/sc/niro/model/Operators.scala
@@ -3,7 +3,31 @@ package nl.uva.se.sc.niro.model
 object Operators {
   abstract class Operator
   abstract class BinaryOperator extends Operator
+  object BinaryOperator {
+    def apply(operator: String): BinaryOperator = operator match {
+      case "<" => Lt
+      case "<=" => LTe
+      case "==" => Eq
+      case "!=" => Ne
+      case ">=" => GTe
+      case ">" => Gt
+      case "-" => Sub
+      case "+" => Add
+      case "/" => Div
+      case "*" => Mul
+      case "||" => Or
+      case "&&" => And
+    }
+  }
+
   abstract class UnaryOperator extends Operator
+  object UnaryOperator {
+    def apply(operator: String): UnaryOperator = operator match {
+      case "!" => Neg
+      case "-" => Min
+    }
+  }
+
 
   abstract class BasicArithmeticOperator extends BinaryOperator
   case object Add extends BasicArithmeticOperator
@@ -25,20 +49,4 @@ object Operators {
 
   case object Min extends UnaryOperator
   case object Neg extends UnaryOperator
-
-  def apply(operator: String): Operator = operator match {
-    case "<" => Lt
-    case "<=" => LTe
-    case "==" => Eq
-    case "!=" => Ne
-    case ">=" => GTe
-    case ">" => Gt
-    case "-" => Sub
-    case "+" => Add
-    case "/" => Div
-    case "*" => Mul
-    case "||" => Or
-    case "&&" => And
-    case "!" => Neg
-  }
 }

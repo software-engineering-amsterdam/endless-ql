fbf0351d07cdd4655ebf99331e1808df09094609
Sara Oonk <sara.oonk@live.com>
2018-02-21 09:59:13 +0100

    Simple WebUI main page, interpreter with dummy data

---
+++ JS/qlparser/src/main/java/nl/uva/js/qlparser/ui/WebUI.java
@@ -0,0 +1,53 @@
+package nl.uva.js.qlparser.ui;
+
+
+import com.vaadin.ui.*;
+import nl.uva.js.qlparser.interpreter.FormInterpreter;
+import nl.uva.js.qlparser.logic.Ingester;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import nl.uva.js.qlparser.models.Form;
+
+import java.io.IOException;
+
+@SpringUI
+@Theme("valo")
+public class WebUI extends UI {
+
+    private Ingester ingester = new Ingester();
+    private FormInterpreter interpreter = new FormInterpreter();
+
+//    public WebUI(Ingester ingester) {
+//        this.ingester = ingester;
+//    }
+
+    @Override
+    protected void init(VaadinRequest request) {
+        TextArea area = new TextArea("Enter Questionnaire Language:");
+        area.setWidth("700");
+        area.setHeight("500");
+
+        final Button btnRender = new Button("Render QL");
+
+        setContent(new VerticalLayout(area, btnRender));
+
+        btnRender.addClickListener(
+                e -> setContent(createLayout(area.getValue()))
+        );
+    }
+
+    private Layout createLayout(String qlInput) {
+        try {
+            Form form = ingester.toParsedForm("");
+            return interpreter.interpretForm(form);
+
+
+        } catch (IOException e) {
+            VerticalLayout error = new VerticalLayout();
+            error.addComponent(new Label("Could not parse Questionnaire Language."));
+            return error;
+        }
+    }
+}

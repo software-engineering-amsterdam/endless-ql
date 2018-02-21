937cf1d38fb6979244a0d44e7a073a9735b7f8f9
Unknown <s.vdh@live.nl>
2018-02-21 11:47:41 +0100

    Patched gui.py after ql_ast.py update

--- Pythonistas/parse/ql_parser.py
+++ Pythonistas/parse/ql_parser.py
@@ -48,7 +48,7 @@ def stmt():
     """
     Possible paterns to be recognized
     """
-    return form_stmt() | assign_stmt() | if_stmt()
+    return form_stmt() | assign_stmt() #| if_stmt()
 
 
 def form_stmt():

20a0882240b1bb88dc76af0e2a584be90261f488
Lars Lokhoff <Lars@wcw-stud-145-109-3-248.wireless.uva.nl>
2018-02-21 14:40:14 +0100

    fix QLast

--- SeaFence_Lars_Timo/antlr_files_2/QLVisitor.py
+++ SeaFence_Lars_Timo/antlr_files_2/QLVisitor.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Generated from QL.g4 by ANTLR 4.7
 from antlr4 import *
 from QLast import *
@@ -11,9 +10,13 @@ class QLVisitor(ParseTreeVisitor):
     def visitForm(self, ctx):
         form_id = ctx.form_id().getText()
         node = formNode(form_id)
-        print(node)
-        children = self.visitChildren(ctx)
-        print children
+        block = ctx.block()
+        statements = block.statement()
+        print statements
+        for statement in statements:
+           node = self.visit(statement)
+           print node
+
         # return self.visitChildren(ctx)
         return 
 
@@ -40,7 +43,6 @@ class QLVisitor(ParseTreeVisitor):
         var = ctx.var().getText()
         vartype = ctx.vartype().getText()
         node = questionNode(question, var, vartype)
-        # print(node)
         return node
         # return self.visitChildren(ctx)
 
@@ -91,91 +93,3 @@ class QLVisitor(ParseTreeVisitor):
     # Visit a parse tree produced by QLParser#else_cond.
     def visitElse_cond(self, ctx):
         return self.visitChildren(ctx)
-=======
-# Generated from QL.g4 by ANTLR 4.7
-from antlr4 import *
-from QLast import *
-
-# This class defines a complete generic visitor for a parse tree produced by QLParser.
-
-class QLVisitor(ParseTreeVisitor):
-
-    # Visit a parse tree produced by QLParser#form.
-    def visitForm(self, ctx):
-        form_id = ctx.form_id().getText()
-        node = formNode(form_id)
-        print(node)
-        return self.visitChildren(ctx)
-
-
-    # Visit a parse tree produced by QLParser#block.
-    def visitBlock(self, ctx):
-        return self.visitChildren(ctx)
-
-
-    # Visit a parse tree produced by QLParser#statement.
-    def visitStatement(self, ctx):
-        return self.visitChildren(ctx)
-
-
-    # Visit a parse tree produced by QLParser#assignment.
-    def visitAssignment(self, ctx):
-        return self.visitChildren(ctx)
-
-
-    # Visit a parse tree produced by QLParser#question.
-    def visitQuestion(self, ctx):
-        question = ctx.STR().getText()
-        var = ctx.var().getText()
-        vartype = ctx.vartype().getText()
-        node = questionNode(question, var, vartype)
-        print(node)
-        return self.visitChildren(ctx)
-
-
-    # Visit a parse tree produced by QLParser#conditional.
-    def visitConditional(self, ctx):
-        return self.visitChildren(ctx)
-
-
-    # Visit a parse tree produced by QLParser#expression.
-    def visitExpression(self, ctx):
-        # Todo: Make distinction between expressions
-        # expression = ctx.expression()
-        # if (expression)
-            # print(ctx.expression().getText())
-        return self.visitChildren(ctx)
-
-
-    # Visit a parse tree produced by QLParser#form_id.
-    def visitForm_id(self, ctx):
-        return self.visitChildren(ctx)
-
-
-    # Visit a parse tree produced by QLParser#var.
-    def visitVar(self, ctx):
-        return self.visitChildren(ctx)
-
-
-    # Visit a parse tree produced by QLParser#vartype.
-    def visitVartype(self, ctx):
-        return self.visitChildren(ctx)
-
-
-    # Visit a parse tree produced by QLParser#if_cond.
-    def visitIf_cond(self, ctx):
-        expression = ctx.expression().getText()
-        node = ifNode(expression)
-        print(node)
-        return self.visitChildren(ctx)
-
-
-    # Visit a parse tree produced by QLParser#elif_cond.
-    def visitElif_cond(self, ctx):
-        return self.visitChildren(ctx)
-
-
-    # Visit a parse tree produced by QLParser#else_cond.
-    def visitElse_cond(self, ctx):
-        return self.visitChildren(ctx)
->>>>>>> 968270c7646140394a067945eb033ac1de4aff1f

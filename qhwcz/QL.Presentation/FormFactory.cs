0b6ed2e957e347ef7d207ed0a294a5d235f35cab
porke <porke@gazeta.pl>
2018-02-21 23:36:25 +0100

    Added a FormBuildingVisitor to construct the UI.

--- qhwcz/QL.Presentation/FormFactory.cs
+++ qhwcz/QL.Presentation/FormFactory.cs
@@ -1,6 +1,5 @@
 ﻿using QL.Core.Api;
 using System.Collections.Generic;
-using System.Windows;
 using System.Windows.Controls;
 
 namespace QL.Presentation
@@ -11,23 +10,15 @@ namespace QL.Presentation
 
         internal FormFactory(IParsingService parsingService)
         {
-            _parsingService = parsingService;
+            _parsingService = parsingService;            
         }
 
         public IList<Control> CreateControls(string question)
         {
             var parsedSymbols = _parsingService.ParseQLInput(question);
-
-            IList<Control> createdControls = new List<Control>();            
-            createdControls.Add(new Label { Content = parsedSymbols.Form.Label, FontWeight = FontWeights.Bold });            
-
-            // Needs to be replaced.
-            /*parsedSymbols.Questions.ToList().ForEach(x =>
-            {
-                createdControls.Add(new Label { Content = x.Description });
-            });*/
-
-            return createdControls;
+            var formBuildingVisitor = new FormBuildingVisitor();
+            parsedSymbols.Form.Accept(formBuildingVisitor);
+            return formBuildingVisitor.Controls;
         }
     }
 }

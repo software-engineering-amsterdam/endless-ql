17a5cb2e0b634f4012fed88fcefb86f2aa3e35a5
George Vletsas <georgevle93@gmail.com>
2018-02-21 17:14:14 +0100

    Changed Antlr Config, Reformatted grammar

--- ForcePush/src/main/java/org/uva/forcepushql/antlr/GrammarParserListener.java
+++ ForcePush/src/main/java/org/uva/forcepushql/antlr/GrammarParserListener.java
@@ -1,5 +1,5 @@
 // Generated from C:/Users/georg/Documents/GitHub/endless-ql/ForcePush/src/main/resources/antlr\GrammarParser.g4 by ANTLR 4.7
-package antlr;
+package org.uva.forcepushql.antlr;
 import org.antlr.v4.runtime.tree.ParseTreeListener;
 
 /**
@@ -7,36 +7,6 @@ import org.antlr.v4.runtime.tree.ParseTreeListener;
  * {@link GrammarParser}.
  */
 public interface GrammarParserListener extends ParseTreeListener {
-	/**
-	 * Enter a parse tree produced by {@link GrammarParser#a}.
-	 * @param ctx the parse tree
-	 */
-	void enterA(GrammarParser.AContext ctx);
-	/**
-	 * Exit a parse tree produced by {@link GrammarParser#a}.
-	 * @param ctx the parse tree
-	 */
-	void exitA(GrammarParser.AContext ctx);
-	/**
-	 * Enter a parse tree produced by {@link GrammarParser#b}.
-	 * @param ctx the parse tree
-	 */
-	void enterB(GrammarParser.BContext ctx);
-	/**
-	 * Exit a parse tree produced by {@link GrammarParser#b}.
-	 * @param ctx the parse tree
-	 */
-	void exitB(GrammarParser.BContext ctx);
-	/**
-	 * Enter a parse tree produced by {@link GrammarParser#r}.
-	 * @param ctx the parse tree
-	 */
-	void enterR(GrammarParser.RContext ctx);
-	/**
-	 * Exit a parse tree produced by {@link GrammarParser#r}.
-	 * @param ctx the parse tree
-	 */
-	void exitR(GrammarParser.RContext ctx);
 	/**
 	 * Enter a parse tree produced by {@link GrammarParser#variable}.
 	 * @param ctx the parse tree
@@ -48,6 +18,56 @@ public interface GrammarParserListener extends ParseTreeListener {
 	 */
 	void exitVariable(GrammarParser.VariableContext ctx);
 	/**
+	 * Enter a parse tree produced by {@link GrammarParser#decisions}.
+	 * @param ctx the parse tree
+	 */
+	void enterDecisions(GrammarParser.DecisionsContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link GrammarParser#decisions}.
+	 * @param ctx the parse tree
+	 */
+	void exitDecisions(GrammarParser.DecisionsContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link GrammarParser#operator}.
+	 * @param ctx the parse tree
+	 */
+	void enterOperator(GrammarParser.OperatorContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link GrammarParser#operator}.
+	 * @param ctx the parse tree
+	 */
+	void exitOperator(GrammarParser.OperatorContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link GrammarParser#type}.
+	 * @param ctx the parse tree
+	 */
+	void enterType(GrammarParser.TypeContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link GrammarParser#type}.
+	 * @param ctx the parse tree
+	 */
+	void exitType(GrammarParser.TypeContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link GrammarParser#arithmetic}.
+	 * @param ctx the parse tree
+	 */
+	void enterArithmetic(GrammarParser.ArithmeticContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link GrammarParser#arithmetic}.
+	 * @param ctx the parse tree
+	 */
+	void exitArithmetic(GrammarParser.ArithmeticContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link GrammarParser#comparison}.
+	 * @param ctx the parse tree
+	 */
+	void enterComparison(GrammarParser.ComparisonContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link GrammarParser#comparison}.
+	 * @param ctx the parse tree
+	 */
+	void exitComparison(GrammarParser.ComparisonContext ctx);
+	/**
 	 * Enter a parse tree produced by {@link GrammarParser#expression}.
 	 * @param ctx the parse tree
 	 */
@@ -58,15 +78,15 @@ public interface GrammarParserListener extends ParseTreeListener {
 	 */
 	void exitExpression(GrammarParser.ExpressionContext ctx);
 	/**
-	 * Enter a parse tree produced by {@link GrammarParser#question}.
+	 * Enter a parse tree produced by {@link GrammarParser#questionFormat}.
 	 * @param ctx the parse tree
 	 */
-	void enterQuestion(GrammarParser.QuestionContext ctx);
+	void enterQuestionFormat(GrammarParser.QuestionFormatContext ctx);
 	/**
-	 * Exit a parse tree produced by {@link GrammarParser#question}.
+	 * Exit a parse tree produced by {@link GrammarParser#questionFormat}.
 	 * @param ctx the parse tree
 	 */
-	void exitQuestion(GrammarParser.QuestionContext ctx);
+	void exitQuestionFormat(GrammarParser.QuestionFormatContext ctx);
 	/**
 	 * Enter a parse tree produced by {@link GrammarParser#conditional}.
 	 * @param ctx the parse tree
@@ -78,13 +98,13 @@ public interface GrammarParserListener extends ParseTreeListener {
 	 */
 	void exitConditional(GrammarParser.ConditionalContext ctx);
 	/**
-	 * Enter a parse tree produced by {@link GrammarParser#form}.
+	 * Enter a parse tree produced by {@link GrammarParser#formStructure}.
 	 * @param ctx the parse tree
 	 */
-	void enterForm(GrammarParser.FormContext ctx);
+	void enterFormStructure(GrammarParser.FormStructureContext ctx);
 	/**
-	 * Exit a parse tree produced by {@link GrammarParser#form}.
+	 * Exit a parse tree produced by {@link GrammarParser#formStructure}.
 	 * @param ctx the parse tree
 	 */
-	void exitForm(GrammarParser.FormContext ctx);
+	void exitFormStructure(GrammarParser.FormStructureContext ctx);
 }

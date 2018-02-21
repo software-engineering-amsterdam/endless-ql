4108a181e82e502d648c73f186c34007dee205f5
V-Jong <vince-ny@hotmail.com>
2018-02-21 16:30:35 +0100

    Renamed test file, adjusted tests to updated model classes and added IfStatement class

---
+++ Erik_Vincent/Assignment1/Assignment1/Model/IfStatement.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment1.Model
+{
+    class IfStatement : Statement
+    {
+        private Expression _condition;
+        private List<Content> _thenContent;
+        private List<Content> _elseContent;
+        public Expression Condition
+        {
+            get
+            {
+                return _condition;
+            }
+        }
+        public List<Content> ThenContent
+        {
+            get
+            {
+                return _thenContent;
+            }
+        }
+        public List<Content> ElseContent
+        {
+            get
+            {
+                return _elseContent;
+            }
+        }
+
+        public IfStatement(Expression condition, bool hasElseContent)
+        {
+            _condition = condition;
+            _thenContent = new List<Content>();
+            if (hasElseContent)
+                _elseContent = new List<Content>();
+        }
+
+        public void AddThenContent(Content newContent)
+        {
+            _thenContent.Add(newContent);
+        }
+
+        public bool AddElseContent(Content newContent)
+        {
+            if (_elseContent == null)
+                return false;
+            _elseContent.Add(newContent);
+            return true;
+        }
+
+        public override Control CreateControl()
+        {
+            FlowLayoutPanel panel = (FlowLayoutPanel)base.CreateControl();
+            if (_condition.Evaluate())
+            {
+                addContentToPanel(panel, _thenContent);
+            }
+            else if (_elseContent.Count > 0)
+            {
+                addContentToPanel(panel, _elseContent);
+            }
+            else
+            {
+                panel.Visible = false;
+            }
+            return panel;
+        }
+
+        private void addContentToPanel(FlowLayoutPanel panel, List<Content> content)
+        {
+            //foreach (Content contentItem in content)
+            foreach (Question question in content.OfType<Question>())
+            {
+                panel.Controls.Add(question.CreateControl());
+            }
+        }
+    }
+}
---
+++ Erik_Vincent/Assignment1/Assignment1Tests/QLModelTests.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using Assignment1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assignment1Tests
+{
+    [TestClass]
+    public class QLModelTests
+    {
+        [TestMethod]
+        public void TestCreateForm()
+        {
+            QuestionForm testF = new QuestionForm(TestFormId, new List<Content>());
+
+            Assert.AreEqual(TestFormId, testF.Id);
+        }
+
+        [TestMethod]
+        public void TestCreateQuestion()
+        {
+            Question testQ = new Question(TestQuestionId, TestQuestionLabel);
+
+            Assert.AreEqual(TestQuestionId, testQ.Id);
+            Assert.AreEqual(TestQuestionLabel, testQ.Label);
+        }
+
+        [TestMethod]
+        public void TestAddQuestionToForm()
+        {
+            Question testQ = new Question(TestQuestionId, TestQuestionLabel);
+            List<Content> contentList = new List<Content>() { testQ };
+            QuestionForm testF = new QuestionForm(TestFormId, contentList);
+
+            Assert.IsNotNull(testF.Content);
+            Assert.AreEqual(1, testF.Content.Count);
+            Assert.AreSame(testQ, testF.Content[0]);
+        }
+
+        [TestMethod]
+        public void TestBinaryExpression()
+        {
+            // Create expression with + operator
+
+            // Run evaluator
+            
+            // Left and right operands should be integers
+
+            // Sum should be the sum of the left and right operands
+        }
+
+        [TestMethod]
+        public void TestLogicalExpressionBothTrue()
+        {
+            // Create epression with && operator with left and right as true
+
+            // Run evaluator
+            
+            // Left and right operands should be boolean
+
+            // Result should be true
+        }
+
+        [TestMethod]
+        public void TestLogicalExpressionLeftFalse()
+        {
+            // Create epression with && operator with left as false and right as true
+
+            // Run evaluator
+            
+            // Left and right operands should be boolean
+
+            // Result should be false
+        }
+
+        [TestMethod]
+        public void TestLogicalExpressionRightFalse()
+        {
+            // Create epression with && operator with left as true and right as false
+
+            // Run evaluator
+            
+            // Left and right operands should be boolean
+
+            // Result should be false
+        }
+
+        [TestMethod]
+        public void TestLogicalExpressionInvalidOperand()
+        {
+            // Create epression with && operator with left as true and right as integer
+
+            // Run evaluator
+
+            // Error should be thrown
+        }
+
+        [TestMethod]
+        public void TestComparisonExpression()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestIfStatement()
+        {
+
+        }
+
+        private static string TestFormId = "testForm";
+        private static string TestQuestionId = "testQuestion";
+        private static string TestQuestionLabel = "Programming, ****, do you speak it?";
+    }
+}

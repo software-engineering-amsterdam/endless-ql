53608ec72b790ff8f529de30c653b5955cc61bfc
bicker <bickerkards@tuta.io>
2018-02-21 15:07:17 +0100

    expression grammar now outputs node objects instead of tuples

--- PyQuest/src/scanparse/qlyacc.py
+++ PyQuest/src/scanparse/qlyacc.py
@@ -91,79 +91,79 @@ class QLParser:
     @staticmethod
     def p_not(p):
         """expression : NOT expression"""
-        p[0] = ('NOT', p[2])
+        p[0] = UnaryOperatorNode(p.lineno(1), bool, 'not', p[2])
 
     # Binary operators
     @staticmethod
     def p_and(p):
         """expression : expression AND expression"""
-        p[0] = ('AND', p[1], p[3])
+        p[0] = BinaryOperatorNode(p.lineno(2), bool, 'and', p[1], p[3])
 
     @staticmethod
     def p_or(p):
         """expression : expression OR expression"""
-        p[0] = ('OR', p[1], p[3])
+        p[0] = BinaryOperatorNode(p.lineno(2), bool, 'OR', p[1], p[3])
 
     @staticmethod
     def p_plus(p):
         """expression : expression PLUS expression"""
-        p[0] = ('PLUS', p[1], p[3])
+        p[0] = BinaryOperatorNode(p.lineno(2), None, 'PLUS', p[1], p[3])
 
     @staticmethod
     def p_minus(p):
         """expression : expression MINUS expression"""
-        p[0] = ('MINUS', p[1], p[3])
+        p[0] = BinaryOperatorNode(p.lineno(2), None, 'MINUS', p[1], p[3])
 
     @staticmethod
     def p_times(p):
         """expression : expression TIMES expression"""
-        p[0] = ('TIMES', p[1], p[3])
+        p[0] = BinaryOperatorNode(p.lineno(2), None, 'TIMES', p[1], p[3])
 
     @staticmethod
     def p_divide(p):
         """expression : expression DIVIDE expression"""
-        p[0] = ('DIVIDE', p[1], p[3])
+        p[0] = BinaryOperatorNode(p.lineno(2), None, 'DIVIDE', p[1], p[3])
 
     @staticmethod
     def p_equals(p):
         """expression : expression EQ expression"""
-        p[0] = ('EQ', p[1], p[3])
+        p[0] = BinaryOperatorNode(p.lineno(2), bool, 'EQ', p[1], p[3])
 
     @staticmethod
     def p_not_equals(p):
         """expression : expression NE expression"""
-        p[0] = ('NE', p[1], p[3])
+        p[0] = BinaryOperatorNode(p.lineno(2), bool, 'NE', p[1], p[3])
 
     @staticmethod
     def p_less_equals(p):
         """expression : expression LE expression"""
-        p[0] = ('LE', p[1], p[3])
+        p[0] = BinaryOperatorNode(p.lineno(2), bool, 'LE', p[1], p[3])
 
     @staticmethod
     def p_less_than(p):
         """expression : expression LT expression"""
-        p[0] = ('LT', p[1], p[3])
+        p[0] = BinaryOperatorNode(p.lineno(2), bool, 'LT', p[1], p[3])
 
     @staticmethod
     def p_greater_equals(p):
         """expression : expression GE expression"""
-        p[0] = ('GE', p[1], p[3])
+        p[0] = BinaryOperatorNode(p.lineno(2), bool, 'EQ', p[1], p[3])
 
     @staticmethod
     def p_greater_than(p):
         """expression : expression GT expression"""
-        p[0] = ('GT', p[1], p[3])
+        p[0] = BinaryOperatorNode(p.lineno(2), bool, 'GT', p[1], p[3])
 
     # Literals
     @staticmethod
     def p_number(p):
         """expression : NUMBER"""
-        p[0] = ('NUMBER', p[1])
+        p[0] = LiteralNode(p.lineno(1), int, p[1])
 
     @staticmethod
     def p_float(p):
         """expression : FLOAT"""
-        p[0] = ('FLOAT', p[1])
+        p[0] = LiteralNode(p.lineno(1), float, p[1])
 
     # Other
     @staticmethod

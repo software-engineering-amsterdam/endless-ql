ca24400722f09ffdf9edff88038af0f97d3d2e55
Remi van Veen <remi.vanveen@anycode.nl>
2018-02-21 13:19:40 +0100

    Removed redundant visitor question type

--- Remi_Hasan/src/main/java/visitor/VisitorQuestion.java
+++ Remi_Hasan/src/main/java/visitor/VisitorQuestion.java
@@ -3,21 +3,30 @@ package visitor;
 import antlr.QLBaseVisitor;
 import antlr.QLParser;
 import expression.Expression;
+import expression.ExpressionFactory;
 import model.Question;
 
 public class VisitorQuestion extends QLBaseVisitor<Question> {
 
     @Override
     public Question visitQuestion(QLParser.QuestionContext ctx) {
-        VisitorQuestionType visitorQuestionType = new VisitorQuestionType();
 
         String questionName = ctx.identifier().getText();
         String questionText = ctx.questionString().getText();
         // remove quotes from text
         questionText = questionText.substring(1, questionText.length() - 1);
-        Expression answer = visitorQuestionType.visit(ctx.questionType());
 
+        Expression answer = getStandardValue(ctx.questionType());
         return new Question(questionName, questionText, answer);
+    }
+
+    private Expression getStandardValue(QLParser.QuestionTypeContext questionType) {
+        if (questionType.expression() != null) {
+            VisitorExpression visitorExpression = new VisitorExpression();
+            return visitorExpression.visit(questionType.expression());
+        }
+
+        return ExpressionFactory.createExpression(questionType.type().getText());
     }
 
 }

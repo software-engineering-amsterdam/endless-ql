80b8b471701efc2c85d6ee6cff811b99301130f5
George Vletsas <georgevle93@gmail.com>
2018-02-21 12:41:49 +0100

    Resorted Conflicts
    Resorted conflicts from migration to maven

--- ForcePush/src/main/java/org/uva/forcepushql/questions/Semantic_Differential_Scales.java
+++ ForcePush/src/main/java/org/uva/forcepushql/questions/Semantic_Differential_Scales.java
@@ -1,7 +1,7 @@
-<<<<<<< HEAD:ForcePush/src/main/java/org/uva/forcepushql/questions/Semantic_Differential_Scales.java
 package org.uva.forcepushql.questions;
 
-public class Semantic_Differential_Scales extends Question{
+
+public class Semantic_Differential_Scales extends Question {
 
 	public Semantic_Differential_Scales(String question, String answerType, String answerName) 
 	{
@@ -10,16 +10,3 @@ public class Semantic_Differential_Scales extends Question{
 
 
 }
-=======
-package questions;
-
-public class Semantic_Differential_Scales extends Question{
-
-	public Semantic_Differential_Scales(String question, String answerType, String answerName) 
-	{
-		super(question, answerType, answerName);
-	}
-
-
-}
->>>>>>> 4e9549a66df631f508566cf71b749537c31c0ae7:ForcePush/src/questions/Semantic_Differential_Scales.java

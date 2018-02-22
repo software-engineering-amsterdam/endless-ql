935f24e7d9cef26eb9b02714313cc17bd1da8053
rmathijn <rocco.mathijnandela@dhl.com>
2018-02-22 11:03:59 +0100

    Refactor IntAnswer a bit

--- niro/src/main/scala/nl/uva/se/sc/niro/model/Expressions/answers/IntAnswer.scala
+++ niro/src/main/scala/nl/uva/se/sc/niro/model/Expressions/answers/IntAnswer.scala
@@ -6,17 +6,17 @@ import nl.uva.se.sc.niro.model._
 case class IntAnswer(possibleValue: Option[Int]) extends Answer {
 
   def apply(operator: BinaryOperator, other: Answer): Answer = other match {
-    case (other: IntAnswer) => operator match {
-      case Add => IntAnswer(combine[Int](other)(_ + _))
-      case Sub => IntAnswer(combine[Int](other)(_ - _))
-      case Mul => IntAnswer(combine[Int](other)(_ * _))
-      case Div => IntAnswer(combine[Int](other)(_ / _))
-      case Lt => BooleanAnswer(combine[Boolean](other)(_ < _))
-      case LTe => BooleanAnswer(combine[Boolean](other)(_ <= _))
-      case GTe => BooleanAnswer(combine[Boolean](other)(_ >= _))
-      case Gt => BooleanAnswer(combine[Boolean](other)(_ > _))
-      case Ne => BooleanAnswer(combine[Boolean](other)(_ != _))
-      case Eq => BooleanAnswer(combine[Boolean](other)(_ == _))
+    case otherIntAnswer: IntAnswer => operator match {
+      case Add => IntAnswer(combine[Int](otherIntAnswer)(_ + _))
+      case Sub => IntAnswer(combine[Int](otherIntAnswer)(_ - _))
+      case Mul => IntAnswer(combine[Int](otherIntAnswer)(_ * _))
+      case Div => IntAnswer(combine[Int](otherIntAnswer)(_ / _))
+      case Lt => BooleanAnswer(combine[Boolean](otherIntAnswer)(_ < _))
+      case LTe => BooleanAnswer(combine[Boolean](otherIntAnswer)(_ <= _))
+      case GTe => BooleanAnswer(combine[Boolean](otherIntAnswer)(_ >= _))
+      case Gt => BooleanAnswer(combine[Boolean](otherIntAnswer)(_ > _))
+      case Ne => BooleanAnswer(combine[Boolean](otherIntAnswer)(_ != _))
+      case Eq => BooleanAnswer(combine[Boolean](otherIntAnswer)(_ == _))
       case _ => throw new UnsupportedOperationException(s"Unsupported operator: $operator")
     }
     case _ => throw new IllegalArgumentException(s"Can't perform operation: $this $operator $other")
@@ -29,5 +29,6 @@ case class IntAnswer(possibleValue: Option[Int]) extends Answer {
 
   def apply(operator: UnaryOperator): Answer = operator match {
     case Min => IntAnswer(possibleValue.map(-_))
+    case _ => throw new IllegalArgumentException(s"Can't perform operation: $operator $this")
   }
 }

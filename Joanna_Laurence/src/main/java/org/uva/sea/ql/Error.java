3481ef7aa1491e264f9c8ad10901764636575c49
lauwdude <l.saes@live.nl>
2018-02-22 17:52:28 +0100

    Update renaming in grammar Clean unused imports Forgot list init Update unit test generator

--- Joanna_Laurence/src/main/java/org/uva/sea/ql/Error.java
+++ Joanna_Laurence/src/main/java/org/uva/sea/ql/Error.java
@@ -1,9 +1,10 @@
 package org.uva.sea.ql;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Error {
-    private List<String> errors;
+    private List<String> errors = new ArrayList<>();
 
     public void addError(String error) {
         this.errors.add(error);

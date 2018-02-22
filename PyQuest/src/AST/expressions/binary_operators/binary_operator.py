1613c77bf9bc13205877a2fbbdfe653e7f46698f
bicker <bickerkards@tuta.io>
2018-02-22 13:32:59 +0100

    changes to expression nodes

---
+++ PyQuest/src/AST/expressions/binary_operators/binary_operator.py
@@ -0,0 +1,16 @@
+from src.AST.expressions.expression import ExpressionNode
+
+
+class BinaryOperatorNode(ExpressionNode):
+    def __init__(self, position, expression_type, left_expression, right_expression):
+        super(BinaryOperatorNode, self).__init__(position, expression_type)
+        self._left_expression = left_expression
+        self._right_expression = right_expression
+
+    @property
+    def left_expression(self):
+        return self._left_expression
+
+    @property
+    def right_expression(self):
+        return self._right_expression
---
+++ PyQuest/src/AST/expressions/binary_operators/or.py
@@ -0,0 +1,7 @@
+from src.AST.expressions.binary_operators.binary_operator import BinaryOperatorNode
+
+
+class OrOperatorNode(BinaryOperatorNode):
+    def __init__(self, position, expression_type, left_expression, right_expression):
+        super(OrOperatorNode, self).__init__(position, expression_type, left_expression, right_expression)
+

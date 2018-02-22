aed31f1b5785dcd21a8f2137e88f93bc9df13431
Jordy Bottelier <Jordy@Bottelier.org>
2018-02-22 10:19:13 +0100

    added comments to expression

--- Jordy_Dennis/AST/expressionnode.py
+++ Jordy_Dennis/AST/expressionnode.py
@@ -1,6 +1,14 @@
+# An expression can be a single variable. Or a combination of a variable, an operator (negation) and multiple other expressions
+
+# if there is only a single variable, it will always be in the LEFT node.
+# A NOT-expression is represented as a -1 in the 'op' node, and an expression in the right node.
+
+# Maybe we have to give each expression a type for the right and left node, we do this in order to make type checking very easy.
+# (so we can check, if type_left != type_right, exception)
+
 
 class ExpressionNode:
-    def __init(self, left, right, op):
+    def __init(self, left, right=None, op=None):
         self.left = left
         self.right = right
         self.op = op

8a2b1f6210f16c9b6fa3109845b681de5f5a67f7
Leó Gunnar vidisson <leo@tolvugerdi.is>
2018-02-22 10:57:58 +0100

    BinaryExpressionPlus added

---
+++ GunOli/src/main/java/ParseObjects/Expressions/BinaryExpressionPlus.java
@@ -0,0 +1,14 @@
+package ParseObjects.Expressions;
+
+public class BinaryExpressionPlus extends BinaryExpression {
+
+    @Override
+    public EvaluationType returnType() {
+        EvaluationType(Integer);
+    }
+
+    @Override
+    public Object evaluate() {
+        return null;
+    }
+}

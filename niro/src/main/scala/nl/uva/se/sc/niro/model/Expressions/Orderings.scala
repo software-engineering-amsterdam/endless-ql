166ec0dda561ccaca24e61cc1c692d495c8db8c7
rmathijn <rocco.mathijnandela@dhl.com>
2018-02-24 15:02:26 +0100

    Make IntAnswer an implicit type class of Orderings

--- niro/src/main/scala/nl/uva/se/sc/niro/model/Expressions/Orderings.scala
+++ niro/src/main/scala/nl/uva/se/sc/niro/model/Expressions/Orderings.scala
@@ -1,6 +1,6 @@
 package nl.uva.se.sc.niro.model.Expressions
 
-import nl.uva.se.sc.niro.model.Expressions.answers.BooleanAnswer
+import nl.uva.se.sc.niro.model.Expressions.answers.{ BooleanAnswer, IntAnswer }
 
 trait Orderings[SubType<:Answer] {
   def lt(x: SubType, y: SubType): BooleanAnswer
@@ -9,4 +9,20 @@ trait Orderings[SubType<:Answer] {
   def gt(x: SubType, y: SubType): BooleanAnswer
   def ne(x: SubType, y: SubType): BooleanAnswer
   def eq(x: SubType, y: SubType): BooleanAnswer
+
+  trait IntAnswerCanDoOrderings extends Orderings[IntAnswer] {
+    import IntAnswerCanDoOrderings._
+    def lt(x: IntAnswer, y: IntAnswer): BooleanAnswer = BooleanAnswer(combine(x, y)(_ < _))
+    def lte(x: IntAnswer, y: IntAnswer): BooleanAnswer = BooleanAnswer(combine(x, y)(_ <= _))
+    def gte(x: IntAnswer, y: IntAnswer): BooleanAnswer = BooleanAnswer(combine(x, y)(_ >= _))
+    def gt(x: IntAnswer, y: IntAnswer): BooleanAnswer = BooleanAnswer(combine(x, y)(_ > _))
+    def ne(x: IntAnswer, y: IntAnswer): BooleanAnswer = BooleanAnswer(combine(x, y)(_ != _))
+    def eq(x: IntAnswer, y: IntAnswer): BooleanAnswer = BooleanAnswer(combine(x, y)(_ == _))
+  }
+  object IntAnswerCanDoOrderings extends IntAnswerCanDoOrderings {
+    private def combine(x: IntAnswer, y: IntAnswer)(f: (Int, Int) => Boolean): Option[Boolean] = for {
+      thisValue <- x.possibleValue
+      thatValue <- y.possibleValue
+    } yield f(thisValue, thatValue)
+  }
 }

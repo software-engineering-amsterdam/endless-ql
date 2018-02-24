cd459f7152d8b895412fa0797a979541dbb0fcca
Unknown <s.vdh@live.nl>
2018-02-24 11:11:53 +0100

    Cleaned up files, improved test_gui.py
    test_gui.py now includes a bot that can click buttons in the gui.

--- Pythonistas/tests/test_gui.py
+++ Pythonistas/tests/test_gui.py
@@ -1,9 +1,11 @@
 from PyQt5.QtWidgets import *
-import sys
-from pytest import *
+from PyQt5 import QtCore
+import os,sys
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from gui import gui
+from pytestqt import qtbot
 
-def testfunc():
+def testfunc(qtbot):
     app = QApplication(sys.argv)
     screen = gui.InputWindow()
     screen.show()
@@ -13,16 +15,13 @@ def testfunc():
     file.close()
 
     screen.qlInput.setText("form Box1HouseOwning {\n hasSoldHouse: \"Did you sell a house in 2010?\" boolean\n hasBoughtHouse: \"Did you by a house in 2010?\" boolean\n hasMaintLoan: \"Did you enter a loan for maintenance/reconstruction?\" boolean\n}")
-    screen.parse()
-    screen.output.submit()
+    qtbot.mouseClick(screen.parsebutton, QtCore.Qt.LeftButton)
+    qtbot.mouseClick(screen.output.submitbutton, QtCore.Qt.LeftButton)
+    qtbot.mouseClick(screen.quitbutton, QtCore.Qt.LeftButton)
 
     file = open("output.txt")
     testtext = file.read()
     file.close()
+    os.remove("output.txt")
 
     assert testtext == '"Did you sell a house in 2010?"undefined\n"Did you by a house in 2010?"undefined\n"Did you enter a loan for maintenance/reconstruction?"undefined\n'
-
-    sys.exit()
-
-
-testfunc()

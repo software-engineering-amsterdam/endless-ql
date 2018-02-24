bf2531e0dffc05985a4e0975f45114769e6aa179
rmathijn <rocco.mathijnandela@dhl.com>
2018-02-24 17:03:59 +0100

    Make BooleanAnswer member of Logicals type class

--- niro/src/main/scala/nl/uva/se/sc/niro/model/Expressions/Logicals.scala
+++ niro/src/main/scala/nl/uva/se/sc/niro/model/Expressions/Logicals.scala
@@ -2,7 +2,28 @@ package nl.uva.se.sc.niro.model.Expressions
 
 import nl.uva.se.sc.niro.model.Expressions.answers.BooleanAnswer
 
-trait Logicals[SubType<: Answer] {
+object Logicals {
+  trait BooleanAnswerCanDoLogicals extends Logicals[BooleanAnswer] {
+    import BooleanAnswerCanDoLogicals._
+    def and(x: BooleanAnswer, y: BooleanAnswer): BooleanAnswer = BooleanAnswer(combine(x, y)(_ && _))
+    def or(x: BooleanAnswer, y: BooleanAnswer): BooleanAnswer = BooleanAnswer(combine(x, y)(_ || _))
+  }
+  implicit object BooleanAnswerCanDoLogicals extends BooleanAnswerCanDoLogicals {
+    private def combine(x: BooleanAnswer, y: BooleanAnswer)(f: (Boolean, Boolean) => Boolean): Option[Boolean] = for {
+      thisValue <- x.possibleValue
+      thatValue <- y.possibleValue
+    } yield f(thisValue, thatValue)
+  }
+}
+
+trait Logicals[SubType<:Answer] {
   def and(x: SubType, y: SubType): BooleanAnswer
   def or(x: SubType, y: SubType): BooleanAnswer
+
+  class Ops(left: SubType) {
+    def &&(right: SubType) = and(left, right)
+    def ||(right: SubType) = or(left, right)
+  }
+
+  implicit def mkLogicalsOp(left: SubType): Ops = new Ops(left)
 }

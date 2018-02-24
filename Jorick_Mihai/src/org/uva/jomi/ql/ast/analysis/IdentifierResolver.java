d55696fab111924f21855bdd23ca7ca4e39f64a3
Mihai Onofrei <mihai303@gmail.com>
2018-02-24 19:09:20 +0200

    Update identifier resolver and add error handler and identifier resolution test skeleton

--- Jorick_Mihai/src/org/uva/jomi/ql/ast/analysis/IdentifierResolver.java
+++ Jorick_Mihai/src/org/uva/jomi/ql/ast/analysis/IdentifierResolver.java
@@ -4,44 +4,44 @@ import java.util.List;
 
 import org.uva.jomi.ql.ast.expressions.*;
 import org.uva.jomi.ql.ast.statements.*;
+import org.uva.jomi.ql.error.ErrorHandler;
 
 public class IdentifierResolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
-	private int numberOfErrors = 0;
+	
 	public final IdentifierStack identifierStack;
+	private final ErrorHandler errorHandler;
 
-	public IdentifierResolver() {
-		identifierStack = new IdentifierStack();
+	public IdentifierResolver(boolean printErrors) {
+		this.identifierStack = new IdentifierStack();
+		this.errorHandler = new ErrorHandler(this.getClass().getSimpleName(), printErrors);
 	}
 
 	public void resolve(List<Stmt> statements) {
+		// Clear previous errors first
+		errorHandler.clearErros();
+		
 		for (Stmt statment : statements) {
 			statment.accept(this);
 		}
 	}
-
+	
 	public int getNumberOfErrors() {
-		return numberOfErrors;
+		return errorHandler.getNumberOfErrors();
 	}
-
-	public void incrementNumberOfErrors() {
-		this.numberOfErrors++;
+	
+	// This method was added for testing purposes
+	public String getErrorAtIndex(int index) {
+		return errorHandler.getErrorAtIndex(index);
 	}
 
 	/*
-	 * Add supplementary visit method for questions in order to add a new identifier
+	 * Add supplementary method in order to add the question identifier 
 	 * to the stack.
 	 */
 	public void resolveQuestionIdentifier(IdentifierExpr identifier) {
 		if (identifierStack.contains(identifier.getName())) {
-			// TODO - create an error handler
-			System.err.printf("[IdentifierResolver] line: %s, column: %s: Duplicated identifier: %s\n",
-						identifier.getLine(),
-						identifier.getColumn(),
-						identifier.getName());
-
-			// Increment the number of identifier resolution errors
-			incrementNumberOfErrors();
+			errorHandler.addError(identifier.getToken(), "Duplicated identifier");
 		} else {
 			// Add the identifier to the inner most scope map
 			identifierStack.add(identifier);
@@ -78,19 +78,30 @@ public class IdentifierResolver implements Expr.Visitor<Void>, Stmt.Visitor<Void
 
 	@Override
 	public Void visit(ComputedQuestionStmt stmt) {
+		
+		/*
+		 * Visit the expressions first in case the question identifier is used 
+		 * inside the expression.
+		 */
+		stmt.expression.accept(this);
+		
 		// Make  sure the question name has not been already declared
 		resolveQuestionIdentifier(stmt.getIdentifier());
-		stmt.expression.accept(this);
 		return null;
 	}
 
 	@Override
 	public Void visit(IfStmt stmt) {
+		stmt.expression.accept(this);
+		stmt.blockStmt.accept(this);
 		return null;
 	}
 
 	@Override
 	public Void visit(IfElseStmt stmt) {
+		stmt.expression.accept(this);
+		stmt.ifBlockStmt.accept(this);
+		stmt.elseBlockStmt.accept(this);
 		return null;
 	}
 
@@ -101,14 +112,7 @@ public class IdentifierResolver implements Expr.Visitor<Void>, Stmt.Visitor<Void
 			IdentifierExpr retrievedIndetifier = identifierStack.getIdentifier(identifier.getName());
 			identifier.updateAllFields(retrievedIndetifier);
 		} else {
-			// TODO - create an error handler
-			System.err.printf("[IdentifierResolver] line: %s, column: %s: Undefined identifier: %s\n",
-						identifier.getLine(),
-						identifier.getColumn(),
-						identifier.getName());
-
-			// Increment the number of identifier resolution errors
-			incrementNumberOfErrors();
+			errorHandler.addError(identifier.getToken(), "Undefined identifier");
 		}
 
 		return null;
@@ -121,94 +125,110 @@ public class IdentifierResolver implements Expr.Visitor<Void>, Stmt.Visitor<Void
 
 	@Override
 	public Void visit(BinaryExpr expr) {
+		expr.left.accept(this);
+		expr.right.accept(this);
 		return null;
 	}
 
 	@Override
 	public Void visit(GroupingExpr expr) {
+		expr.expression.accept(this);
 		return null;
 	}
 
 	@Override
 	public Void visit(UnaryExpr expr) {
+		expr.right.accept(this);
 		return null;
 	}
 
 	@Override
 	public Void visit(AdditionExpr expr) {
-		// TODO Auto-generated method stub
+		expr.left.accept(this);
+		expr.right.accept(this);
 		return null;
 	}
 
 	@Override
 	public Void visit(SubtractionExpr expr) {
-		// TODO Auto-generated method stub
+		expr.left.accept(this);
+		expr.right.accept(this);
 		return null;
 	}
 
 	@Override
 	public Void visit(MultiplicationExpr expr) {
-		// TODO Auto-generated method stub
+		expr.left.accept(this);
+		expr.right.accept(this);
 		return null;
 	}
 
 	@Override
 	public Void visit(DivisionExpr expr) {
-		// TODO Auto-generated method stub
+		expr.left.accept(this);
+		expr.right.accept(this);
 		return null;
 	}
 
 	@Override
 	public Void visit(LessThanExpr expr) {
-		// TODO Auto-generated method stub
+		expr.left.accept(this);
+		expr.right.accept(this);
 		return null;
 	}
 
 	@Override
 	public Void visit(LessThanOrEqualExpr expr) {
-		// TODO Auto-generated method stub
+		expr.left.accept(this);
+		expr.right.accept(this);
 		return null;
 	}
 
 	@Override
 	public Void visit(GreaterThanExpr expr) {
-		// TODO Auto-generated method stub
+		expr.left.accept(this);
+		expr.right.accept(this);
 		return null;
 	}
 
 	@Override
 	public Void visit(GreaterThanOrEqualExpr expr) {
-		// TODO Auto-generated method stub
+		expr.left.accept(this);
+		expr.right.accept(this);
 		return null;
 	}
 
 	@Override
 	public Void visit(NotEqualExpr expr) {
-		// TODO Auto-generated method stub
+		expr.left.accept(this);
+		expr.right.accept(this);
 		return null;
 	}
 
 	@Override
 	public Void visit(EqualExpr expr) {
-		// TODO Auto-generated method stub
+		expr.left.accept(this);
+		expr.right.accept(this);
 		return null;
 	}
 
 	@Override
 	public Void visit(AndExpr expr) {
-		// TODO Auto-generated method stub
+		expr.left.accept(this);
+		expr.right.accept(this);
 		return null;
 	}
 
 	@Override
 	public Void visit(OrExpr expr) {
-		// TODO Auto-generated method stub
+		expr.left.accept(this);
+		expr.right.accept(this);
 		return null;
 	}
 
 	@Override
 	public Void visit(UnaryNotExpr expr) {
-		// TODO Auto-generated method stub
+		expr.right.accept(this);
 		return null;
 	}
 }
---
+++ Jorick_Mihai/src/org/uva/jomi/ql/error/ErrorHandler.java
@@ -0,0 +1,71 @@
+package org.uva.jomi.ql.error;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.uva.jomi.ql.ast.QLToken;
+
+public class ErrorHandler {
+	
+	private class Error {
+		private final int line;
+		private final int column;
+		private final String message;
+		
+		public Error(int line, int column, String message) {
+			this.line = line;
+			this.column = column;
+			this.message = message;
+		}
+
+		public int getLine() {
+			return line;
+		}
+
+		public int getColumn() {
+			return column;
+		}
+
+		public String getMessage() {
+			return message;
+		}
+
+		@Override
+		public String toString() {
+			return String.format("[%s] line: %d, column: %d: %s", moduleName, getLine(), getColumn(), getMessage());
+		}
+	}
+	
+	private final String moduleName;
+	private List<Error> errorMessages;
+	private boolean printErrors;
+	
+	public ErrorHandler(String moduleName, boolean printErrors) {
+		this.moduleName = moduleName;
+		this.printErrors = printErrors;
+		errorMessages = new ArrayList<>();
+	}
+	
+	public int getNumberOfErrors() {
+		return errorMessages.size();
+	}
+
+	public void addError(QLToken token, String errorMessage) {
+		Error error = new Error(token.getLine(), token.getColumn(), errorMessage);
+		errorMessages.add(error);
+		if (printErrors)
+			System.err.println(error.toString());
+	}
+	
+	public void clearErros() {
+		errorMessages.clear();
+	}
+	
+	public String getErrorAtIndex(int index) {
+		if (index >= 0 && index < errorMessages.size()) {
+			return errorMessages.get(index).toString();
+		} else {
+			return String.format("No error is present at index %d", index);
+		}
+	}
+}

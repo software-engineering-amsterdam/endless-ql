ff7650c8a21ffda8e196c14fdd8cce62031c522e
Peter Takacs <hhike@outlook.com>
2018-02-24 19:07:02 +0100

    Adds QuestionRenderer

--- karra_takacs/src/main/java/qlviz/gui/renderer/JavafxQuestionBlockRenderer.java
+++ karra_takacs/src/main/java/qlviz/gui/renderer/JavafxQuestionBlockRenderer.java
@@ -1,5 +1,6 @@
 package qlviz.gui.renderer;
 
+import qlviz.model.Question;
 import qlviz.model.QuestionBlock;
 
 public class JavafxQuestionBlockRenderer implements QuestionBlockRenderer {
---
+++ karra_takacs/src/main/java/qlviz/gui/renderer/QuestionRenderer.java
@@ -0,0 +1,7 @@
+package qlviz.gui.renderer;
+
+import qlviz.model.Question;
+
+public interface QuestionRenderer {
+    void render(Question question);
+}

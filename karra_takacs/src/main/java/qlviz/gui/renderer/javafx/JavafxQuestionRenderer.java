ddfe381c4699965b172d8584835a80cf8fe63735
Peter Takacs <hhike@outlook.com>
2018-02-24 19:42:58 +0100

    Adds QuestionVisitor interface to QuestionRenderer

--- karra_takacs/src/main/java/qlviz/gui/renderer/javafx/JavafxQuestionRenderer.java
+++ karra_takacs/src/main/java/qlviz/gui/renderer/javafx/JavafxQuestionRenderer.java
@@ -4,9 +4,9 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import qlviz.gui.renderer.QuestionRenderer;
-import qlviz.model.question.Question;
+import qlviz.model.question.*;
 
-public class JavafxQuestionRenderer implements QuestionRenderer {
+public class JavafxQuestionRenderer implements QuestionRenderer, QuestionVisitor {
 
     private final Pane target;
 
@@ -16,11 +16,42 @@ public class JavafxQuestionRenderer implements QuestionRenderer {
 
     @Override
     public void render(Question question) {
-        Label label = new Label(question.getText());
+        question.accept(this);
+    }
+
+    @Override
+    public void visit(BooleanQuestion booleanQuestion) {
+
+    }
+
+    @Override
+    public void visit(DateQuestion dateQuestion) {
+
+    }
+
+    @Override
+    public void visit(DecimalQuestion decimalQuestion) {
+
+    }
+
+    @Override
+    public void visit(IntegerQuestion integerQuestion) {
+
+    }
+
+    @Override
+    public void visit(MoneyQuestion moneyQuestion) {
+
+    }
+
+    @Override
+    public void visit(StringQuestion stringQuestion) {
+        Label label = new Label(stringQuestion.getText());
         TextField textField = new TextField();
         textField.setMinWidth(50);
         textField.setPrefWidth(50);
         target.getChildren().add(label);
         target.getChildren().add(textField);
+
     }
 }

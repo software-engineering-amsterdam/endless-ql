cdb8cad9d341279f6c772c38d637d4cdf0087444
Peter Takacs <hhike@outlook.com>
2018-02-24 20:25:59 +0100

    Adds formviewmodel stub

--- karra_takacs/src/main/java/qlviz/gui/viewModel/FormViewModel.java
+++ karra_takacs/src/main/java/qlviz/gui/viewModel/FormViewModel.java
@@ -1,6 +1,35 @@
 package qlviz.gui.viewModel;
 
+import qlviz.gui.renderer.FormRenderer;
+import qlviz.model.Form;
+import qlviz.model.QuestionBlock;
+import qlviz.model.question.Question;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
 public interface FormViewModel extends QuestionObserver {
 
 }
 
+public class FormViewModelImpl implements FormViewModel {
+    private final Form model;
+    private final FormRenderer renderer;
+    private final List<QuestionBlockViewModel> questionBlockViewModels;
+
+    public FormViewModelImpl(Form model, FormRenderer renderer, Function<QuestionBlock, QuestionBlockViewModel> viewModelFactory) {
+        this.model = model;
+        this.renderer = renderer;
+        this.questionBlockViewModels = new ArrayList<>();
+
+        for (QuestionBlock block : model.getQuestions()) {
+            this.questionBlockViewModels.add(viewModelFactory.apply(block));
+        }
+    }
+
+    @Override
+    public void notifyValueChanged(QuestionViewModel source) {
+        renderer.render(this.model);
+    }
+}
---
+++ karra_takacs/src/main/java/qlviz/gui/viewModel/QuestionBlockViewModel.java
@@ -0,0 +1,4 @@
+package qlviz.gui.viewModel;
+
+public interface QuestionBlockViewModel {
+}

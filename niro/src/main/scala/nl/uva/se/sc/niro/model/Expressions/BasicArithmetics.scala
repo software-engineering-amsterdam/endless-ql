8d66805172e91d42909bf3116375c8bbb9b0b747
rmathijn <rocco.mathijnandela@dhl.com>
2018-02-24 14:46:49 +0100

    Make IntAnswer an implicit type class of BasicArithmetics

--- niro/src/main/scala/nl/uva/se/sc/niro/model/Expressions/BasicArithmetics.scala
+++ niro/src/main/scala/nl/uva/se/sc/niro/model/Expressions/BasicArithmetics.scala
@@ -1,9 +1,28 @@
 package nl.uva.se.sc.niro.model.Expressions
 
-trait BasicArithmetics[SubType<:Answer] {
-  def plus(x: SubType, y: SubType): SubType
-  def minus(x: SubType, y: SubType): SubType
-  def times(x: SubType, y: SubType): SubType
-  def div(x: SubType, y: SubType): SubType
-  def negate(x: SubType): SubType
+import nl.uva.se.sc.niro.model.Expressions.answers.IntAnswer
+
+object BasicArithmetics {
+  trait BasicArithmetics[SubType<:Answer] {
+    def plus(x: SubType, y: SubType): SubType
+    def minus(x: SubType, y: SubType): SubType
+    def times(x: SubType, y: SubType): SubType
+    def div(x: SubType, y: SubType): SubType
+    def negate(x: SubType): SubType
+  }
+
+  trait IntAnswerCanDoBasicArithmetics extends BasicArithmetics[IntAnswer] {
+    import IntAnswerCanDoBasicArithmetics._
+    def plus(x: IntAnswer, y: IntAnswer): IntAnswer = IntAnswer(combine(x, y)(_ + _))
+    def minus(x: IntAnswer, y: IntAnswer): IntAnswer = IntAnswer(combine(x, y)(_ - _))
+    def times(x: IntAnswer, y: IntAnswer): IntAnswer = IntAnswer(combine(x, y)(_ * _))
+    def div(x: IntAnswer, y: IntAnswer): IntAnswer = IntAnswer(combine(x, y)(_ / _))
+    def negate(x: IntAnswer) = IntAnswer(x.possibleValue.map(-_))
+  }
+  implicit object IntAnswerCanDoBasicArithmetics extends IntAnswerCanDoBasicArithmetics {
+    private def combine(x: IntAnswer, y: IntAnswer)(f: (Int, Int) => Int): Option[Int] = for {
+      thisValue <- x.possibleValue
+      thatValue <- y.possibleValue
+    } yield f(thisValue, thatValue)
+  }
 }

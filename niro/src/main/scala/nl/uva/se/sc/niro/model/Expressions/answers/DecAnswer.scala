e6bbc3825ad51b9476590eca6fe9079e4d677425
rmathijn <rocco.mathijnandela@dhl.com>
2018-02-24 16:29:18 +0100

    Make DecAnswer a member of Orderings

--- niro/src/main/scala/nl/uva/se/sc/niro/model/Expressions/answers/DecAnswer.scala
+++ niro/src/main/scala/nl/uva/se/sc/niro/model/Expressions/answers/DecAnswer.scala
@@ -1,14 +1,30 @@
 package nl.uva.se.sc.niro.model.Expressions.answers
 
 import nl.uva.se.sc.niro.model.Expressions.Answer
-import nl.uva.se.sc.niro.model.{ BinaryOperator, UnaryOperator }
+import nl.uva.se.sc.niro.model.Expressions.BasicArithmetics.DecAnswerCanDoBasicArithmetics._
+import nl.uva.se.sc.niro.model.Expressions.Orderings.DecAnswerCanDoOrderings._
+import nl.uva.se.sc.niro.model._
 
-final case class DecAnswer(possibleValue: Option[Double]) extends Answer {
-  def applyUnaryOperator(unaryOperator: UnaryOperator): Answer = ???
-  def applyBinaryOperator(binaryOperator: BinaryOperator, other: Answer): Answer = ???
+final case class DecAnswer(possibleValue: Option[BigDecimal]) extends Answer {
+  def applyBinaryOperator(operator: BinaryOperator, that: Answer): Answer = that match {
+    case that: DecAnswer => operator match {
+      case Add => this + that
+      case Sub => this - that
+      case Mul => this * that
+      case Div => this / that
+      case Lt => this < that
+      case LTe => this <= that
+      case GTe => this >= that
+      case Gt => this > that
+      case Ne => BooleanAnswer(this.possibleValue.flatMap(x => that.possibleValue.map(y => x != y)))
+      case Eq => BooleanAnswer(this.possibleValue.flatMap(x => that.possibleValue.map(y => x == y)))
+      case _ => throw new UnsupportedOperationException(s"Unsupported operator: $operator")
+    }
+    case _ => throw new IllegalArgumentException(s"Can't perform operation: $this $operator $that")
+  }
 
-  private def combine[R](other: DecAnswer)(f: (Double, Double) => R): Option[R] = for {
-    thisValue <- possibleValue
-    thatValue <- other.possibleValue
-  } yield f(thisValue, thatValue)
+  def applyUnaryOperator(operator: UnaryOperator): Answer = operator match {
+    case Min => DecAnswer(possibleValue.map(-_))
+    case _ => throw new IllegalArgumentException(s"Can't perform operation: $operator $this")
+  }
 }

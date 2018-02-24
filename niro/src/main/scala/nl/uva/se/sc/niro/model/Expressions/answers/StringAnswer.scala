a82a52f981d81f7a64ecac9c745eddf7326d1e1b
rmathijn <rocco.mathijnandela@dhl.com>
2018-02-24 16:17:06 +0100

    Get rid of orderings logic in StringAnswer

--- niro/src/main/scala/nl/uva/se/sc/niro/model/Expressions/answers/StringAnswer.scala
+++ niro/src/main/scala/nl/uva/se/sc/niro/model/Expressions/answers/StringAnswer.scala
@@ -1,21 +1,22 @@
 package nl.uva.se.sc.niro.model.Expressions.answers
 
 import nl.uva.se.sc.niro.model.Expressions.Answer
+import nl.uva.se.sc.niro.model.Expressions.Orderings.StringAnswerCanDoOrderings._
 import nl.uva.se.sc.niro.model._
 
 final case class StringAnswer(possibleValue: Option[String]) extends Answer {
 
-  def applyBinaryOperator(operator: BinaryOperator, other: Answer): Answer = other match {
-    case otherStringAnswer: StringAnswer => operator match {
-      case Lt => BooleanAnswer(combine[Boolean](otherStringAnswer)(_ < _))
-      case LTe => BooleanAnswer(combine[Boolean](otherStringAnswer)(_ <= _))
-      case GTe => BooleanAnswer(combine[Boolean](otherStringAnswer)(_ >= _))
-      case Gt => BooleanAnswer(combine[Boolean](otherStringAnswer)(_ > _))
-      case Ne => BooleanAnswer(combine[Boolean](otherStringAnswer)(_ != _))
-      case Eq => BooleanAnswer(combine[Boolean](otherStringAnswer)(_ == _))
+  def applyBinaryOperator(operator: BinaryOperator, that: Answer): Answer = that match {
+    case that: StringAnswer => operator match {
+      case Lt => this < that
+      case LTe => this <= that
+      case GTe => this >= that
+      case Gt => this > that
+      case Ne => BooleanAnswer(combine[Boolean](that)(_ != _))
+      case Eq => BooleanAnswer(combine[Boolean](that)(_ == _))
       case _ => throw new UnsupportedOperationException(s"Unsupported operator: $operator")
     }
-    case _ => throw new IllegalArgumentException(s"Can't perform operation: $this $operator $other")
+    case _ => throw new IllegalArgumentException(s"Can't perform operation: $this $operator $that")
   }
 
   def applyUnaryOperator(operator: UnaryOperator): Answer = throw new IllegalArgumentException(s"Can't perform operation: $operator $this")

bdb9a0ad57e616666aa92cc10dfecd6db5637d9e
Tim <tim.vonsee@me.com>
2018-02-25 20:31:57 +0100

    Added import QL file implementation

--- LDG_TV/SC-Java-LDG-TV/src/main/java/tool/ToolController.java
+++ LDG_TV/SC-Java-LDG-TV/src/main/java/tool/ToolController.java
@@ -1,15 +1,21 @@
 package tool;
 
+import domain.Utilities;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
+import java.io.*;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class ToolController implements Initializable {
@@ -41,7 +47,35 @@ public class ToolController implements Initializable {
      * @param event that kicked of the invocation
      */
     public void importQLFile(ActionEvent event) {
-        // TODO Import QL file
+        FileChooser fileChooser = getFileChooser();
+
+        File selectedFile = fileChooser.showOpenDialog(new Stage());
+
+        if (selectedFile == null) {
+            return;
+        }
+
+        Optional<String> qlText = Utilities.readFile(selectedFile.getAbsolutePath());
+
+        qlText.ifPresentOrElse(
+                text -> taSourceCode.setText(text),
+                () -> showAlertBox("Could not read file.")
+        );
     }
 
+    private void showAlertBox(String errorMessage){
+        Alert alert = new Alert(Alert.AlertType.ERROR, errorMessage);
+        alert.showAndWait();
+    }
+
+    private FileChooser getFileChooser(){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open QL File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Questionnaire Language File (*.ql)", "*.ql"),
+                new FileChooser.ExtensionFilter("All Files", "*.*")
+        );
+
+        return fileChooser;
+    }
 }

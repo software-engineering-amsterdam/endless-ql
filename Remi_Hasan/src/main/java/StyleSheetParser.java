da3070abd901002ed258338501a44756137a8557
hasan <hasanbilen38@gmail.com>
2018-02-27 17:31:43 +0100

    Added boilerplate QLS visitor classes.

---
+++ Remi_Hasan/src/main/java/StyleSheetParser.java
@@ -0,0 +1,35 @@
+import model.StyleSheet;
+import org.antlr.v4.gui.Trees;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import visitor.VistorStyleSheet;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class StyleSheetParser {
+
+    public static StyleSheet parseStyleSheet(InputStream stream) throws IOException, IllegalArgumentException, UnsupportedOperationException{
+        try{
+            // TODO
+            QLSLexer lexer = new QLSLexer(CharStreams.fromStream(stream));
+
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            QLSParser parser = new QLSParser(tokens);
+
+            // Walk it and attach our listener
+            VistorStyleSheet visitor = new VistorStyleSheet();
+            StyleSheet styleSheet = visitor.visit(parser.root());
+
+            // Debug
+            parser.reset();
+            Trees.inspect(parser.root(), parser);
+
+            return styleSheet;
+        } catch (Exception e){
+            // TODO improve exception
+            System.out.println("exception thrown during parsing");
+            throw new UnsupportedOperationException(e.getMessage());
+        }
+    }
+}
---
+++ Remi_Hasan/src/main/java/visitor/VistorStyleSheet.java
@@ -0,0 +1,8 @@
+package visitor;
+
+import antlr.QLBaseVisitor;
+import model.StyleSheet;
+
+public class VistorStyleSheet   extends QLBaseVisitor<StyleSheet> {
+    // TODO
+}

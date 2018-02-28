3d8d3abab1470c76e9e32555e27ba683049f161f
Meess <abcmaster8@hotmail.com>
2018-02-28 18:36:03 +0100

    REFACTOR node structure QLS

--- schneider_kalf/src/modules/styling/form/nodes/Nodes.ts
+++ schneider_kalf/src/modules/styling/form/nodes/Nodes.ts
@@ -1,34 +1,85 @@
-export class Stylesheet {
-  constructor(public identifier: string, public pages: Page[]) {
+import NodeVisitor from "../../../../form/nodes/visitors/NodeVisitor";
+
+class Stylesheet {
+  readonly name: string;
+  readonly children: FormChild[];
+
+  constructor(name: string, children: FormChild[]) {
+    this.name = name;
+    this.children = children;
   }
 }
 
-export class Page {
-  constructor(public identifier: string, public sections: Section[]) {
+interface FormChild {}
+
+// Style
+interface StyleAttribuut {
+  readonly name: string;
+  readonly value: string;
+}
+
+class BaseAttribuut implements StyleAttribuut {
+  readonly name: string;
+  readonly value: string;
+
+  constructor(name: string, value: string) {
+    this.name = name;
+    this.value = value;
   }
 }
 
-export class Section {
-  constructor(public name: string, public questions: Question[]) {
+class WidgetAttribuut implements StyleAttribuut {
+  readonly name: string;
+  readonly value: string;
+  readonly options: string[] | undefined;
+
+  constructor(name: string, value: string, options?: string[]) {
+    this.name = name;
+    this.value = value;
+    this.options = options;
   }
 }
 
-export class Widget {
-  constructor(public name: string, public options?: String[]) {
+// Page
+class Page implements FormChild {
+  readonly body: PageChild[];
+  readonly name: string;
+  constructor(name: string, body: PageChild[]) {
+    this.name = name;
+    this.body = body;
   }
 }
 
-export class Question {
-  constructor(public identifier: string, public widget: Widget) {
+interface PageChild {}
+
+// Section
+class Section implements PageChild {
+  readonly body: SectionChild[];
+  readonly name: string;
+  constructor(name: string, body: SectionChild[]) {
+    this.name = name;
+    this.body = body;
   }
 }
 
-export class Default {
-  constructor(public type: string, public styles: Style[]) {
+interface SectionChild {}
+
+// Question
+class Question implements SectionChild, PageChild {
+  readonly identifier: string;
+  readonly children: StyleAttribuut[];
+  constructor(identifier: string, children: StyleAttribuut[]) {
+    this.identifier = identifier;
+    this.children = children;
   }
 }
 
-export default class Style {
-  constructor(public name: string, public value: string) {
+// Default
+class Default implements SectionChild, PageChild, FormChild {
+  readonly type: string;
+  readonly children: StyleAttribuut[];
+  constructor(type: string, children: StyleAttribuut[]) {
+    this.type = type;
+    this.children = children;
   }
 }

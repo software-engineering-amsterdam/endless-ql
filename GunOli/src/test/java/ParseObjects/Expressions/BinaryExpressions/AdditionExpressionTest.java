fd0267ef0b49f5872c71cfad50896e0181e87dba
Leó Gunnar vidisson <leo@tolvugerdi.is>
2018-02-28 03:54:29 +0100

    Added more tests

--- GunOli/src/test/java/ParseObjects/Expressions/BinaryExpressions/AdditionExpressionTest.java
+++ GunOli/src/test/java/ParseObjects/Expressions/BinaryExpressions/AdditionExpressionTest.java
@@ -10,23 +10,27 @@ import static org.junit.Assert.*;
 
 public class AdditionExpressionTest {
 
+    private IntegerConstant a = new IntegerConstant( 3);
+    private DecimalConstant b = new DecimalConstant(5.0);
+    private AdditionExpression ae = new AdditionExpression(a, b);
+
     @Test
     public void returnType() {
-        //ToDo:Implement
+        assertEquals(Decimal, ae.returnType());
+
     }
 
     @Test
     public void evaluate() {
-        IntegerConstant a = new IntegerConstant( 3);
-        DecimalConstant b = new DecimalConstant(5.0);
-        AdditionExpression ae = new AdditionExpression(a, b);
+
         double value = ae.evaluate().getValue();
         assertEquals(8.00, value, 0);
     }
 
     @Test
     public void isArithmetic() {
-        //ToDo:Implement
+        assertEquals(true,ae.isArithmetic());
+
     }
 
 }

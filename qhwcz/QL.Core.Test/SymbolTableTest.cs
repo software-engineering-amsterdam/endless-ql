b49e7d40d3748bc7fca81dd3d333c3e5e0381ced
Quinten <samuel.heijn@gmail.com>
2018-02-28 11:41:55 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ qhwcz/QL.Core.Test/SymbolTableTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QL.Core.Test
+{
+    [TestClass]
+    public class SymbolTableTest
+    {
+        [TestMethod]
+        public void FormWithOneQuestion_OneSymbolDetectedCorrectly()
+        {
+            Assert.Fail();
+        }
+    }
+}

c8a14093642ecb76f7d7cc8816699beea900af48
Hector Stenger <github@orti.works>
2018-02-28 16:57:17 +0100

    feat(ast): add ast flattener so all non-terminals are extracted

--- henk/src/main/scala/ql/parsers/ql.scala
+++ henk/src/main/scala/ql/parsers/ql.scala
@@ -11,7 +11,7 @@ import java.net.URL
 import org.antlr.v4.runtime._
 
 object QlFormParser {
-  def getParser(input:String): ql.grammar.QlParser = {
+  def getParser(input: String): ql.grammar.QlParser = {
     val charStream = new ANTLRInputStream(input)
     val lexer = new QlLexer(charStream)
     val tokens = new CommonTokenStream(lexer)
@@ -19,7 +19,7 @@ object QlFormParser {
     return parser
   }
 
-  def parseFromURL(location:URL): ASTNode = {
+  def parseFromURL(location: URL): ASTNode = {
     val source = Source.fromURL(location)
     val sourcedLines = source.mkString
     source.close
@@ -29,5 +29,18 @@ object QlFormParser {
     val tree = parser.root()
 
     return visitor.visit(tree)
+  }
+
+  def flattenNT(node: ASTNode): List[ASTNode] = {
+    node match {
+      case ASTRoot(header, body) => List(header) ++ flattenNT(body)
+      case form @ ASTFormBody(statements) =>
+        List(form) ++ statements.map(flattenNT).flatten
+      case ifStmt @ ASTIfStatement(expr, statements) => {
+        List(ifStmt) ++ statements.map(flattenNT).flatten ++ flattenNT(expr)
+      }
+      case binOp @ ASTBinary(lhs, rhs, op) => List(binOp, lhs, rhs, op)
+      case other                           => List(other)
+    }
   }
 }

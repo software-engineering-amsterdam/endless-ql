e5484d6b3d197b82e9b1f61c47de67f94bd91a66
hasan <hasanbilen38@gmail.com>
2018-03-01 14:49:37 +0100

    Attempt to move answers to form instead of question itself.

--- Remi_Hasan/src/main/java/model/Form.java
+++ Remi_Hasan/src/main/java/model/Form.java
@@ -2,18 +2,31 @@ package model;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import expression.Expression;
+import expression.ExpressionFactory;
+import expression.ReturnType;
+import visitor.VisitorExpression;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Form {
 
     public final String identifier;
     public final List<Question> questions;
+    public final Map<String, Expression> answers;
 
     public Form(String identifier, List<Question> questions) {
         this.identifier = identifier;
         this.questions = questions;
+        this.answers = new HashMap<String, Expression>();
+    }
+
+    public void setAnswer(String name, ReturnType type, String value){
+        Expression expression = ExpressionFactory.createExpression(type, value);
+        answers.put(name, expression);
     }
 
     @Override

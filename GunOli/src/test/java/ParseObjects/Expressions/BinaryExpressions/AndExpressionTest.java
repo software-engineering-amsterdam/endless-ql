f017f860e3aec22f5e7992c974b98b5e026aa58c
Leó Gunnar vidisson <leo@tolvugerdi.is>
2018-03-01 12:37:41 +0100

    Quickcheck used for testing

--- GunOli/src/test/java/ParseObjects/Expressions/BinaryExpressions/AndExpressionTest.java
+++ GunOli/src/test/java/ParseObjects/Expressions/BinaryExpressions/AndExpressionTest.java
@@ -2,27 +2,27 @@ package ParseObjects.Expressions.BinaryExpressions;
 
 import ParseObjects.Expressions.ExpressionConstants.BooleanConstant;
 import static ParseObjects.Expressions.EvaluationType.Boolean;
+
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import sun.text.normalizer.NormalizerBase;
 
 import static org.junit.Assert.*;
 
+@RunWith(JUnitQuickcheck.class)
 public class AndExpressionTest {
 
-    private BooleanConstant a = new BooleanConstant(true) ;
-    private BooleanConstant b = new BooleanConstant( false);
+    @Property
+    public void evaluate(boolean left, boolean right){
 
-    private AndExpression trueToTest = new AndExpression(a, a);
-    private AndExpression falseToTest = new AndExpression(b, b);
-    private AndExpression trueAndFalseToTest = new AndExpression(a,b);
+        BooleanConstant leftConst = new BooleanConstant(left);
+        BooleanConstant rightConst = new BooleanConstant(right);
 
+        AndExpression toTest = new AndExpression(leftConst, rightConst);
+        assertEquals((left && right),toTest.evaluate().getValue());
 
-
-    @Test
-    public void evaluate() {
-        assertEquals(false, falseToTest.evaluate().getValue());
-        assertEquals(false,trueAndFalseToTest.evaluate().getValue());
-        assertEquals(true, trueToTest.evaluate().getValue());
     }
-
 
 }

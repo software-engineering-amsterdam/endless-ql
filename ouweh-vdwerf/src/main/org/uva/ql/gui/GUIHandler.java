f2e84fa28fea3331213c4c7fb9489adf7e131c59
Edwin <edwin_ou@hotmail.com>
2018-03-01 11:26:18 +0100

    Add default close operation

--- ouweh-vdwerf/src/main/org/uva/ql/gui/GUIHandler.java
+++ ouweh-vdwerf/src/main/org/uva/ql/gui/GUIHandler.java
@@ -8,6 +8,7 @@ public class GUIHandler {
 
     public GUIHandler(Form form){
         JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,500);
 
         frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));

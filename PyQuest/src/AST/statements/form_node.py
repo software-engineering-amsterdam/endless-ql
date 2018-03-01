6d4c9b8b5bd46445fc515a08dc7fb94077109522
MichielBoswijk <michiel.boswijk@gmail.com>
2018-03-01 11:55:31 +0100

    Changed semi-private variables to private variables

--- PyQuest/src/AST/statements/form_node.py
+++ PyQuest/src/AST/statements/form_node.py
@@ -4,8 +4,9 @@ from AST.statements.block_statement_node import BlockStatementNode
 class FormNode(BlockStatementNode):
     def __init__(self, position, block, label):
         super(FormNode, self).__init__(position, block)
-        self._label = label
+        self.__label = label
 
     @property
     def label(self):
-        return self._label
+        return self.__label
+

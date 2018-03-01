e6bb64b3e73711ca8c0dc345a53fed77077cf163
Leó Gunnar vidisson <leo@tolvugerdi.is>
2018-03-01 11:31:38 +0100

    Expression Tests added

--- GunOli/src/main/java/ParseObjects/Expressions/ExpressionConstants/MoneyConstant.java
+++ GunOli/src/main/java/ParseObjects/Expressions/ExpressionConstants/MoneyConstant.java
@@ -4,11 +4,10 @@ import ParseObjects.Expressions.Constant;
 import ParseObjects.Expressions.EvaluationType;
 
 public class MoneyConstant extends Constant<Double> {
-    public MoneyConstant(Double value){
-        super(value);
+    public MoneyConstant(Number value){
+        super(value.doubleValue());
     }
     public MoneyConstant(String value) { super(Double.parseDouble(value)); }
-
     public EvaluationType returnType(){
         return EvaluationType.Money;
     }
--- GunOli/src/test/java/ParseObjects/Expressions/BinaryExpressions/AdditionExpressionTest.java
+++ GunOli/src/test/java/ParseObjects/Expressions/BinaryExpressions/AdditionExpressionTest.java
@@ -3,34 +3,36 @@ package ParseObjects.Expressions.BinaryExpressions;
 import ParseObjects.Expressions.ExpressionConstants.DecimalConstant;
 import ParseObjects.Expressions.ExpressionConstants.IntegerConstant;
 
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static ParseObjects.Expressions.EvaluationType.Decimal;
 import static org.junit.Assert.*;
 
+@RunWith(JUnitQuickcheck.class)
 public class AdditionExpressionTest {
 
     private IntegerConstant a = new IntegerConstant( 3);
     private DecimalConstant b = new DecimalConstant(5.0);
-    private AdditionExpression ae = new AdditionExpression(a, b);
+    private DecimalConstant c = new DecimalConstant(-5.0);
+    private AdditionExpression positiveTest = new AdditionExpression(a, b);
+    private AdditionExpression negativeTest = new AdditionExpression(a, c);
 
-    @Test
-    public void returnType() {
-        assertEquals(Decimal, ae.returnType());
-
-    }
-
-    @Test
+    /*@Test
     public void evaluate() {
 
-        double value = ae.evaluate().getValue();
-        assertEquals(8.00, value, 0);
+        double posValue = positiveTest.evaluate().getValue();
+        double posEXP = 8.00;
+        double negValue = negativeTest.evaluate().getValue();
+        double negEXP = -2.00;
+
+        assertEquals(negEXP, negValue, 0);
+        assertEquals(posEXP, posValue, 0);
+    }*/
+    @Property
+    public void evaluate() {
+
     }
-
-    @Test
-    public void isArithmetic() {
-        assertEquals(true,ae.isArithmetic());
-
-    }
-
 }

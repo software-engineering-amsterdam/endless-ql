4e7908da5a1446a6bdfb64a65a61285782d57aa4
Quinten <samuel.heijn@gmail.com>
2018-03-02 11:18:59 +0100

    Replace SymbolType with QLType
    - Remove symbolType.
    - Add QLTypes to the AST and the symbol table.
    - Replace all old type representations in tests with QLTypes.

--- qhwcz/QL.Core.Test/Scopes/ScopeTreeTests.cs
+++ qhwcz/QL.Core.Test/Scopes/ScopeTreeTests.cs
@@ -2,6 +2,7 @@
 using QL.Core.Api;
 using QL.Core.Scopes;
 using QL.Core.Symbols;
+using QL.Core.Types;
 
 namespace QL.Core.Test.Scopes
 {
@@ -28,23 +29,23 @@ namespace QL.Core.Test.Scopes
             Scope scope = scopeExtractor.ScopeTree;
             Assert.AreEqual(1, scope.Variables.Count);
             Assert.AreEqual("whatIsMeaning", scope.Variables[0].Name);
-            Assert.AreEqual(SymbolType.Money, scope.Variables[0].Type);
+            Assert.AreEqual(QLType.Money, scope.Variables[0].Type);
             Assert.AreEqual(1, scope.References.Count);
             Assert.AreEqual("whatIsMeaning", scope.References[0].Name);
-            Assert.AreEqual(SymbolType.Money, scope.References[0].Type);
+            Assert.AreEqual(QLType.Money, scope.References[0].Type);
             Assert.AreEqual(1, scope.Childeren.Count);
             scope = scope.Childeren[0];
             Assert.AreEqual(1, scope.Variables.Count);
             Assert.AreEqual("hasSoldHouse", scope.Variables[0].Name);
-            Assert.AreEqual(SymbolType.Boolean, scope.Variables[0].Type);
+            Assert.AreEqual(QLType.Boolean, scope.Variables[0].Type);
             Assert.AreEqual(1, scope.References.Count);
             Assert.AreEqual("whatIsMeaning", scope.References[0].Name);
-            Assert.AreEqual(SymbolType.Money, scope.References[0].Type);
+            Assert.AreEqual(QLType.Money, scope.References[0].Type);
             Assert.AreEqual(1, scope.Childeren.Count);
             scope = scope.Childeren[0];
             Assert.AreEqual(1, scope.Variables.Count);
             Assert.AreEqual("dayToday", scope.Variables[0].Name);
-            Assert.AreEqual(SymbolType.Date, scope.Variables[0].Type);
+            Assert.AreEqual(QLType.Date, scope.Variables[0].Type);
             Assert.AreEqual(0, scope.References.Count);
             Assert.AreEqual(0, scope.Childeren.Count);
         }

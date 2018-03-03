efed1016d909ca252e1a9e551fd4f0e399efb135
Deepa Karra <deepa.karra@student.uva.nl>
2018-03-03 14:05:39 +0100

    Allows only numeric values in the text fields

--- karra_takacs/src/main/java/qlviz/gui/renderer/javafx/JavafxQuestionRenderer.java
+++ karra_takacs/src/main/java/qlviz/gui/renderer/javafx/JavafxQuestionRenderer.java
@@ -13,82 +13,80 @@ import qlviz.model.question.*;
 import java.math.BigDecimal;
 import java.text.NumberFormat;
 
-
 public class JavafxQuestionRenderer implements QuestionRenderer, QuestionViewModelVisitor {
 
-    private final Pane target;
+	private final Pane target;
 
-    public JavafxQuestionRenderer(Pane target) {
-        this.target = target;
-    }
+	public JavafxQuestionRenderer(Pane target) {
+		this.target = target;
+	}
 
-    @Override
-    public void visit(BooleanQuestionViewModel booleanQuestion) {
-        Label label = new Label(booleanQuestion.getText());
-        CheckBox checkBox = new CheckBox();
-        checkBox.selectedProperty().bindBidirectional(booleanQuestion.valueProperty());
-        target.getChildren().add(label);
-        target.getChildren().add(checkBox);
+	@Override
+	public void visit(BooleanQuestionViewModel booleanQuestion) {
+		Label label = new Label(booleanQuestion.getText());
+		CheckBox checkBox = new CheckBox();
+		checkBox.selectedProperty().bindBidirectional(booleanQuestion.valueProperty());
+		target.getChildren().add(label);
+		target.getChildren().add(checkBox);
 
-    }
+	}
 
-    @Override
-    public void visit(DateQuestionViewModel dateQuestion) {
+	@Override
+	public void visit(DateQuestionViewModel dateQuestion) {
 
-    }
+	}
 
-    @Override
-    public void visit(DecimalQuestionViewModel decimalQuestion) {
+	@Override
+	public void visit(DecimalQuestionViewModel decimalQuestion) {
 
-    }
+	}
 
-    @Override
-    public void visit(IntegerQuestionViewModel integerQuestion) {
+	@Override
+	public void visit(IntegerQuestionViewModel integerQuestion) {
 
-    }
+	}
 
-    @Override
-    public void visit(MoneyQuestionViewModel moneyQuestion) {
-        Label label = new Label(moneyQuestion.getText());
-        TextField textField = new TextField();
-        textField.setMinWidth(50);
-        textField.setPrefWidth(50);
-        textField.textProperty().bindBidirectional(moneyQuestion.valueProperty(), new StringConverter<BigDecimal>() {
-            @Override
-            public String toString(BigDecimal object) {
-                return object.toString();
-            }
+	@Override
+	public void visit(MoneyQuestionViewModel moneyQuestion) {
+		Label label = new Label(moneyQuestion.getText());
+		TextField textField = new TextField();
+		textField.setMinWidth(50);
+		textField.setPrefWidth(50);
+		textField.textProperty().bindBidirectional(moneyQuestion.valueProperty(), new StringConverter<BigDecimal>() {
+			@Override
+			public String toString(BigDecimal object) {
+				return object.toString();
+			}
 
-            @Override
-            public BigDecimal fromString(String string) {
-                try
-                {
-                    return new BigDecimal(string);
-                }
-                catch (NumberFormatException e)
-                {
-                    return BigDecimal.ZERO;
-                }
-            }
-        });
-        target.getChildren().add(label);
-        target.getChildren().add(textField);
+			@Override
+			public BigDecimal fromString(String string) {
+				try {
+					return new BigDecimal(string);
+				} catch (NumberFormatException e) {
+					textField.setText("");
+					return BigDecimal.ZERO;
+				}
+			}
+		});
 
-    }
+		target.getChildren().add(label);
+		target.getChildren().add(textField);
 
-    @Override
-    public void visit(StringQuestionViewModel stringQuestion) {
-        Label label = new Label(stringQuestion.getText());
-        TextField textField = new TextField();
-        textField.setMinWidth(50);
-        textField.setPrefWidth(50);
-        target.getChildren().add(label);
-        target.getChildren().add(textField);
+	}
 
-    }
+	@Override
+	public void visit(StringQuestionViewModel stringQuestion) {
+		Label label = new Label(stringQuestion.getText());
+		TextField textField = new TextField();
+		textField.setMinWidth(50);
+		textField.setPrefWidth(50);
+		target.getChildren().add(label);
+		target.getChildren().add(textField);
 
-    @Override
-    public void render(QuestionViewModel question) {
-       question.accept(this);
-    }
+	}
+
+	@Override
+	public void render(QuestionViewModel question) {
+		question.accept(this);
+	}
 }

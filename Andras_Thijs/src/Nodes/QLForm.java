50fdd4a45ddaa81cd0b4cc4bbf82d4052f9312c9
AHerczeg <h.herczeg.andras@gmail.com>
2018-03-03 01:05:09 +0100

    Addde Condition class and refractored QLForm

--- Andras_Thijs/src/Nodes/QLForm.java
+++ Andras_Thijs/src/Nodes/QLForm.java
@@ -5,19 +5,22 @@ import java.util.List;
 public class QLForm {
     private String name;
     private List<Question> questions;
+    private List<Condition> conditions;
+
+
+    public QLForm(String name){
+        this.name = name;
+    }
 
     public QLForm(String name, List<Question> questions) {
         this.name = name;
         this.questions = questions;
     }
 
-    public QLForm(String name, Question question) {
+    public QLForm(String name, List<Question> questions, List<Condition> conditions) {
         this.name = name;
-        this.questions.add(question);
-    }
-
-    public QLForm(String name){
-        this.name = name;
+        this.questions = questions;
+        this.conditions = conditions;
     }
 
     public String getName() {

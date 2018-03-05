6e994d98c4d6cd91d66930de11ed0cf927b5393e
Toine Khonraad <a.khonraad@khonraad.nl>
2018-03-06 00:45:59 +0100

    Protect Type to changes in grammar.

--- Rasha_Toine/Toine/src/main/java/nl/khonraad/domain/Type.java
+++ Rasha_Toine/Toine/src/main/java/nl/khonraad/domain/Type.java
@@ -6,16 +6,20 @@ public enum Type {
 
 	private String text;
 
-	Type(String text) {
+	private Type(String text) {
 		this.text = text;
 	}
 
 	public static Type fromString(String text) {
+		
 		for (Type type : Type.values()) {
+		
 			if (type.text.equalsIgnoreCase(text)) {
 				return type;
 			}
 		}
-		return null;
+		
+		throw new RuntimeException(
+				"CHeck your grammar. Do not know how to instantiate a Type from \"" + text + "\"" );
 	}
 }

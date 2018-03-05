33d6feef19c94ef0c820448ab264c08c527a1a22
Leó Gunnar vidisson <leo@tolvugerdi.is>
2018-03-05 14:17:27 +0100

    JavaFX button handler added

--- GunOli/src/main/java/Application/Main.java
+++ GunOli/src/main/java/Application/Main.java
@@ -1,25 +1,10 @@
 package Application;
 
-import GUI.Handler;
-import com.sun.corba.se.pept.transport.EventHandler;
+import GUI.QLUserInterface;
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.FileChooser;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
-
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.io.File;
-
-
-import ParseObjects.Question;
-import ParseObjects.Condition;
-import ParseObjects.Form;
 
 
 public class Main extends Application{
@@ -27,8 +12,10 @@ public class Main extends Application{
 
     @Override
     public void start(Stage stage){
-        Handler handler = new Handler(stage);
+        stage.setTitle("QL displayer");
+        QLUserInterface qlGui = new QLUserInterface(stage);
     }
+    
 
     public static void main(String[] args) {
         launch(args);

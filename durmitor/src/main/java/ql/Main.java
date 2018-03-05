c4ab88121a2b82b9f89c112e24fb81402861e77e
ighmelene <ighmelene@ighmelenes-air.home>
2018-03-05 01:00:03 +0100

    only run type checker if there antlr found  no syntax errors

--- durmitor/src/main/java/ql/Main.java
+++ durmitor/src/main/java/ql/Main.java
@@ -13,38 +13,38 @@ public class Main {
 
 
         String filePath     = (args.length == 0)? "resources/default.ql" : args[0];
-        MessageBag errors     = new MessageBag();
+        MessageBag errors   = new MessageBag();
         QL ql               = new QL(filePath, errors);
         Form form           = null;
         
         try {
-            form = ql.getForm();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        if(errors.isEmpty())
-        {
-            // Visit and TypeCheck the AST
-            TypeChecker checker = new TypeChecker(form);
-            checker.checkForm();
-            checker.getWarnings().print();
             
-            if(checker.hasErrors())
+            form = ql.getForm();
+            
+            if(errors.isEmpty())
             {
-                checker.getErrors().print();
-            }
-            else
-            {
-                // Visit and build GUI from AST
-                GUI gui = new GUI();
-                form.getBlock().accept(new ASTtoGUI(gui));
+                // Visit and TypeCheck the AST
+                TypeChecker checker = new TypeChecker(form);
+                checker.checkForm();
+                checker.getWarnings().print();
                 
-                // Add Action/DocumentListeners to GUI.
+                if(checker.hasErrors())
+                {
+                    checker.getErrors().print();
+                }
+                else
+                {
+                    // Visit and build GUI from AST
+                    GUI gui = new GUI();
+                    form.getBlock().accept(new ASTtoGUI(gui));
+                    
+                    // Add Action/DocumentListeners to GUI.
+                }
+            } else {
+                errors.print();
             }
-        }
-        else
-        {
+        } catch (Exception e) {
+            //e.printStackTrace();
             errors.print();
         }
     }

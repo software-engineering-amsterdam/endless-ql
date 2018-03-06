804debd8713c9910a687d2eda43218b3ea3047a1
Toine Khonraad <a.khonraad@khonraad.nl>
2018-03-06 01:07:28 +0100

    Make undefined value explicit.

--- Rasha_Toine/Toine/src/main/java/nl/khonraad/domain/ComputedQuestion.java
+++ Rasha_Toine/Toine/src/main/java/nl/khonraad/domain/ComputedQuestion.java
@@ -5,9 +5,13 @@ public class ComputedQuestion extends Value {
 	String identifier;
 	String label;
 
+	// An undefined boolean will therefore be considered False, a date as EPOCH.
+	
+	private static int UNDEFINED_VALUE = 0;   
+	
 	public ComputedQuestion(String identifier, String label, Type type) {
 		
-		super(type, 0);
+		super(type, UNDEFINED_VALUE);
 		
 		this.identifier = identifier;
 		this.label = label;

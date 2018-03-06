9b2eb4195f1482e8d9e8c1f778c18ce438042660
Peter Takacs <hhike@outlook.com>
2018-03-06 20:07:36 +0100

    Adds page visitor

--- karra_takacs/src/main/java/qlviz/model/style/Page.java
+++ karra_takacs/src/main/java/qlviz/model/style/Page.java
@@ -1,5 +1,22 @@
 package qlviz.model.style;
 
+import java.util.List;
+
 public class Page {
 
+    private final String name;
+    private final List<Section> sections;
+
+    public Page(String name, List<Section> sections) {
+        this.name = name;
+        this.sections = sections;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Section> getSections() {
+        return sections;
+    }
 }

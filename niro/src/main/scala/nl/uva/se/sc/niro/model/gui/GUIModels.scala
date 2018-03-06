a1461591e32d05f20c8e9bde9d43956f37fceb1a
Nico Tromp <nico.tromp@gmail.com>
2018-03-06 21:43:57 +0100

    Add new GUI model classes.

---
+++ niro/src/main/scala/nl/uva/se/sc/niro/model/gui/GUIModels.scala
@@ -0,0 +1,3 @@
+package nl.uva.se.sc.niro.model.gui
+
+class GUIModels(name: String, questions: Seq[QuestionInfo]) {}
---
+++ niro/src/main/scala/nl/uva/se/sc/niro/model/gui/ModelConverter.scala
@@ -0,0 +1,22 @@
+package nl.uva.se.sc.niro.model.gui
+
+import nl.uva.se.sc.niro.model.Expressions.Expression
+import nl.uva.se.sc.niro.model.Expressions.answers.BooleanAnswer
+import nl.uva.se.sc.niro.model.{ Conditional, QLForm, Question, Statement }
+import nl.uva.se.sc.niro.util.StringUtil
+
+object ModelConverter {
+  def convert(form: QLForm): GUIModels = {
+    new GUIModels(StringUtil.splitCamelCase(form.formName), convert(BooleanAnswer(true), form.statements))
+  }
+
+  def convert(visible: Expression, statements: Seq[Statement]): Seq[QuestionInfo] = {
+    statements.flatMap(statement =>
+      statement match {
+        case question: Question       => Seq(QuestionInfo(visible, question))
+        case conditional: Conditional => convert(conditional.predicate, conditional.thenStatements)
+        case _                        => Seq.empty
+    })
+  }
+
+}

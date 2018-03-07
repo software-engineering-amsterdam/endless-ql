c6032f76f009976842fc67f6aaf3473be090776a
Peter Takacs <hhike@outlook.com>
2018-03-07 10:16:52 +0100

    Adds propertySetting class

--- karra_takacs/src/main/java/qlviz/model/style/DefaultWidgetDeclaration.java
+++ karra_takacs/src/main/java/qlviz/model/style/DefaultWidgetDeclaration.java
@@ -1,4 +1,22 @@
 package qlviz.model.style;
 
+import java.util.List;
+
 public class DefaultWidgetDeclaration {
+
+    private final List<PropertySetting> propertySettings;
+    private final Widget widget;
+
+    public DefaultWidgetDeclaration(List<PropertySetting> propertySettings, Widget widget) {
+        this.propertySettings = propertySettings;
+        this.widget = widget;
+    }
+
+    public List<PropertySetting> getPropertySettings() {
+        return propertySettings;
+    }
+
+    public Widget getWidget() {
+        return widget;
+    }
 }

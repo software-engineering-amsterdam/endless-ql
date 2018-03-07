53e029ad6aec580cafe3009a1d83d996d7c69326
lauwdude <l.saes@live.nl>
2018-03-07 11:57:08 +0100

    Double define is only allowed in if-else statements

--- Joanna_Laurence/src/main/java/org/uva/sea/ql/staticAnalysis/LinkAndCheckUsageVariable.java
+++ Joanna_Laurence/src/main/java/org/uva/sea/ql/staticAnalysis/LinkAndCheckUsageVariable.java
@@ -1,14 +1,14 @@
 package org.uva.sea.ql.staticAnalysis;
 
 import org.uva.sea.ql.dataObject.MessageTypes;
-import org.uva.sea.ql.parser.elements.ASTNode;
-import org.uva.sea.ql.parser.elements.Form;
-import org.uva.sea.ql.parser.elements.Question;
+import org.uva.sea.ql.parser.elements.*;
 import org.uva.sea.ql.parser.elements.types.Variable;
 import org.uva.sea.ql.staticAnalysis.helpers.Messages;
 import org.uva.sea.ql.visitor.BaseASTVisitor;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -20,19 +20,19 @@ public class LinkAndCheckUsageVariable extends BaseASTVisitor implements IStatic
     /**
      * Contain what questions is related to what variable
      */
-    private Map<String, Question> variableMap = new HashMap<String, Question>();
+    private Map<String, Question> variableMap = new HashMap<>();
 
     /**
-     * Messages that occur during this process
+     *
      */
-    private Messages errors = new Messages(MessageTypes.ERROR);
+    private Messages messages = new Messages(MessageTypes.ERROR);
 
     /**
      * @param error Error that occur
      * @param node  The node that caused the error
      */
     private void error(String error, ASTNode node) {
-        this.errors.addMessage(error + " on line:  " + node.getLine() + " column: " + node.getColumn());
+        this.messages.addMessage(error + " on line:  " + node.getLine() + " column: " + node.getColumn());
     }
 
     /**
@@ -42,10 +42,10 @@ public class LinkAndCheckUsageVariable extends BaseASTVisitor implements IStatic
      * @return If an error occurred
      */
     public Messages doCheck(Form node) {
-        this.errors.clear();
+        this.messages.clear();
 
         node.accept(this);
-        return this.errors;
+        return this.messages;
     }
 
     /**
@@ -76,11 +76,68 @@ public class LinkAndCheckUsageVariable extends BaseASTVisitor implements IStatic
     @Override
     public Void visit(Question node) {
         String variableName = node.getVariable().getVariableName();
+        if(variableMap.containsKey(variableName)) {
+            this.error("Question already exists", node);
+            return null;
+        }
+
+        //Add new question to the lookup
         variableMap.put(variableName, node);
 
         //Visit all siblings
         super.visit(node);
 
         return null;
+    }
+
+    @Override
+    public Object visit(IfStatement node) {
+        node.getExpression().accept(this);
+
+        HashMap<String, Question> baseMap = new HashMap<>(this.variableMap);
+
+        //It is allowed to have duplicate elements in the then and else. So run both with the base map
+        HashMap<String, Question> thenMap = visitStatementsWithVariableMap(baseMap, node.getThen());
+        HashMap<String, Question> elseMap = visitStatementsWithVariableMap(baseMap, node.getOtherwise());
+
+        this.variableMap = this.combineVariableMap(baseMap, thenMap, elseMap);
+
+        return null;
+
+    }
+
+    /**
+     * Visits statements with a base map
+     * @param baseMap The base map
+     * @param statementsToCheck Statements to check
+     * @return A new HashSet that contains all the mappings from base and statements
+     */
+    private HashMap<String, Question> visitStatementsWithVariableMap(final HashMap<String, Question> baseMap, Statements statementsToCheck) {
+
+        HashMap<String, Question> result = new HashMap<>();
+        if (statementsToCheck == null)
+            return result;
+
+        this.variableMap = new HashMap<>(baseMap);
+        statementsToCheck.accept(this);
+        result.putAll(this.variableMap);
+        return result;
+    }
+
+    /**
+     * Combines variable map
+     * @param maps Variable maps
+     * @return New map with all elements combined
+     */
+    private Map<String, Question> combineVariableMap(Map<String, Question>... maps) {
+        if(maps.length == 0)
+            return new HashMap<>();
+
+        Map<String, Question> result = new HashMap<>();
+        for(Map<String, Question> map : maps) {
+            result.putAll(map);
+        }
+
+        return result;
     }
 }

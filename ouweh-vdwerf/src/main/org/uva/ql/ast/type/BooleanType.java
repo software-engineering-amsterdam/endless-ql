7e76a05b1365c0e5aed8f8e8cf6eb5989c37d036
Edwin <edwin_ou@hotmail.com>
2018-03-07 11:45:02 +0100

    Add mechanism to compare types for typechecking

--- ouweh-vdwerf/src/main/org/uva/ql/ast/type/BooleanType.java
+++ ouweh-vdwerf/src/main/org/uva/ql/ast/type/BooleanType.java
@@ -5,6 +5,11 @@ import org.uva.ql.visitor.TypeVisitor;
 public class BooleanType extends Type {
 
     @Override
+    public boolean isCompatible(Type type) {
+        return "BooleanType".equals(type.toString());
+    }
+
+    @Override
     public <T, C> T accept(TypeVisitor<T, C> visitor, C context) {
         return visitor.visit(this, context);
     }

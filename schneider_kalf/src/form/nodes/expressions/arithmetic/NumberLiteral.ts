421355962f548040d39c1d4fa20217a32365ef49
Simon Schneider <simon.sch@outlook.com>
2018-03-07 12:43:42 +0100

    TASK Add fromString method to number literal

--- schneider_kalf/src/form/nodes/expressions/arithmetic/NumberLiteral.ts
+++ schneider_kalf/src/form/nodes/expressions/arithmetic/NumberLiteral.ts
@@ -1,11 +1,30 @@
 import ExpressionVisitor from "../../visitors/ExpressionVisitor";
 import Expression from "../Expression";
+import { FieldType } from "../../../FieldType";
 
 export default class NumberLiteral implements Expression {
+  private type: FieldType;
   private value: number;
 
-  constructor(value: number) {
+  static fromString(text: string) {
+    let type: FieldType = FieldType.Float;
+    let value: number = parseFloat(text);
+
+    if (text.indexOf('.') === -1) {
+      type = FieldType.Integer;
+      value = parseInt(text, 10);
+    }
+
+    return new NumberLiteral(value, type);
+  }
+
+  constructor(value: number, type?: FieldType) {
+    if (!type) {
+      type = FieldType.Float;
+    }
+
     this.value = value;
+    this.type = type;
   }
 
   accept(visitor: ExpressionVisitor): any {
@@ -15,4 +34,5 @@ export default class NumberLiteral implements Expression {
   getValue() {
     return this.value;
   }
+
 }

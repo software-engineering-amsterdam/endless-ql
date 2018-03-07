9a88e3557a2e3d65556fb559712d252527c56172
Jovan Maric <jovanmaric777@gmail.com>
2018-03-08 00:19:31 +0100

    feat(window): add primary window and basic components

---
+++ henk/src/main/scala/view/window.scala
@@ -0,0 +1,59 @@
+import scala.swing._
+
+class CodeSection extends TextArea {
+  lineWrap = true;
+  wordWrap = true;
+  tabSize = 2;
+}
+
+class LeftPanel extends BoxPanel(Orientation.Vertical) {
+  border = Swing.EmptyBorder(10, 10, 10, 10)
+
+  val codeSection = new CodeSection
+  contents += new ScrollPane(codeSection)
+  contents += Swing.VStrut(10)
+
+  contents += new BoxPanel(Orientation.Horizontal) {
+    contents += Swing.HGlue
+    contents += Button("Generate") { println(codeSection.text) }
+  }
+}
+
+class RightPanel extends BoxPanel(Orientation.Vertical) {
+  border = Swing.EmptyBorder(10, 10, 10, 10)
+
+  contents += new BoxPanel(Orientation.Horizontal) {
+    contents += Swing.HGlue
+    contents += Button("Report") { println("Report") }
+  }
+
+  List("Did you sell a house in 2010", "Did you sell a house in 2010", "Did you enter a loan for maintenance/reconstruction?").map(
+    labels =>
+      this.contents += new BoxPanel(Orientation.Horizontal) {
+        contents += Swing.HGlue
+        contents += new Label(labels ++ "  ")
+        contents += new CheckBox("Yes")
+        contents += Swing.HGlue
+      }
+    )
+
+  // for (e <- contents)
+  //   e.xLayoutAlignment = 1.0
+}
+
+class Window extends MainFrame {
+  title = "Henk GUI"
+
+  contents = new GridPanel(1,1) {
+    contents += new LeftPanel
+    contents += new RightPanel
+  }
+}
+
+// TODO: Main trigger, move to other file
+object GuiProgramOne {
+  def main(args: Array[String]) {
+    val mainWindow = new Window
+    mainWindow.visible = true
+  }
+}

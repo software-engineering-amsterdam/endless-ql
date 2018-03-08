1e1ceb3844797bb6350a102ef097269c0f98628e
bicker <bickerkards@tuta.io>
2018-03-08 11:47:21 +0100

    Hide questions of which the if condition is not satisfied in the GUI

--- PyQuest/src/render/question.py
+++ PyQuest/src/render/question.py
@@ -1,14 +1,16 @@
 from PyQt5.QtWidgets import QLabel
+from visitors.expression_evaluator import ExpressionEvaluator
 
 
 class Question:
-    def __init__(self, label, identifier, answer_type, answer, show):
+    def __init__(self, label, identifier, answer_type, answer, show_condition):
         self.__label = label
         self.__identifier = identifier
         self.__answer_type = answer_type
         self.__answer = answer
-        self.__show = show
+        self.__show_condition = show_condition
         self.__widget = None
+        self.__widget_label = None
 
     @property
     def label(self):
@@ -31,12 +33,12 @@ class Question:
         return self.__answer
 
     @property
-    def show(self):
-        return self.__show
+    def show_condition(self):
+        return self.__show_condition
 
-    @show.setter
-    def show(self, value):
-        self.__show = value
+    @show_condition.setter
+    def show_condition(self, value):
+        self.__show_condition = value
 
     @property
     def widget(self):
@@ -46,6 +48,26 @@ class Question:
     def widget(self, value):
         self.__widget = value
 
-    def pyqt5_render(self, layout):
+    @property
+    def widget_label(self):
+        return self.__widget_label
+
+    @widget_label.setter
+    def widget_label(self, value):
+        self.__widget_label = value
+
+    def evaluate_show_condition(self, form):
+        visitor = ExpressionEvaluator(form)
+        visitor.visit(self.show_condition)
+
+        return visitor.result
+
+    def pyqt5_render(self, layout, show=True):
         self.widget = self.answer_type.pyqt5_default_widget()
-        layout.addRow(QLabel(self.label), self.widget)
+        self.__widget_label = QLabel(self.label)
+
+        if not show:
+            self.widget.hide()
+            self.widget_label.hide()
+
+        layout.addRow(self.__widget_label, self.widget)

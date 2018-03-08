c2349ddf74702be4047ec146f5e6a9d58d243b17
AHerczeg <h.herczeg.andras@gmail.com>
2018-03-08 14:43:18 +0100

    Added Operator classes (The rest of the classes are now obsolete)

---
+++ Andras_Thijs/src/Nodes/Operator/BoolOperator.java
@@ -0,0 +1,22 @@
+package Nodes.Operator;
+
+import Nodes.Term.Boolean;
+
+public class BoolOperator extends Operator {
+
+
+    public BoolOperator(String value){
+        super(value);
+    }
+
+    // TODO Use Term instead of Boolean
+    public Boolean calculate (Boolean left, Boolean right) {
+        if(this.getValue().equals("&&"))
+            return new Boolean(left.getBoolean() && right.getBoolean());
+        if(this.getValue().equals("||"))
+            return new Boolean(left.getBoolean() && right.getBoolean());
+
+        // TODO throw exception if codes get here
+        return new Boolean(false);
+    }
+}
---
+++ Andras_Thijs/src/Nodes/Operator/Comparison.java
@@ -0,0 +1,34 @@
+package Nodes.Operator;
+
+import Nodes.Term.Boolean;
+import Nodes.Term.Float;
+import Nodes.Term.Integer;
+import Nodes.Term.QLString;
+import Nodes.Term.Term;
+
+public class Comparison extends Operator{
+
+    public Comparison(String value){
+        super(value);
+    }
+
+    public Boolean calculate(Term left, Term right) throws UnsupportedOperationException{
+
+        // Check if the sides are comparable types
+        if(!((left instanceof Integer || left instanceof Float) && (right instanceof Integer || right instanceof Float))){
+            throw new UnsupportedOperationException();
+        }
+
+        switch (this.getValue()){
+            case ">=": return new Boolean(left.getValue() >= right.getValue());
+            case "<=": return new Boolean(left.getValue() <= right.getValue());
+            case ">": return new Boolean(left.getValue() > right.getValue());
+            case "<": return new Boolean(left.getValue() < right.getValue());
+            default: ; //TODO throw error
+        }
+        // TODO throw error if code gets here
+        return new Boolean(false);
+    }
+
+
+}

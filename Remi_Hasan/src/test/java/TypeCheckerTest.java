a3cb6e398bc97e711a1bc28ac1b8b87a6f5ff0a5
Remi van Veen <remivveen@gmail.com>
2018-03-08 13:30:15 +0100

    type checking test fixed

--- Remi_Hasan/src/test/java/TypeCheckerTest.java
+++ Remi_Hasan/src/test/java/TypeCheckerTest.java
@@ -1,32 +1,38 @@
 import analysis.TypeChecker;
+import model.expression.Expression;
 import model.expression.binary.ExpressionArithmeticSum;
 import model.expression.variable.ExpressionVariableBoolean;
 import model.expression.variable.ExpressionVariableInteger;
+import org.antlr.v4.runtime.CommonToken;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.tree.TerminalNode;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class TypeCheckerTest {
 
+    private Expression expressionFromString(String input) {
+        ANTLRTester tester = new ANTLRTester(input);
+        return tester.visitor.visit(tester.parser.expression());
+    }
+
     @Test
     public void SomeHappyTypeCheckTest() {
-        ExpressionVariableInteger intExpression = new ExpressionVariableInteger(null,1);
-        ExpressionArithmeticSum sumExpression = new ExpressionArithmeticSum(null, intExpression, intExpression);
+        Expression expression = this.expressionFromString("1 + 1");
 
         TypeChecker typeChecker = new TypeChecker(null, null);
-        typeChecker.visit(sumExpression);
+        typeChecker.visit(expression);
 
         assertEquals(typeChecker.getErrors().size(), 0);
     }
 
     @Test
     public void SomeSadTypeCheckTest() {
-        ExpressionVariableInteger intExpression = new ExpressionVariableInteger(null, 1);
-        ExpressionVariableBoolean boolExpression = new ExpressionVariableBoolean(null, true);
-        ExpressionArithmeticSum sumExpression = new ExpressionArithmeticSum(null, intExpression, boolExpression);
+        Expression expression = this.expressionFromString("1 + true");
 
         TypeChecker typeChecker = new TypeChecker(null, null);
-        typeChecker.visit(sumExpression);
+        typeChecker.visit(expression);
 
         assertEquals(typeChecker.getErrors().size(), 1);
     }

76d48a004ac625999c18f2cfaf99d71710c6380a
Michael de Lang <kingoipo@gmail.com>
2018-03-08 15:37:35 +0100

    Refactor qls questions for rendering css styles

--- Carly_Michael/jectacular-ql/src/app/app.component.ts
+++ Carly_Michael/jectacular-ql/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component} from '@angular/core';
+import {Component, isDevMode} from '@angular/core';
 import {parse} from '../parser/ql-parser';
 import {parse as parseQls} from '../parser/qls-parser';
 import {QuestionBase} from './domain/angular-questions/question-base';
 import {FormGroup} from '@angular/forms';
 import {QuestionControlService} from './services/question-control.service';
-import {Page, Question as QlsQuestion, Section, Stylesheet, Widget, WidgetType} from './domain/ast/qls';
+import {Page, Question as QlsQuestion, Section, Style, Stylesheet, Widget, WidgetType} from './domain/ast/qls';
 import {emptyLoc, Form, QuestionType, Question as QlQuestion} from './domain/ast';
 import {ParseQlWithQlsFactoryService} from './services/parse-ql-with-qls-factory.service';
 import {ParseFactoryInterface} from './services/parse-factory-interface';
@@ -28,7 +28,56 @@ export class AppComponent {
   qlsToQlQuestionDictionary: Map<string, QuestionBase<any>> = new Map<string, QuestionBase<any>>();
 
   constructor (private questionControlService: QuestionControlService) {
+  this.inputQls = 'stylesheet "taxOfficeExample"\n' +
+    '{\n' +
+    '  page "Housing"\n' +
+    '  {\n' +
+    '    section "Buying"\n' +
+    '    {\n' +
+    '      question question3\n' +
+    '        widget checkbox\n' +
+    '    }\n' +
+    '    section "Loaning"\n' +
+    '      question question4\n' +
+    '  }\n' +
+    '\n' +
+    '  page "Selling"\n' +
+    '  {\n' +
+    '    section "Selling"\n' +
+    '    {\n' +
+    '      question question5\n' +
+    '        widget radio("Yes", "No")\n' +
+    '      section "You sold a house"\n' +
+    '      {\n' +
+    '        question question1\n' +
+    '          widget spinbox\n' +
+    '        question question2\n' +
+    '          widget spinbox\n' +
+    '        question question6\n' +
+    '        default integer\n' +
+    '        {\n' +
+    '          width: 400\n' +
+    '          font: "Arial"\n' +
+    '          fontsize: 14\n' +
+    '          color: #999999\n' +
+    '          widget spinbox\n' +
+    '        }\n' +
+    '      }\n' +
+    '    }\n' +
+    '    default boolean widget radio("Yes", "No")\n' +
+    '  }\n' +
+    '}';
 
+  this.input = 'form form {\n' +
+    '  question1: "IntegerQuestion?"  integer\n' +
+    '  question2: "DecimalQuestion?"  integer\n' +
+    '  question3: "BooleanQuestion?"  boolean\n' +
+    '  question4: "StringQuestion?"  string\n' +
+    '  question5: "DateQuestion?"  date\n' +
+    '  if (question3) {\n' +
+    '    question6: "ifQuestion" integer\n' +
+    '  }\n' +
+    '}';
   }
 
   getQuestionBaseByName(name: string): QuestionBase<any> {
@@ -42,21 +91,43 @@ export class AppComponent {
   }
 
   createQuestionMappingCache() {
-    for (const qlsQuestion of this.qlsStylesheet.getQuestions()) {
+    for (const qlsQuestion of this.qlsStylesheet.getQuestions([])) {
       let questionBase: QuestionBase<any>;
 
       for (const q of this.questions) {
-        if (q.key === qlsQuestion.name) {
+        if (q.key === qlsQuestion.question.name) {
           questionBase = q;
           break;
         }
       }
 
       if (!questionBase) {
-        throw new Error(`Couldn't find question ${qlsQuestion.name}`);
+        throw new Error(`Couldn't find question ${qlsQuestion.question.name}`);
       }
 
-      this.qlsToQlQuestionDictionary[qlsQuestion.name] = questionBase;
+      this.qlsToQlQuestionDictionary[qlsQuestion.question.name] = questionBase;
+    }
+  }
+
+  assignStyleToQuestions() {
+    for (const qlsQuestionWithAppliedStyles of this.qlsStylesheet.getQuestions([])) {
+      const question = this.qlsToQlQuestionDictionary[qlsQuestionWithAppliedStyles.question.name];
+
+      if (qlsQuestionWithAppliedStyles.styles && qlsQuestionWithAppliedStyles.styles.length > 0) {
+        const styles: Map<string, Style> = new Map<string, Style>();
+
+        for (const style of qlsQuestionWithAppliedStyles.styles) {
+          styles[style.name] = style;
+        }
+
+        let questionStyle = '';
+
+        for (const [, v] of styles) {
+          questionStyle += v.name + ': ' + v.value + ';';
+        }
+        //console.log(question.name, questionStyle);
+        question.style = questionStyle;
+      }
     }
   }
 
@@ -81,11 +152,16 @@ export class AppComponent {
       this.errorMessage = undefined;
 
       this.createQuestionMappingCache();
+      //this.assignStyleToQuestions();
     } catch (e) {
       this.form = undefined;
       this.formName = undefined;
       this.questions = undefined;
       this.errorMessage = e.message;
+
+      if (isDevMode()) {
+        console.log(e);
+      }
     }
     this.payload = undefined;
   }

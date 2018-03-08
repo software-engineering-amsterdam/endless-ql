b0057fb88f6644a7b0140ab0d05092f8a4a958a5
Nick <popanicolaemarian94@gmail.com>
2018-03-08 14:46:35 +0100

    refactor grammar and visitor to support computed questions

--- nitpickers/pyql/pyql/antlr/QLVisitor.py
+++ nitpickers/pyql/pyql/antlr/QLVisitor.py
@@ -1,4 +1,4 @@
-# Generated from QL.g4 by ANTLR 4.5.3
+# Generated from QL.g4 by ANTLR 4.7
 from antlr4 import *
 if __name__ is not None and "." in __name__:
     from .QLParser import QLParser
@@ -64,8 +64,13 @@ class QLVisitor(ParseTreeVisitor):
         return self.visitChildren(ctx)
 
 
-    # Visit a parse tree produced by QLParser#moneyType.
-    def visitMoneyType(self, ctx:QLParser.MoneyTypeContext):
+    # Visit a parse tree produced by QLParser#basicMoneyType.
+    def visitBasicMoneyType(self, ctx:QLParser.BasicMoneyTypeContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by QLParser#computedMoneyType.
+    def visitComputedMoneyType(self, ctx:QLParser.ComputedMoneyTypeContext):
         return self.visitChildren(ctx)
 
 
@@ -141,11 +146,6 @@ class QLVisitor(ParseTreeVisitor):
 
     # Visit a parse tree produced by QLParser#identifier.
     def visitIdentifier(self, ctx:QLParser.IdentifierContext):
-        return self.visitChildren(ctx)
-
-
-    # Visit a parse tree produced by QLParser#money.
-    def visitMoney(self, ctx:QLParser.MoneyContext):
         return self.visitChildren(ctx)
 
 

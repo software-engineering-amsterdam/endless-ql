8107c529a529e7683891f6ef9bb814046233a4a5
Edwin <edwin_ou@hotmail.com>
2018-03-08 14:21:43 +0100

    Add before to question checker and some cleanup

--- ouweh-vdwerf/src/test/org/uva/ql/validation/QuestionCheckerTest.java
+++ ouweh-vdwerf/src/test/org/uva/ql/validation/QuestionCheckerTest.java
@@ -1,26 +1,31 @@
 package org.uva.ql.validation;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.uva.ql.ast.Form;
+import org.uva.ql.parsing.ASTBuilder;
 
-import static org.junit.Assert.*;
+import java.io.File;
+import java.io.FileReader;
 
 public class QuestionCheckerTest {
+
+    private Form form;
+    private QuestionChecker questionChecker;
+
+    @Before
+    public void setUp() {
+        ASTBuilder parser = new ASTBuilder();
+        FileReader reader = new FileReader();
+
+
+        questionChecker = new QuestionChecker(form);
+    }
 
     @Test
     public void runCheck() {
     }
 
-    @Test
-    public void visit() {
-    }
-
-    @Test
-    public void visit1() {
-    }
-
-    @Test
-    public void visit2() {
-    }
 
     @Test
     public void getSymbolTable() {

fb2519b06fb71c7e40573c9b5fe10f4db163dc1c
Dennis Kruidenberg <kruidenbergdennis@gmail.com>
2018-03-08 13:16:18 +0100

    update Questions

--- Jordy_Dennis/GUI/question_generator.py
+++ Jordy_Dennis/GUI/question_generator.py
@@ -9,10 +9,11 @@ import collections
 
 class Question_Generator:
 
-    def __init__(self, varDict, ast):
+    def __init__(self, varDict, ast, form):
         self.varDict = varDict
         self.ast = ast
         self.questions = collections.OrderedDict()
+        self.form = form
 
     def getVarDict(self):
         return self.varDict
@@ -20,6 +21,11 @@ class Question_Generator:
     # Get a list of all the questions that need to be rendered (depending on the evaluation of the statements)
     def updateQuestions(self):
         self.get_questions(self.ast.form.block)
+        if self.form:
+            for varName in self.questions:
+                label = self.questions[varName].getQuestion()
+                type = self.varDict[varName]['node'].checkTypes()
+                self.form.add_question(varName, label, type)
         return self.questions
 
     # Create the list of all the questions by recursively looping through the statements and adding them to te dictionairy

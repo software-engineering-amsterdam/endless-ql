9948d8831c451f54bd46fb214a8408b034f07866
Edwin <edwin_ou@hotmail.com>
2018-03-08 14:15:40 +0100

    Add validation test scaffolding

---
+++ ouweh-vdwerf/src/test/org/uva/ql/validation/QuestionCheckerTest.java
@@ -0,0 +1,28 @@
+package org.uva.ql.validation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QuestionCheckerTest {
+
+    @Test
+    public void runCheck() {
+    }
+
+    @Test
+    public void visit() {
+    }
+
+    @Test
+    public void visit1() {
+    }
+
+    @Test
+    public void visit2() {
+    }
+
+    @Test
+    public void getSymbolTable() {
+    }
+}
---
+++ ouweh-vdwerf/src/test/org/uva/ql/validation/TypeCheckerTest.java
@@ -0,0 +1,48 @@
+package org.uva.ql.validation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TypeCheckerTest {
+
+    @Test
+    public void runCheck() {
+    }
+
+    @Test
+    public void visit() {
+    }
+
+    @Test
+    public void visit1() {
+    }
+
+    @Test
+    public void visit2() {
+    }
+
+    @Test
+    public void visit3() {
+    }
+
+    @Test
+    public void visit4() {
+    }
+
+    @Test
+    public void visit5() {
+    }
+
+    @Test
+    public void visit6() {
+    }
+
+    @Test
+    public void visit7() {
+    }
+
+    @Test
+    public void visit8() {
+    }
+}

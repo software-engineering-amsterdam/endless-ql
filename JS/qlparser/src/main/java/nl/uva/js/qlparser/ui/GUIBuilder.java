7e32ce15264b21db0000e1e86fd40bb75ce182c0
Sara Oonk <sara.oonk@live.com>
2018-03-08 23:13:53 +0100

    Refactored GUIBuilder for readability (WIP replacing constants)

--- JS/qlparser/src/main/java/nl/uva/js/qlparser/ui/GUIBuilder.java
+++ JS/qlparser/src/main/java/nl/uva/js/qlparser/ui/GUIBuilder.java
@@ -9,34 +9,22 @@ import java.awt.event.WindowEvent;
 import java.util.List;
 
 public class GUIBuilder {
+
+    private static final int FORM_VIEW_HEIGHT = 700;
+
+    private static final int BUTTON_HEIGHT = 50;
+    private static final int LOG_HEIGHT    = 100;
+    private static final int FOOTER_HEIGHT = BUTTON_HEIGHT + LOG_HEIGHT;
+
+    private static final int FULL_WIDTH  = 1200;
+    private static final int FULL_HEIGHT = FORM_VIEW_HEIGHT + FOOTER_HEIGHT;
+
     public static Frame getGUI(Form form) {
-        Frame mainFrame = new Frame();
-        mainFrame.setSize(new Dimension(1200, 730));
-        mainFrame.setResizable(false);
-        mainFrame.setVisible(true);
-        mainFrame.setLayout(new BorderLayout());
+        Frame mainFrame = getMainFrame();
 
-        JPanel inputPane = new JPanel();
-        TextArea inputArea = new TextArea("",0, 0, TextArea.SCROLLBARS_VERTICAL_ONLY);
-        inputArea.setPreferredSize((new Dimension(500, 700)));
-        inputPane.add(inputArea);
-
-        JPanel formPanel = getFormPanel(form);
-
-        JPanel bottomPane = new JPanel();
-        bottomPane.setLayout(new BorderLayout());
-
-        JPanel buttonPane = new JPanel();
-        buttonPane.setPreferredSize(new Dimension(1200, 50));
-
-        JPanel logPane = new JPanel();
-        TextArea logArea = new TextArea("",0, 0, TextArea.SCROLLBARS_VERTICAL_ONLY);
-        logArea.setPreferredSize((new Dimension(1200, 100)));
-        logPane.add(logArea);
-        logPane.setVisible(true);
-
-        bottomPane.add(buttonPane, BorderLayout.PAGE_START);
-        bottomPane.add(logPane, BorderLayout.PAGE_END);
+        JPanel inputPane  = getInputPanel();
+        JPanel formPanel  = getFormPanel(form);
+        JPanel bottomPane = getBottomPanel();
 
         mainFrame.add(inputPane, BorderLayout.LINE_START);
         mainFrame.add(formPanel, BorderLayout.CENTER);
@@ -48,7 +36,30 @@ public class GUIBuilder {
                 mainFrame.dispose();
             }
         });
+
         return mainFrame;
+    }
+
+    private static Frame getMainFrame() {
+        Frame mainFrame = new Frame();
+
+        mainFrame.setSize(new Dimension(FULL_WIDTH, FULL_HEIGHT));
+        mainFrame.setResizable(false);
+        mainFrame.setLayout(new BorderLayout());
+        mainFrame.setVisible(true);
+
+        return mainFrame;
+    }
+
+    private static JPanel getInputPanel() {
+        JPanel inputPanel  = new JPanel();
+
+        TextArea inputArea = new TextArea("",0, 0, TextArea.SCROLLBARS_VERTICAL_ONLY);
+        inputArea.setPreferredSize((new Dimension(500, 700)));
+
+        inputPanel.add(inputArea);
+
+        return inputPanel;
     }
 
     private static JPanel getFormPanel(Form form) {
@@ -72,5 +83,34 @@ public class GUIBuilder {
         return formPanel;
     }
 
+    private static JPanel getBottomPanel() {
+        JPanel bottomPanel = new JPanel();
 
+        JPanel buttonPanel = getButtonPanel();
+        JPanel logPanel    = getLogPanel();
+
+        bottomPanel.setLayout(new BorderLayout());
+        bottomPanel.add(buttonPanel, BorderLayout.PAGE_START);
+        bottomPanel.add(logPanel, BorderLayout.PAGE_END);
+        bottomPanel.setVisible(true);
+
+        return bottomPanel;
+    }
+
+    private static JPanel getButtonPanel() {
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setPreferredSize(new Dimension(1200, 50));
+        return buttonPanel;
+    }
+
+    private static JPanel getLogPanel() {
+        JPanel logPanel = new JPanel();
+
+        TextArea logArea = new TextArea("",0, 0, TextArea.SCROLLBARS_VERTICAL_ONLY);
+        logArea.setPreferredSize((new Dimension(1200, 100)));
+
+        logPanel.add(logArea);
+
+        return logPanel;
+    }
 }

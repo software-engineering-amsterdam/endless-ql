8d20b4bb0b176cd9029966f87c0225bfb547e6e9
Rob <rob.kunst@student.uva.nl>
2018-03-08 12:55:42 +0100

    Example with formlayout

--- Rob_Jaap/java/src/main/java/com/chariotit/uva/sc/qdsl/Application.java
+++ Rob_Jaap/java/src/main/java/com/chariotit/uva/sc/qdsl/Application.java
@@ -1,6 +1,6 @@
 package com.chariotit.uva.sc.qdsl;
 
-import main.java.com.chariotit.uva.sc.qdsl.QLFrame;
+import com.chariotit.uva.sc.qdsl.QLFrame;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
@@ -14,17 +14,17 @@ public class Application {
                 .run(args);
     }
 
-
-    /**
-     * Creates the {@link QLFrame} object and returns it.
-     *
-     * This @Bean could have been replaced by a @Component annotation being
-     * added to the {@link QLFrame} class.
-     *
-     * @return the application window
-     */
-    @Bean
-    public QLFrame frame() {
-        return new QLFrame();
-    }
+//
+//    /**
+//     * Creates the {@link QLFrame} object and returns it.
+//     *
+//     * This @Bean could have been replaced by a @Component annotation being
+//     * added to the {@link QLFrame} class.
+//     *
+//     * @return the application window
+//     */
+//    @Bean
+//    public QLFrame frame() {
+//        return new QLFrame();
+//    }
 }
--- Rob_Jaap/java/src/main/java/com/chariotit/uva/sc/qdsl/ApplicationRunner.java
+++ Rob_Jaap/java/src/main/java/com/chariotit/uva/sc/qdsl/ApplicationRunner.java
@@ -1,7 +1,7 @@
 package com.chariotit.uva.sc.qdsl;
 
 import com.chariotit.uva.sc.qdsl.parser.QLVisitor;
-import main.java.com.chariotit.uva.sc.qdsl.QLFrame;
+import com.chariotit.uva.sc.qdsl.QLFrame;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,12 +17,12 @@ import javax.swing.JFrame;
 
 @Component
 public class ApplicationRunner implements CommandLineRunner {
-
-    /**
-     * Pull in the JFrame to be displayed.
-     */
-    @Autowired
-    private QLFrame frame;
+//
+//    /**
+//     * Pull in the JFrame to be displayed.
+//     */
+//    @Autowired
+//    private QLFrame frame;
 
     @Override
     public void run(String... args) throws Exception {
@@ -31,6 +31,8 @@ public class ApplicationRunner implements CommandLineRunner {
             System.err.println("Missing filename argument. Please provide input file.");
             System.exit(1);
         }
+
+        System.out.println("Starting application");
 
         String filePath = args[0];
 
@@ -43,8 +45,15 @@ public class ApplicationRunner implements CommandLineRunner {
 
         System.out.println(visitor.visit(tree));
 
-        frame.setVisible(true);
+//        QLFrame frame = new QLFrame();
 
+        QLFormBuilder builder = new QLFormBuilder();
+
+        builder.showForm();
+
+//        QLFormFrame frame = new QLFormFrame();
+
+        System.out.println("Frame generated");
     }
 
 }

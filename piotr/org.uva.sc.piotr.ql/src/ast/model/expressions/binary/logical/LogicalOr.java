903c41970f6a706b667652ec3c01f23d0d41baa4
piotrkosytorz <piotr.kosytorz@gmail.com>
2018-03-08 11:00:17 +0100

    AST refactoring - Meta Information

--- piotr/org.uva.sc.piotr.ql/src/ast/model/expressions/binary/logical/LogicalOr.java
+++ piotr/org.uva.sc.piotr.ql/src/ast/model/expressions/binary/logical/LogicalOr.java
@@ -5,8 +5,8 @@ import ast.model.expressions.binary.BinaryExpression;
 import ast.visitors.ASTNodeVisitor;
 
 public class LogicalOr extends BinaryExpression {
-    public LogicalOr(Expression leftSide, Expression rightSide, Integer startLine, Integer endLine) {
-        super(leftSide, rightSide, startLine, endLine);
+    public LogicalOr(Expression leftSide, Expression rightSide, MetaInformation metaInformation) {
+        super(leftSide, rightSide, metaInformation);
     }
 
     @Override

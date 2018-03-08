645f554c81a1489cc215b432ce25017df30bd7c2
MichielBoswijk <michiel.boswijk@gmail.com>
2018-03-08 14:56:13 +0100

    Add type checking for AND/OR AST nodes

--- PyQuest/src/AST/expressions/binary_operators/and_node.py
+++ PyQuest/src/AST/expressions/binary_operators/and_node.py
@@ -1,6 +1,14 @@
 from AST.expressions.binary_operators.binary_operator_node import BinaryOperatorNode
+from AST.types.type_boolean import TypeBoolean
+from AST.types.type_undefined import TypeUndefined
 
 
 class AndOperatorNode(BinaryOperatorNode):
     def __init__(self, position, expression_type, left_expression, right_expression, value):
         super(AndOperatorNode, self).__init__(position, expression_type, left_expression, right_expression, value)
+        self.__valid_types = {(TypeBoolean, TypeBoolean): TypeBoolean}
+
+    def get_result_type(self, type1, type2):
+        if self.__valid_types.get((type1, type2)):
+            return self.__valid_types.get((type1, type2))
+        return TypeUndefined

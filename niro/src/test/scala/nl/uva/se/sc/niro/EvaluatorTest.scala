b3f19632c87cb67f8cec080200fd4d7504f14344
Nico Tromp <nico.tromp@gmail.com>
2018-03-09 17:09:46 +0100

    Add test for evaluator that exposes a failure in expression evaluation.

--- niro/src/test/scala/nl/uva/se/sc/niro/EvaluatorTest.scala
+++ niro/src/test/scala/nl/uva/se/sc/niro/EvaluatorTest.scala
@@ -1,8 +1,9 @@
 package nl.uva.se.sc.niro
 
-import nl.uva.se.sc.niro.model.expressions.answers.{ BooleanAnswer, IntAnswer }
-import nl.uva.se.sc.niro.model.expressions.{ BinaryOperation, Reference }
+import nl.uva.se.sc.niro.Evaluator.Dictionary
 import nl.uva.se.sc.niro.model._
+import nl.uva.se.sc.niro.model.expressions.answers.{ BooleanAnswer, DecAnswer, IntAnswer }
+import nl.uva.se.sc.niro.model.expressions.{ BinaryOperation, Reference, UnaryOperation }
 import org.scalatest.WordSpec
 
 class EvaluatorTest extends WordSpec {
@@ -15,7 +16,7 @@ class EvaluatorTest extends WordSpec {
 
     "evaluateStatement" in {}
 
-    "evaluateQLForm" in {
+    "evaluateQLForm with single expression" in {
       val qLForm = QLForm(
         formName = "Revenue",
         statements = List(
@@ -36,6 +37,77 @@ class EvaluatorTest extends WordSpec {
       val result = Evaluator.evaluate(qLForm, Map.empty)
       val expected =
         Map("revenue" -> IntAnswer(Some(1000)), "expenses" -> IntAnswer(Some(800)), "profit" -> IntAnswer(Some(200)))
+
+      assert(result == expected)
+    }
+
+    "evaluateQLForm with multiple expressions" in {
+      val qlForm = QLForm(
+        "EditOrNotToEdit",
+        List(
+          Question("booleanVariable", "Boolean variable", BooleanType, BooleanAnswer(None)),
+          Question("integerVariable", "Integer variable", IntegerType, IntAnswer(None)),
+          Question("decimalVariable", "Decimal variable", DecimalType, DecAnswer(None)),
+          Conditional(
+            Reference("booleanVariable"),
+            List(
+              Question(
+                "integerConstant",
+                "Integer constant",
+                IntegerType,
+                BinaryOperation(Mul, IntAnswer(21), IntAnswer(2))
+              ),
+              Question("decimalConstant", "Decimal constant", DecimalType, DecAnswer(42.4))
+            )
+          ),
+          Conditional(
+            UnaryOperation(Neg, Reference("booleanVariable")),
+            List(
+              Question(
+                "integerExpression",
+                "Integer expression",
+                IntegerType,
+                BinaryOperation(
+                  Add,
+                  BinaryOperation(Add, Reference("integerConstant"), IntAnswer(Some(1))),
+                  Reference("integerVariable")
+                )
+              ),
+              Question(
+                "decimalExpression",
+                "Decimal expression",
+                DecimalType,
+                BinaryOperation(
+                  Add,
+                  BinaryOperation(Add, Reference("decimalConstant"), DecAnswer(1.0)),
+                  Reference("decimalVariable")
+                )
+              )
+            )
+          )
+        )
+      )
+      val inputs: Dictionary = Map(
+        "integerConstant" -> IntAnswer(42),
+        "decimalExpression" -> DecAnswer(None),
+        "decimalVariable" -> DecAnswer(None),
+        "booleanVariable" -> BooleanAnswer(false),
+        "integerExpression" -> IntAnswer(None),
+        "integerVariable" -> IntAnswer(12),
+        "decimalConstant" -> DecAnswer(42.4)
+      )
+
+      val result = Evaluator.evaluate(qlForm, inputs)
+      val expected: Dictionary =
+        Map(
+          "integerConstant" -> IntAnswer(42),
+          "decimalExpression" -> DecAnswer(None),
+          "decimalVariable" -> DecAnswer(None),
+          "booleanVariable" -> BooleanAnswer(false),
+          "integerExpression" -> IntAnswer(55),
+          "integerVariable" -> IntAnswer(12),
+          "decimalConstant" -> DecAnswer(42.4)
+        )
 
       assert(result == expected)
     }

89cdc6467c710e8e135e6983229b69bbfdbf950f
Nico Tromp <nico.tromp@gmail.com>
2018-03-12 22:12:29 +0100

    Add dynamic pagination depending on stylesheet.

--- niro/src/main/scala/nl/uva/se/sc/niro/QLStylesheetService.scala
+++ niro/src/main/scala/nl/uva/se/sc/niro/QLStylesheetService.scala
@@ -5,20 +5,19 @@ import java.io.File
 import nl.uva.se.sc.niro.errors.Errors
 import nl.uva.se.sc.niro.errors.Errors.Error
 import nl.uva.se.sc.niro.model.ql.QLForm
-import nl.uva.se.sc.niro.model.qls.{ Page, QLStylesheet }
+import nl.uva.se.sc.niro.model.qls.QLStylesheet
 import nl.uva.se.sc.niro.parser.QLStylesheetParser
-import nl.uva.se.sc.niro.typechecking.TypeChecker
+import nl.uva.se.sc.niro.typechecking.QLSTypeChecker
 import org.antlr.v4.runtime.CharStreams
 
+// TODO check if this is the correct name, maybe facade is better
 object QLStylesheetService {
   def importQLStylesheetSpecification(form: QLForm, qlsFile: File): Either[Seq[Errors.Error], Option[QLStylesheet]] = {
     if (qlsFile.exists()) {
       val stylesheet: QLStylesheet = QLStylesheetParser.parse(CharStreams.fromFileName(qlsFile.getAbsolutePath))
       val parseErrors: Seq[Error] = QLStylesheetParser.getParseErrors.toList
       if (parseErrors.isEmpty) {
-        //      TypeChecker.pipeline(form, stylesheet).left.map(error => Seq(error))
-//        Right(Some(stylesheet))
-        Right(Some(QLStylesheet("test", Seq(Page("PageOne", Seq.empty)))))
+        QLSTypeChecker.pipeline(form, stylesheet).left.map(error => Seq(error)).right.map(stylesheet => Some(stylesheet))
       } else {
         Left(parseErrors)
       }

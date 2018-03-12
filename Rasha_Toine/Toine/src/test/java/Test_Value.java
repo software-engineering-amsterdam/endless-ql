a9ea1abee2085fd59f2338d569963dd3a0b9faa3
Toine Khonraad <a.khonraad@khonraad.nl>
2018-03-12 12:00:23 +0100

    Added more tests

--- Rasha_Toine/Toine/src/test/java/Test_Value.java
+++ Rasha_Toine/Toine/src/test/java/Test_Value.java
@@ -31,6 +31,11 @@ public class Test_Value {
 	}
 
 	@Test
+	public void not_boolean() {
+		qt().forAll( booleans().all() ).check( ( φ ) -> booleanValue( φ ).apply( "!" ).equals( booleanValue( !φ ) ) );
+	}
+
+	@Test
 	public void boolean_and_boolean() {
 		qt().forAll( booleans().all(), booleans().all() ).check(
 				( φ, ψ ) -> booleanValue( φ ).apply( "&&", booleanValue( ψ ) ).equals( booleanValue( φ && ψ ) ) );
@@ -44,33 +49,103 @@ public class Test_Value {
 
 	@Test
 	public void string_plus_string() {
-		qt().forAll( strings().basicLatinAlphabet().ofLengthBetween( 0, 10000 ),
-				strings().basicLatinAlphabet().ofLengthBetween( 0, 10000 ) )
-				.check( ( s1, s2 ) -> stringValue( s1 ).apply( "+", stringValue( s2 ) )
-						.equals( stringValue( s1 + s2 ) ) );
+		qt().forAll( strings().basicLatinAlphabet().ofLengthBetween( 0, 20000 ),
+				strings().basicLatinAlphabet().ofLengthBetween( 0, 20000 ) )
+				.check( ( left_string, right_string ) -> stringValue( left_string )
+						.apply( "+", stringValue( right_string ) )
+						.equals( stringValue( left_string + right_string ) ) );
 	}
 
 	@Test
 	public void string_plus_integer() {
-		qt().forAll( strings().basicLatinAlphabet().ofLengthBetween( 0, 10000 ), integers().all() )
+		qt().forAll( strings().basicLatinAlphabet().ofLengthBetween( 0, 20000 ), integers().all() )
 				.check( ( string, integer ) -> stringValue( string ).apply( "+", integerValue( integer ) )
 						.equals( stringValue( string + integer ) ) );
 	}
 
 	@Test
 	public void string_plus_money() {
-		qt().forAll( strings().basicLatinAlphabet().ofLengthBetween( 0, 10000 ),
+		qt().forAll( strings().basicLatinAlphabet().ofLengthBetween( 0, 20000 ),
 				bigDecimals().ofBytes( 32 ).withScale( 2 ) )
 				.check( ( string, money ) -> stringValue( string ).apply( "+", moneyValue( money ) )
 						.equals( stringValue( string + money.toString() ) ) );
 	}
 
 	@Test
-	public void integer_plus_integer() {
-		qt().forAll( integers().between( -(Integer.MAX_VALUE) / 2, (Integer.MAX_VALUE) / 2 ),
-				integers().between( -(Integer.MAX_VALUE) / 2, (Integer.MAX_VALUE) / 2 ) )
-				.check( ( integer1, integer2 ) -> integerValue( integer1 ).apply( "+", integerValue( integer2 ) )
-						.equals( integerValue( integer1 + integer2 ) ) );
+	public void min_integer() {
+		qt().forAll( integers().all() ).check(
+				( left_integer ) -> integerValue( left_integer ).apply( "-" ).equals( integerValue( -left_integer ) ) );
 	}
 
+	@Test
+	public void integer_times_integer() {
+		qt().forAll( integers().all(), integers().all() )
+				.check( ( left_integer, right_integer ) -> integerValue( left_integer )
+						.apply( "*", integerValue( right_integer ) )
+						.equals( integerValue( left_integer * right_integer ) ) );
+	}
+
+	@Test
+	public void integer_dividedBy_integer() {
+		qt().forAll( integers().all(), integers().all().assuming( integer -> integer != 0 ) )
+				.check( ( left_integer, right_integer ) -> integerValue( left_integer )
+						.apply( "/", integerValue( right_integer ) )
+						.equals( integerValue( left_integer / right_integer ) ) );
+	}
+
+	@Test
+	public void integer_plus_integer() {
+		qt().forAll( integers().all(), integers().all() )
+				.check( ( left_integer, right_integer ) -> integerValue( left_integer )
+						.apply( "+", integerValue( right_integer ) )
+						.equals( integerValue( left_integer + right_integer ) ) );
+	}
+
+	@Test
+	public void integer_min_integer() {
+		qt().forAll( integers().all(), integers().all() )
+				.check( ( left_integer, right_integer ) -> integerValue( left_integer )
+						.apply( "-", integerValue( right_integer ) )
+						.equals( integerValue( left_integer - right_integer ) ) );
+	}
+
+	@Test
+	public void integer_compared_with_integer() {
+		qt().forAll( integers().all(), integers().all() )
+				.check( ( left_integer, right_integer ) -> integerValue( left_integer )
+						.apply( "==", integerValue( right_integer ) )
+						.equals( booleanValue( left_integer.equals( right_integer ) ) ) );
+	}
+
+	@Test
+	public void integer_isLowerThen_integer() {
+		qt().forAll( integers().all(), integers().all() )
+				.check( ( left_integer, right_integer ) -> integerValue( left_integer )
+						.apply( "<", integerValue( right_integer ) )
+						.equals( booleanValue( left_integer < right_integer ) ) );
+	}
+
+	@Test
+	public void integer_isGreaterThen_integer() {
+		qt().forAll( integers().all(), integers().all() )
+				.check( ( left_integer, right_integer ) -> integerValue( left_integer )
+						.apply( ">", integerValue( right_integer ) )
+						.equals( booleanValue( left_integer > right_integer ) ) );
+	}
+
+	@Test
+	public void integer_isLowerThenOrEqualTo_integer() {
+		qt().forAll( integers().all(), integers().all() )
+				.check( ( left_integer, right_integer ) -> integerValue( left_integer )
+						.apply( "<=", integerValue( right_integer ) )
+						.equals( booleanValue( left_integer <= right_integer ) ) );
+	}
+
+	@Test
+	public void integer_isGreaterThenOrEqualTo_integer() {
+		qt().forAll( integers().all(), integers().all() )
+				.check( ( left_integer, right_integer ) -> integerValue( left_integer )
+						.apply( ">=", integerValue( right_integer ) )
+						.equals( booleanValue( left_integer >= right_integer ) ) );
+	}
 }

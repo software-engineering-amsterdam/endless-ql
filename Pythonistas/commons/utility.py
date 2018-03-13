70c257081401b5106c0d24c68684d06afbc04c7f
Dylan Bartels <d_bartels@live.nl>
2018-03-13 11:00:07 +0100

    Refactor debug grammar from shell script to python code

--- Pythonistas/commons/utility.py
+++ Pythonistas/commons/utility.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
+import os
+
+
 def open_file(filename):
+    """Open file and return string"""
     file = open(filename)
     characters = file.read()
     file.close()
--- Pythonistas/run_app.py
+++ Pythonistas/run_app.py
@@ -46,7 +46,7 @@ def main():
     if args.grammar:
         # todo: make it so that you give path in CLI call
         # debug_grammar('C:/Users/svdh/PycharmProjects/sql/endless-ql/Pythonistas/tests/forms/if.ql')
-        debug_grammar("tests/forms/if.ql")
+        # debug_grammar("tests/forms/if.ql")
         sys.stdout.write(debug_grammar("tests/forms/arithmetic.ql"))
         sys.exit(0)
 

753ab1ccdeda4c3dcb0217845a0b49f9d96f561a
tdobber <timo.dobber@student.uva.nl>
2018-03-14 14:23:23 +0100

    Setup QLS files

---
+++ SeaFence_Lars_Timo/QLSVisitorHelper.py
@@ -0,0 +1,92 @@
+from QLS.QLSVisitor import QLSVisitor
+from antlr4 import *
+
+# This class defines a complete generic visitor for a parse tree produced by QLParser.
+
+class QLSVisitorHelper(QLSVisitor):
+
+	# Visit a parse tree produced by QLSParser#stylesheet.
+    def visitStylesheet(self, ctx):
+        stylesheet_id = ctx.stylesheet_id().getText()
+
+
+    # Visit a parse tree produced by QLSParser#page.
+    def visitPage(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by QLSParser#section.
+    def visitSection(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by QLSParser#question.
+    def visitQuestion(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by QLSParser#default_style.
+    def visitDefault_style(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by QLSParser#default_options.
+    def visitDefault_options(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by QLSParser#stylesheet_id.
+    def visitStylesheet_id(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by QLSParser#page_id.
+    def visitPage_id(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by QLSParser#section_id.
+    def visitSection_id(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by QLSParser#widget.
+    def visitWidget(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by QLSParser#widget_type.
+    def visitWidget_type(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by QLSParser#width.
+    def visitWidth(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by QLSParser#font.
+    def visitFont(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by QLSParser#fontsize.
+    def visitFontsize(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by QLSParser#color.
+    def visitColor(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by QLSParser#var.
+    def visitVar(self, ctx):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by QLSParser#vartype.
+    def visitVartype(self, ctx):
+        return self.visitChildren(ctx)
+
+

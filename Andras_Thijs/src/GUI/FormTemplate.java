6db507835301b8e78b08082fb8e177db49350517
AHerczeg <h.herczeg.andras@gmail.com>
2018-03-14 19:21:35 +0100

    FormTemplate can now display all and only the available questions

--- Andras_Thijs/src/GUI/FormTemplate.java
+++ Andras_Thijs/src/GUI/FormTemplate.java
@@ -1,10 +1,13 @@
 package GUI;
 
+import AST.gen.QLParser;
+import Nodes.Condition;
 import Nodes.QLForm;
 import Nodes.Question;
 import Nodes.Type;
 import com.jgoodies.forms.builder.DefaultFormBuilder;
 import com.jgoodies.forms.layout.FormLayout;
+import javafx.util.Builder;
 
 import javax.swing.*;
 import javax.swing.text.DateFormatter;
@@ -13,17 +16,19 @@ import java.awt.*;
 import java.text.DateFormat;
 import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Iterator;
+import java.util.*;
 import java.util.List;
 
 public class FormTemplate {
+
     private FormLayout layout;
     private JFrame frame;
     private JPanel mainPanel;
     private QLForm body;
     private JButton submit;
     private List<Component> components;
+    private DefaultFormBuilder builder;
+    private Map<Question, JPanel> questionDictionary;
 
     public FormTemplate(){
         body  = new QLForm("Undefined");
@@ -39,31 +44,75 @@ public class FormTemplate {
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        setPanel(initPanel());
-    }
-
-    private JPanel initPanel(){
-
         layout = new FormLayout(
                 "left:default, 3dlu default"); // 5 columns; add rows later
 
-        DefaultFormBuilder builder = new DefaultFormBuilder(layout);
-        try{
-            List<Question> questions = body.getQuestions();
-            Iterator<Question> iterator = questions.iterator();
-            while (iterator.hasNext()) {
-                Question question = iterator.next();
-                builder.append(question.getLabel(), getComponent(question.getType()));
-            }
-            builder.append(submit = new JButton("submit"));
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-        return builder.getPanel();
+        builder = new DefaultFormBuilder(layout);
+
+        updatePanel();
+
+        //refreshPanel(builder.getPanel());
+    }
+
+    private void updatePanel(){
+
+        List<Question> questions = body.getQuestions();
+
+        iterateQuestions(questions);
+
+        List<Condition> conditions = body.getConditions();
+        Iterator<Condition> conditionIterator = conditions.iterator();
+        while (conditionIterator.hasNext())
+            iterateQuestions(visitCondition(conditionIterator.next()));
+
+        //builder.append(submit = new JButton("submit"));
 
     }
 
-    private void setPanel(JPanel newPanel){
+    private List<Question> visitCondition(Condition condition){
+
+        List<Question> questions;
+
+        if(condition.getExpression().getBoolean()){
+            questions = condition.getQuestions();
+            List<Condition> conditions = condition.getConditions();
+            Iterator<Condition> conditionIterator = conditions.iterator();
+            while (conditionIterator.hasNext()) {
+                List<Question> subQuestions = visitCondition(conditionIterator.next());
+                if (subQuestions != null)
+                    questions.addAll(subQuestions);
+            }
+
+            return questions;
+        }
+
+        return null;
+    }
+
+    private void iterateQuestions (List<Question> questions){
+        Iterator<Question> questionIterator = questions.iterator();
+        while (questionIterator.hasNext()) {
+            System.out.println(questionIterator.next().getLabel());
+        }
+    }
+
+    private void addQuestion(Question question) {
+        if (!(questionDictionary.containsKey(question))){
+            DefaultFormBuilder panelBuilder = new DefaultFormBuilder(layout);
+            panelBuilder.append(question.getLabel(), getComponent(question.getType()));
+            questionDictionary.put(question, panelBuilder.getPanel());
+        }
+        question.setDisplayed(true);
+        frame.add(questionDictionary.get(question));
+    }
+
+    private void removeQuestion(Question question){
+        if (questionDictionary.containsKey(question) && question.isDisplayed()){
+            frame.remove(questionDictionary.get(question));
+        }
+    }
+
+    private void refreshPanel(JPanel newPanel){
 
         mainPanel = newPanel;
 

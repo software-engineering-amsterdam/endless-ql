92f8338f3847c91869a86d858a3914cbd9f8c4a0
Tim <tim.vonsee@me.com>
2018-03-14 13:21:18 +0100

    Added TextField with callback for Money values

--- LDG_TV/SC-Java-LDG-TV/src/main/java/domain/visitor/UIVisitor.java
+++ LDG_TV/SC-Java-LDG-TV/src/main/java/domain/visitor/UIVisitor.java
@@ -1,9 +1,12 @@
 package domain.visitor;
 
+import domain.Utilities;
 import domain.model.value.ArithmeticExpressionValue;
+import domain.model.value.MoneyValue;
 import domain.model.variable.BooleanVariable;
 import domain.model.variable.MoneyVariable;
 import domain.model.variable.StringVariable;
+import javafx.application.Platform;
 import javafx.scene.Node;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
@@ -36,6 +39,20 @@ public class UIVisitor implements Visitor {
 
     @Override
     public Node visit(MoneyVariable mv) {
-        return (Node) new TextField();
+        TextField tf = new TextField();
+
+        tf.textProperty()
+                .addListener((observable, oldValue, newValue) -> {
+                    boolean isNotNumeric =!Utilities.isNumeric(newValue);
+                    if(isNotNumeric){
+                        return;
+                    }
+
+                    int val = Integer.valueOf(newValue);
+                    mv.setValue(new MoneyValue(val));
+                });
+
+
+        return tf;
     }
 }

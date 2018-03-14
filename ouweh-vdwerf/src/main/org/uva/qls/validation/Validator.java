c4854812ea27c0eb6938e24df96443599d40cd82
DennisvanderWerf <dennisvandwerf@gmail.com>
2018-03-14 15:44:03 +0100

    Update QLS type checking

--- ouweh-vdwerf/src/main/org/uva/qls/validation/Validator.java
+++ ouweh-vdwerf/src/main/org/uva/qls/validation/Validator.java
@@ -1,6 +1,7 @@
 package org.uva.qls.validation;
 
 import org.uva.ql.ast.Question;
+import org.uva.ql.validation.Checker;
 import org.uva.ql.validation.LogHandler;
 import org.uva.qls.ast.Stylesheet;
 
@@ -14,17 +15,29 @@ public class Validator {
     private Stylesheet stylesheet;
     private ArrayList<Question> qlQuestions;
     private LogHandler logHandler;
+    private List<Checker> checkers;
 
     public Validator (ArrayList<Question> qlQuestions, Stylesheet stylesheet){
         this.qlQuestions = qlQuestions;
         this.stylesheet = stylesheet;
+        this.checkers = new ArrayList<>();
 
         this.logHandler = (LogHandler) Logger.getGlobal().getHandlers()[0];
+
+        initializeCheckers();
+    }
+
+    private void initializeCheckers() {
+        List<String> qlQuestionIds = qlQuestions.stream().map(Question::getName).collect(Collectors.toList());
+        List<String> qlsQuestionIds = stylesheet.getQuestions().stream().map(org.uva.qls.ast.Segment.Question::getId).collect(Collectors.toList());
+
+        checkers.add(new ReferenceChecker(qlQuestionIds, qlsQuestionIds));
     }
 
     public void run() {
-        List<String> qlQuestionIds = qlQuestions.stream().map(Question::getName).collect(Collectors.toList());
-        List<String> qlsQuestionIds = stylesheet.getQuestions().stream().map(org.uva.qls.ast.Segment.Question::getId).collect(Collectors.toList());
+        for (Checker checker : checkers) {
+            checker.runCheck();
+        }
     }
 
 }

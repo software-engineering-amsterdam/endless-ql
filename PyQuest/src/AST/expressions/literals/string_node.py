2db9078f50d9d12ccf05a54dc65c4798a0ea010a
bicker <bickerkards@tuta.io>
2018-03-14 15:17:07 +0100

    Add literal AST nodes for date and string

---
+++ PyQuest/src/AST/expressions/literals/string_node.py
@@ -0,0 +1,6 @@
+from AST.expressions.literals.literal_node import LiteralNode
+
+
+class StringNode(LiteralNode):
+    def __init__(self, position, expression_type, value):
+        super(StringNode, self).__init__(position, expression_type, value)

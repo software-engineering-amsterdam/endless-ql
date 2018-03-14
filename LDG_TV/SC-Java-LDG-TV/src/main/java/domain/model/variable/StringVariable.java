2bf8491f39b8856c751cb6791800862edc28db3e
Tim <tim.vonsee@me.com>
2018-03-14 12:39:01 +0100

    Add callback for textfields/strings

--- LDG_TV/SC-Java-LDG-TV/src/main/java/domain/model/variable/StringVariable.java
+++ LDG_TV/SC-Java-LDG-TV/src/main/java/domain/model/variable/StringVariable.java
@@ -6,19 +6,20 @@ import domain.visitor.Visitor;
 import javafx.scene.Node;
 
 public class StringVariable extends Variable {
-    private Value value;
+    private Value<String> value;
 
     public StringVariable(String identifier) {
         super(identifier);
+        this.value = new PlainValue("");
     }
 
     @Override
     public Value getValue() {
         return value;
     }
-    @Override
-    public void setValue(PlainValue value){
-        this.value = value;
+
+    public void setValue(String newVale){
+        value.setValue(newVale);
     }
 
     @Override

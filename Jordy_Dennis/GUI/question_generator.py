aa1f1bd1a502d486fddb45224ea8f300aa29f266
Dennis Kruidenberg <kruidenbergdennis@gmail.com>
2018-03-14 16:06:41 +0100

    comments

--- Jordy_Dennis/GUI/question_generator.py
+++ Jordy_Dennis/GUI/question_generator.py
@@ -55,12 +55,15 @@ class Question_Generator:
 
         return self.questions
 
+    # This function is use to delete question under an if statement, in order to insert an if en re-redener the
+    # questions below the if
     def deleteQuestionInForm(self, varName):
         for formQuestion in self.form.questions:
             if formQuestion.varName == varName:
                 formQuestion.frame.destroy()
                 self.form.questions.remove(formQuestion)
 
+    # this function is used to delete question that are no longer valid, i.e. the questions in an if or elif or else block
     def deleteInvalidQuestions(self):
         for question in self.form.questions:
             if (question.varName not in self.questions):

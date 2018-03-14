43a96d8a2cf0360d357e4232eb0f31015aaa66b4
Nico Tromp <nico.tromp@gmail.com>
2018-03-14 12:59:44 +0100

    Improve readability.

--- niro/src/main/scala/nl/uva/se/sc/niro/model/qls/QLStylesheet.scala
+++ niro/src/main/scala/nl/uva/se/sc/niro/model/qls/QLStylesheet.scala
@@ -1,8 +1,17 @@
 package nl.uva.se.sc.niro.model.qls
 
 case class QLStylesheet(name: String, pages: Seq[Page]) {
+  
   def getPageNamesWithQuestion(questionId: String): Seq[String] =
-    pages.filter(_.sections.filter(_.questions.filter(_.name == questionId).nonEmpty).nonEmpty).map(_.name)
+    pages.filter(containsSectionWithQuestion(_, questionId)).map(_.name)
+
+  private def containsSectionWithQuestion(page: Page, questionId: String) = {
+    page.sections.exists(containsQuestion(_, questionId))
+  }
+
+  private def containsQuestion(section: Section, questionId: String) = {
+    section.questions.exists(_.name == questionId)
+  }
 }
 
 case class Page(name: String, sections: Seq[Section])

3b1bd5680e733bd6923c6fcb2fc802ac81612eeb
Nico Tromp <nico.tromp@gmail.com>
2018-03-14 12:50:09 +0100

    Improve readability.

--- niro/src/main/scala/nl/uva/se/sc/niro/gui/factory/PageVisibilityFactory.scala
+++ niro/src/main/scala/nl/uva/se/sc/niro/gui/factory/PageVisibilityFactory.scala
@@ -1,14 +1,14 @@
 package nl.uva.se.sc.niro.gui.factory
 
-import nl.uva.se.sc.niro.model.ql.{ And, Eq, Or, Question }
+import nl.uva.se.sc.niro.model.ql.expressions.answers.{ BooleanAnswer, StringAnswer }
 import nl.uva.se.sc.niro.model.ql.expressions.{ BinaryOperation, Expression, Reference }
-import nl.uva.se.sc.niro.model.ql.expressions.answers.StringAnswer
+import nl.uva.se.sc.niro.model.ql.{ And, Eq, Or, Question }
 import nl.uva.se.sc.niro.model.qls.QLStylesheet
 
 object PageVisibilityFactory {
   val ACTIVE_PAGE_NAME = "__active_page_name__"
 
-  def createPageVisibility(question: Question, stylesheet: Option[QLStylesheet], visible: Expression) = {
+  def createPageVisibility(question: Question, stylesheet: Option[QLStylesheet], visible: Expression): Expression = {
     val pageNames = getPageNamesContainingQuestion(question, stylesheet)
     val pageNameChecks = createPageCheckExpressions(pageNames)
     createCumulativePageNameCheck(pageNameChecks).map(BinaryOperation(And, _, visible)).getOrElse(visible)
@@ -18,16 +18,13 @@ object PageVisibilityFactory {
     stylesheet.map(_.getPageNamesWithQuestion(question.id)).getOrElse(Seq.empty)
   }
 
-  private def createPageCheckExpressions(pageNames: Seq[String]): Seq[BinaryOperation] = {
+  private def createPageCheckExpressions(pageNames: Seq[String]): Seq[Expression] = {
     pageNames.map(pageName => BinaryOperation(Eq, Reference(ACTIVE_PAGE_NAME), StringAnswer(pageName)))
   }
 
-  private def createCumulativePageNameCheck(pageNameChecks: Seq[BinaryOperation]): Option[BinaryOperation] = {
-    pageNameChecks match {
-      case head +: Seq() => Some(head)
-      case head +: tail  => Some(tail.foldLeft(head)((l: Expression, r: Expression) => BinaryOperation(Or, l, r)))
-      case Seq()         => None
-    }
+  private def createCumulativePageNameCheck(pageNameChecks: Seq[Expression]): Option[Expression] = {
+    if (pageNameChecks.isEmpty) None
+    else Some(pageNameChecks.fold(BooleanAnswer(false))((l: Expression, r: Expression) => BinaryOperation(Or, l, r)))
   }
 
 }

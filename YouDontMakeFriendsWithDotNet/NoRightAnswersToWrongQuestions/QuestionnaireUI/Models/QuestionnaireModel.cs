be44473adfb3c6ae2a144e892ab5bf40e60b3ef5
jewelEarthDeveloper <paul_spencer85@hotmail.com>
2018-03-15 09:50:39 +0100

    add question wrapper

--- YouDontMakeFriendsWithDotNet/NoRightAnswersToWrongQuestions/QuestionnaireUI/Models/QuestionnaireModel.cs
+++ YouDontMakeFriendsWithDotNet/NoRightAnswersToWrongQuestions/QuestionnaireUI/Models/QuestionnaireModel.cs
@@ -5,8 +5,16 @@ namespace QuestionnaireUI.Models
 {
     public class QuestionnaireModel
     {
-        public Guid QuestionnaireId { get; set; }
-        public string QuestionnaireDisplayName { get; set; }
+        public QuestionnaireModel(
+            Guid questionnaireId, 
+            string questionnaireDisplayName)
+        {
+            QuestionnaireId = questionnaireId;
+            QuestionnaireDisplayName = questionnaireDisplayName;
+        }
+
+        public Guid QuestionnaireId { get; }
+        public string QuestionnaireDisplayName { get; }
         public IList<QuestionModel> Questions { get; set; }
             = new List<QuestionModel>();
     }

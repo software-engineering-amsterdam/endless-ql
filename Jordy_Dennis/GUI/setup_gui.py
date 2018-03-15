64d30987eba6996dc4ae9002216752e7474c0d57
Dennis Kruidenberg <kruidenbergdennis@gmail.com>
2018-03-15 13:11:46 +0100

    now showing basic qls in gui

--- Jordy_Dennis/GUI/setup_gui.py
+++ Jordy_Dennis/GUI/setup_gui.py
@@ -1,13 +1,13 @@
 """
     This class defines the overall gui. The created mainframe is the root of our application.
-    
+
     We can create a form, and add questions to the pages of the form. The questions are generated
     by the QuestionGenerator, which uses the AST to evaluate expressions and list all of the questions
     that need rendering.
 
     If you need to know more about the question generator, the questions or the forms, please navigate to
-    their respecting files and look in the comments, everything is explained. 
-    
+    their respecting files and look in the comments, everything is explained.
+
 """
 
 from .gui_imports import *
@@ -22,15 +22,17 @@ class Gui:
         The question generator is initialized first and then later set after the form is created.
         This is done because their is a circular dependency.
     """
-    def __init__(self, ast, qls=False):
+    def __init__(self, ast, astQLS=None):
         self.gui = Tk()
         self.mainframe = create_frame(self.gui, background='pink')
         self.mainframe.pack(expand=True, fill='both')
         self.form = None
         self.ast = ast
+        self.astQLS = astQLS
         self.varDict = ast.varDict
-        self.questionsGenerator = Question_Generator(self.varDict, self.ast, self.form)
-        self.form = FormGui(self.mainframe, self.questionsGenerator, self.ast.getName(), qls=qls)
+        self.questionsGenerator = Question_Generator(self.varDict, self.ast, self.astQLS, self.form)
+
+        self.form = FormGui(self.mainframe, self.questionsGenerator, self.ast.getName(), qls=astQLS!=None)
         self.questionsGenerator.form = self.form
         self.createForm()
         self.execute()
@@ -40,7 +42,8 @@ class Gui:
         and add a submit button which collects the answers
     """
     def createForm(self):
-        self.questionsGenerator.updateQuestions(True)
+        # self.questionsGenerator.updateQuestions(True)
+        self.questionsGenerator.qls()
         b = Button(self.mainframe, text="SUBMIT", command=self.collectAnswers)
         b.pack()
 

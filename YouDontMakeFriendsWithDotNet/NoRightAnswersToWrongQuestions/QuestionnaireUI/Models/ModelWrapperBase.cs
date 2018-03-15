b534479c5b61eb3168ec98f9eff0b2c7906a3a76
jewelEarthDeveloper <paul_spencer85@hotmail.com>
2018-03-15 11:10:39 +0100

    generalize the raise property changed event into the base class

--- YouDontMakeFriendsWithDotNet/NoRightAnswersToWrongQuestions/QuestionnaireUI/Models/ModelWrapperBase.cs
+++ YouDontMakeFriendsWithDotNet/NoRightAnswersToWrongQuestions/QuestionnaireUI/Models/ModelWrapperBase.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace QuestionnaireUI.Models
 {
@@ -15,5 +16,21 @@ namespace QuestionnaireUI.Models
         }
 
         public T Model { get; }
+
+        protected void SetValue<TValue>(
+            TValue value,
+            [CallerMemberName] string propertyName = null)
+        {
+            var propertyInfo = Model
+                .GetType()
+                .GetProperty(propertyName);
+            var currentValue = propertyInfo.GetValue(Model);
+            if (!Equals(currentValue, value))
+            {
+                propertyInfo.SetValue(Model, value);
+                RaisePropertyChanged(propertyName);
+            }
+        }
+
     }
 }

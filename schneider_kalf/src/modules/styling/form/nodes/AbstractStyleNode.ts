65536a798cde2749707cca32edfa34d3db67aa54
Meess <abcmaster8@hotmail.com>
2018-03-15 21:34:25 +0100

    FIX parent is undefined, temporary fix implemented but need refactoring

--- schneider_kalf/src/modules/styling/form/nodes/AbstractStyleNode.ts
+++ schneider_kalf/src/modules/styling/form/nodes/AbstractStyleNode.ts
@@ -12,7 +12,12 @@ export default abstract class AbstractStyleNode implements StyleTreeNode {
     const parents: StyleTreeNode[] = [];
     let parent: StyleTreeNode | null = this.parent;
 
+    // Can it actually be null? got error that stylesheet parent is undefined
     while (parent !== null) {
+
+      // TODO: Refactor so this isn't needed
+      if (parent === undefined) { break; }
+
       parents.push(parent);
       parent = parent.getParent();
     }
@@ -22,6 +27,10 @@ export default abstract class AbstractStyleNode implements StyleTreeNode {
 
   setParent(parent: StyleTreeNode): void {
     this.parent = parent;
+  }
+
+  setStyle() {
+    return;
   }
 
   abstract accept(visitor: StyleNodeVisitor);

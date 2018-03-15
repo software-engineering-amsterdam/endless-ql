eb9b387dcf2ff827f1e245d7000c9cf84f58c6e4
TerryvanWalen <github@terryvanwalen.nl>
2018-03-15 15:31:34 +0100

    Add computed_question to gui

--- nitpickers/pyql/pyql/gui/window.py
+++ nitpickers/pyql/pyql/gui/window.py
@@ -17,38 +17,37 @@ class Window(ttk.Frame):
         self.btn_update = ttk.Button(self, text='Update', command=self.btn_update)
         self.btn_update.grid(column=0, row=0, columnspan=4)
 
-        self._labels = []
-        self._widgets = {}
+        self._statements = {}
 
-    def add_question(self, identifier, text, widget):
-        current_row = len(self._widgets) + 1
+    def build_statement(self, identifier, text, widget, value):
+        current_row = len(self._statements) + 1
 
         question_label = ttk.Label(self, text=text, width=40)
         question_label.grid(column=0, row=current_row)
-        self._labels.append(question_label)
-
-        value = ""
-        try:
-            value = self._symbol_table.get(identifier)
-        except KeyError:
-            pass
 
         question_widget = widget(self, identifier, value)
         question_widget.grid(column=1, row=current_row)
-        self._widgets[identifier] = question_widget
+
+        return question_label, question_widget
+
+    def add_question(self, identifier, text, widget, value):
+        question_label, question_widget = self.build_statement(identifier, text, widget, value)
+        self._statements[identifier] = (question_label, question_widget)
+
+    def add_computed_question(self, identifier, text, widget, value):
+        computed_question_label, computed_question_widget = self.build_statement(identifier, text, widget, value)
+        computed_question_widget.configure(state='disabled')
+        self._statements[identifier] = (computed_question_label, computed_question_widget)
 
     def clear(self):
-        for l in self._labels:
+        for i, (l, w) in self._statements.items():
             l.destroy()
-        self._labels = []
-
-        for i, w in self._widgets.items():
             w.destroy()
-        self._widgets = {}
+        self._statements = {}
 
     def btn_update(self):
-        for identifier, w in self._widgets.items():
-            widget_value = w.get()
+        for identifier, (_, widget) in self._statements.items():
+            widget_value = widget.get()
             if widget_value is not None:
                 self._symbol_table.update_or_create(identifier, widget_value)
         self._gui_visitor.build()

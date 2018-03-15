d9c98bf291da802e8a21eef00a21ecd6309135fd
jewelEarthDeveloper <paul_spencer85@hotmail.com>
2018-03-15 19:09:37 +0100

    spike viewmodel

---
+++ YouDontMakeFriendsWithDotNet/NoRightAnswersToWrongQuestions/QuestionaireOrchestration/Commands/UpdateValuesCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using QuestionaireOrchestration.Models;
+using QuestionnaireInfrastructure.API;
+
+namespace QuestionaireOrchestration.Commands
+{
+    public class UpdateValuesCommand : ICommandMessage
+    {
+        public QuestionnaireModel Questionnaire;
+    }
+}

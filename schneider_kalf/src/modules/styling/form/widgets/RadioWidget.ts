cbd95e723db7ff8a90550dbd518fe35cb68504ab
Meess <abcmaster8@hotmail.com>
2018-03-15 22:41:03 +0100

    FIX whitespace for lint

--- schneider_kalf/src/modules/styling/form/widgets/RadioWidget.ts
+++ schneider_kalf/src/modules/styling/form/widgets/RadioWidget.ts
@@ -1,6 +1,6 @@
 import Widget from "./Widget";
 
-export default class RadioWidget implements Widget{
+export default class RadioWidget implements Widget {
   checkAttributeType(value: string): any {
     return undefined;
   }

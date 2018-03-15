33f1bf0d945e1f5ad2dc764a5af503c80e05af3e
carlyhill6895 <carly6895@gmail.com>
2018-03-15 12:31:56 +0100

    refactoring part 4: parse factory -> parse service

--- Carly_Michael/jectacular-ql/src/app/services/parse.service.ts
+++ Carly_Michael/jectacular-ql/src/app/services/parse.service.ts
@@ -1,4 +1,5 @@
-import {Form} from '../domain/ast/ql/index';
+import { Injectable } from '@angular/core';
+import {Form} from '../domain/ast/ql';
 import {Stylesheet} from '../domain/ast/qls';
 import {parse} from '../../parser/ql-parser';
 import {parse as parseQls} from '../../parser/qls-parser';
@@ -7,17 +8,22 @@ export class ParseResult {
   constructor(readonly formName: string, readonly form: Form, readonly styles: Stylesheet) { }
 }
 
-export class ParseFactory {
-  static parse(qlInput: string, qlsInput: string): ParseResult {
+@Injectable()
+export class ParseService {
+
+  parse(qlInput: string, qlsInput: string): ParseResult {
     let astQls: Stylesheet;
 
     const astQl: Form = parse(qlInput, {});
-    // check form
+
+    // static type checking
     astQl.checkForm();
 
+    // only parse qls if there is valid input
     if (qlsInput && qlsInput.trim().length > 0 ) {
       astQls = parseQls(qlsInput, {});
-      console.log(astQls);
+
+      // static type checking on stylesheet
       astQls.checkStylesheet([], astQl.getAllQuestions());
     }
     return new ParseResult(astQl.name, astQl, astQls);

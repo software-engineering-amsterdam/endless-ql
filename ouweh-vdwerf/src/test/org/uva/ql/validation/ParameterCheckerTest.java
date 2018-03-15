1736288782259df11f6424541b8bc4ac00a43492
Edwin <edwin_ou@hotmail.com>
2018-03-15 10:37:11 +0100

    Tests reformat and import cleanup

--- ouweh-vdwerf/src/test/org/uva/ql/validation/ParameterCheckerTest.java
+++ ouweh-vdwerf/src/test/org/uva/ql/validation/ParameterCheckerTest.java
@@ -5,18 +5,17 @@ import org.junit.Test;
 import org.uva.ql.ast.CalculatedQuestion;
 import org.uva.ql.ast.Form;
 import org.uva.ql.ast.Statement;
-import org.uva.ql.ast.expression.unary.BooleanLiteral;
 import org.uva.ql.ast.expression.unary.Parameter;
 import org.uva.ql.ast.type.BooleanType;
 import org.uva.ql.ast.type.IntegerType;
-import org.uva.ql.ast.type.StringType;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class ParameterCheckerTest {
 
@@ -33,7 +32,7 @@ public class ParameterCheckerTest {
     @Test
     public void runCheckEmptySymbolTable() {
         ArrayList<Statement> statements;
-        statements= new ArrayList<>(Arrays.asList(
+        statements = new ArrayList<>(Arrays.asList(
                 new CalculatedQuestion(
                         "name",
                         "content",
@@ -52,7 +51,7 @@ public class ParameterCheckerTest {
     @Test
     public void runCheckInSymbolTable() {
         ArrayList<Statement> statements;
-        statements= new ArrayList<>(Arrays.asList(
+        statements = new ArrayList<>(Arrays.asList(
                 new CalculatedQuestion(
                         "name",
                         "content",

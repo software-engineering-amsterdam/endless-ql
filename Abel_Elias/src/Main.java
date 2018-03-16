bdeb7b6ce55ff3bf5c230a0742ab4d5fbe008b58
Unknown <ajmkaandorp@hotmail.com>
2018-03-16 01:07:20 +0100

    Changed main class structure, added tests

--- Abel_Elias/src/Main.java
+++ Abel_Elias/src/Main.java
@@ -1,7 +1,9 @@
 import classes.Question;
 import gui.FormBuilder;
+import jdk.nashorn.api.tree.Tree;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import parsing.TreeBuilder;
 import parsing.visitors.BaseVisitor;
 import parsing.gen.QLLexer;
 import parsing.gen.QLParser;
@@ -20,18 +22,11 @@ public class Main {
      * parse and build the form file
      * @param inputStream - input stream of the given form file
      */
-    public void parseAndBuild(InputStream inputStream){
+
+    private void parseAndBuild(InputStream inputStream){
         try{
-            //Call the lexer and get the tokens
-            QLLexer lexer = new QLLexer(CharStreams.fromStream(inputStream));
-            CommonTokenStream tokens = new CommonTokenStream(lexer);
 
-            //Parse the tokens/tree
-            QLParser parser = new QLParser(tokens);
-            QLParser.FormContext tree = parser.form();
-
-            //Call the visitor and build the tree
-            InitVisitor builder = new InitVisitor(tree);
+            InitVisitor builder = new InitVisitor(new TreeBuilder().build(inputStream));
             HashMap<String, Question> memory = builder.getQuestions();
 
             //Test output
@@ -41,18 +36,6 @@ public class Main {
                   System.out.println(pair.getKey() + " : " + pair.getValue() + " = " + ((Question) pair.getValue()).getValue());
                   it.remove();
             }
-            //System.out.println("done");
-
-            //Construct the form
-            //ParseTree parseTree = parser.form();
-            //Form form = (Form) parseTree.accept(builder);
-
-            //Call parse tree inspector: Show the tree
-            //Trees.inspect(tree, parser);
-
-            //Do typechecking
-            //TypeChecker typeChecker = new TypeChecker();
-            //typeChecker.initTypeChecking(form);
 
             //Pass the relevant questions to the UI builder
             FormBuilder formBuilder = new FormBuilder(builder, memory);

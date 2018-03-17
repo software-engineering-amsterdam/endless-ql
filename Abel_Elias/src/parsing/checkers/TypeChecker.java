291c7d1dc42b96b731481835f4fb0be0dc3a8cbd
Elias <elias.el.khaldi@gmail.com>
2018-03-17 01:45:06 +0100

    fixing typeChecker

--- Abel_Elias/src/parsing/checkers/TypeChecker.java
+++ Abel_Elias/src/parsing/checkers/TypeChecker.java
@@ -1,37 +1,69 @@
 package parsing.checkers;
 
+import classes.Question;
 import classes.values.BooleanValue;
 import classes.values.NumericValue;
 import classes.values.Value;
 import parsing.checkers.errors.TypeMismatchError;
+import parsing.gen.QLBaseVisitor;
 import parsing.gen.QLParser;
+import parsing.visitors.TypeVisitor;
+import parsing.visitors.expressions.ExpressionVisitor;
 import parsing.visitors.refactor_tmp.BaseVisitor;
 
-public class TypeChecker extends BaseVisitor {
+import java.util.Date;
+import java.util.HashMap;
+
+public class TypeChecker extends QLBaseVisitor {
     // Typechecker checks if there are any inconsistensies in the types that were given in the syntax of the code
-    public TypeChecker(QLParser.FormContext ctx){
-        initQuestionMap();
+    private ExpressionVisitor expressionVisitor;
+    private TypeVisitor typeVisitor;
+    private HashMap<String, Question> questionMap;
+
+    public TypeChecker(QLParser.FormContext ctx, HashMap<String, Question> questionMap) {
+        this.questionMap = questionMap;
+        this.expressionVisitor = new ExpressionVisitor(questionMap);
+        this.typeVisitor = new TypeVisitor();
+
         visitBlock(ctx.block());
     }
 
     @Override
     public Object visitFixedQuestion(QLParser.FixedQuestionContext ctx) {
         String id = ctx.IDENTIFIER().getText();
-        Value expectedType = (Value) visit(ctx.type());
-        Class valueType = visit(ctx.expression()).getClass();
+        String type = ctx.type().getText();
+        Object value = expressionVisitor.visitExpression(ctx.expression());
+        boolean correctMatch = false;
 
-//        if(!haveSameType(valueType, expectedType)){
-//            throw new TypeMismatchError(id, ctx.type().getText());
-//        }
+        switch (type) {
+            case Value.MONEY:
+            case Value.DECIMAL:
+            case Value.INTEGER:
+                correctMatch = isNumeric(value);
+                break;
+            case Value.DATE:
+                correctMatch = isDate(value);
+                break;
+            case Value.STRING:
+                correctMatch = isString(value);
+                break;
+            case Value.BOOLEAN:
+                correctMatch = isBoolean(value);
+                break;
+        }
 
-        return super.visitFixedQuestion(ctx);
+        if (!correctMatch) {
+            throw new TypeMismatchError(id, ctx.type().getText());
+        }
+
+        return questionMap;
     }
 
     @Override
     public Object visitBoolIdentifier(QLParser.BoolIdentifierContext ctx) {
         String id = ctx.getText();
 
-        if(!haveSameType(BooleanValue.class, getQuestion(id).getValue().getClass())){
+        if (!questionMap.get(id).getValue().getType().equals(Value.BOOLEAN)) {
             throw new TypeMismatchError(id, "boolean");
         }
 
@@ -39,17 +71,29 @@ public class TypeChecker extends BaseVisitor {
     }
 
     @Override
-    public Double visitNumIdentifier(QLParser.NumIdentifierContext ctx) {
+    public Object visitNumIdentifier(QLParser.NumIdentifierContext ctx) {
         String id = ctx.getText();
 
-        if(!haveSameType(NumericValue.class, getQuestion(id).getValue().getClass())){
+        if (!isNumeric(questionMap.get(id).getValue().getValue())){
             throw new TypeMismatchError(id, "number");
         }
 
-        return super.visitNumIdentifier(ctx);
+        return questionMap;
     }
 
-    private boolean haveSameType(Class value, Class type){
-        return value.isAssignableFrom(type);
+    private boolean isNumeric(Object value) {
+        return value.getClass().isAssignableFrom(Number.class);
+    }
+
+    private boolean isBoolean(Object value) {
+        return value instanceof Boolean;
+    }
+
+    private boolean isDate(Object value) {
+        return value instanceof Date;
+    }
+
+    private boolean isString(Object value) {
+        return value instanceof String;
     }
 }

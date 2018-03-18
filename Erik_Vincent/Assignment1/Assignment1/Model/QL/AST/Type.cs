82be8e8d5fff04bd9116eb73811b9b884566eca3
evanscharrenburg <evanscharrenburg@users.noreply.github.com>
2018-03-18 11:45:43 +0100

    Modify QLS AST based on feedback.

--- Erik_Vincent/Assignment1/Assignment1/Model/QL/AST/Type.cs
+++ Erik_Vincent/Assignment1/Assignment1/Model/QL/AST/Type.cs
@@ -6,6 +6,7 @@
         Integer,
         String,
         Date,
-        Decimal
+        Decimal,
+        Money
     }
 }
---
+++ Erik_Vincent/Assignment1/Assignment1/Model/QLS/AST/Style/Widget/YesNoWidget.cs
@@ -0,0 +1,16 @@
+﻿namespace Assignment1.Model.QLS.AST.Style.Widget
+{
+    public abstract class YesNoWidget : IWidget
+    {
+        public string Yes { get; }
+        public string No { get; }
+
+        protected YesNoWidget(string yes, string no)
+        {
+            Yes = yes;
+            No = no;
+        }
+
+        public abstract void Accept(IStyleVisitor visitor);
+    }
+}

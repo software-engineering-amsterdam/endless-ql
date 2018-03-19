dc69b5100796b252958fd42a1fa1d8f2147ed2db
MichielBoswijk <michiel.boswijk@gmail.com>
2018-03-19 15:28:15 +0100

    Add visits for date nodes

--- PyQuest/src/ql/ast/visitors/dependency_visitor.py
+++ PyQuest/src/ql/ast/visitors/dependency_visitor.py
@@ -16,6 +16,7 @@ from ql.ast.expressions.binary_operators.or_node import OrOperatorNode
 from ql.ast.expressions.binary_operators.subtraction_node import SubtractionOperatorNode
 from ql.ast.expressions.literals.integer_node import IntegerNode
 from ql.ast.expressions.literals.decimal_node import DecimalNode
+from ql.ast.expressions.literals.date_node import DateNode
 from ql.ast.visitors.visitor_helper import on, when
 
 
@@ -119,6 +120,10 @@ class DependencyVisitor(object):
         pass
 
     @when(DecimalNode)
+    def visit(self, node):
+        pass
+
+    @when(DateNode)
     def visit(self, node):
         pass
 

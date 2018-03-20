5c488d2df9fcf09919df855abbc7a0a3bd26bc1a
piotrkosytorz <piotr.kosytorz@gmail.com>
2018-03-20 15:45:45 +0100

    migrated to gradle

--- piotr/org.uva.sc.piotr.ql/src/main/java/Main.java
+++ piotr/org.uva.sc.piotr.ql/src/main/java/Main.java
@@ -3,7 +3,7 @@ package main;
 import gui.QLGui;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
         // the whole program runs in a GUI
         new QLGui();

0837bfbb2f8bd3f5b8b78d6116da1ba3c9e40e3c
rmathijn <rocco.mathijnandela@dhl.com>
2018-03-20 22:57:26 +0100

    Order answer types by alphabet

--- niro/src/main/scala/nl/uva/se/sc/niro/model/ql/AnswerTypes.scala
+++ niro/src/main/scala/nl/uva/se/sc/niro/model/ql/AnswerTypes.scala
@@ -11,46 +11,46 @@ case object BooleanType extends AnswerType {
   def typeOf(operator: Operator): Either[TypeCheckError, AnswerType] = operator match {
     case _: BooleanOperator => BooleanType.asRight
     case _: LogicalOperator => BooleanType.asRight
-    case _ => TypeCheckError(message = s"Operand: $BooleanType of invalid type to operator: $operator").asLeft
-  }
-}
-
-case object IntegerType extends AnswerType {
-  def typeOf(operator: Operator): Either[TypeCheckError, AnswerType] = operator match {
-    case _: ArithmeticOperator => IntegerType.asRight
-    case _: BooleanOperator => BooleanType.asRight
-    case _ => TypeCheckError(message = s"Operand: $IntegerType of invalid type to operator: $operator").asLeft
-  }
-}
-
-case object StringType extends AnswerType {
-  def typeOf(operator: Operator): Either[TypeCheckError, AnswerType] = operator match {
-    case _: BooleanOperator => BooleanType.asRight
-    case _ => TypeCheckError(message = s"Operand: $StringType of invalid type to operator: $operator").asLeft
-  }
-}
-
-case object DecimalType extends AnswerType {
-  def typeOf(operator: Operator): Either[TypeCheckError, AnswerType] = operator match {
-    case _: ArithmeticOperator => DecimalType.asRight
-    case _: BooleanOperator => BooleanType.asRight
-    case _ => TypeCheckError(message = s"Operand: $DecimalType of invalid type to operator: $operator").asLeft
-  }
-}
-
-case object MoneyType extends AnswerType {
-  def typeOf(operator: Operator): Either[TypeCheckError, AnswerType] = operator match {
-    case _: ArithmeticOperator => MoneyType.asRight
-    case _: BooleanOperator => BooleanType.asRight
-    case _ => TypeCheckError(message = s"Operand: $MoneyType of invalid type to operator: $operator").asLeft
+    case _                  => TypeCheckError(message = s"Operand: $BooleanType of invalid type to operator: $operator").asLeft
   }
 }
 
 case object DateType extends AnswerType {
   def typeOf(operator: Operator): Either[TypeCheckError, AnswerType] = operator match {
     case _: ArithmeticOperator => DateType.asRight
+    case _: BooleanOperator    => BooleanType.asRight
+    case _                     => TypeCheckError(message = s"Operand: $DateType of invalid type to operator: $operator").asLeft
+  }
+}
+
+case object DecimalType extends AnswerType {
+  def typeOf(operator: Operator): Either[TypeCheckError, AnswerType] = operator match {
+    case _: ArithmeticOperator => DecimalType.asRight
+    case _: BooleanOperator    => BooleanType.asRight
+    case _                     => TypeCheckError(message = s"Operand: $DecimalType of invalid type to operator: $operator").asLeft
+  }
+}
+
+case object IntegerType extends AnswerType {
+  def typeOf(operator: Operator): Either[TypeCheckError, AnswerType] = operator match {
+    case _: ArithmeticOperator => IntegerType.asRight
+    case _: BooleanOperator    => BooleanType.asRight
+    case _                     => TypeCheckError(message = s"Operand: $IntegerType of invalid type to operator: $operator").asLeft
+  }
+}
+
+case object MoneyType extends AnswerType {
+  def typeOf(operator: Operator): Either[TypeCheckError, AnswerType] = operator match {
+    case _: ArithmeticOperator => MoneyType.asRight
+    case _: BooleanOperator    => BooleanType.asRight
+    case _                     => TypeCheckError(message = s"Operand: $MoneyType of invalid type to operator: $operator").asLeft
+  }
+}
+
+case object StringType extends AnswerType {
+  def typeOf(operator: Operator): Either[TypeCheckError, AnswerType] = operator match {
     case _: BooleanOperator => BooleanType.asRight
-    case _ => TypeCheckError(message = s"Operand: $DateType of invalid type to operator: $operator").asLeft
+    case _                  => TypeCheckError(message = s"Operand: $StringType of invalid type to operator: $operator").asLeft
   }
 }
 

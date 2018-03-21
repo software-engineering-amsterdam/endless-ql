7bf596c4422f755d82b603b1a912a0db28cfc5c4
Remi van Veen <remivveen@gmail.com>
2018-03-21 14:08:14 +0100

    add duplicate label warnings

---
+++ Remi_Hasan/src/main/java/ql/analysis/DuplicateLabelDetector.java
@@ -0,0 +1,51 @@
+package ql.analysis;
+
+import ql.model.Form;
+import ql.model.Question;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class DuplicateLabelDetector {
+
+    private final Form form;
+
+    public DuplicateLabelDetector(Form form) {
+        this.form = form;
+    }
+
+    public Set<String> getDuplicateLabelWarnings() {
+        // Map label to corresponding questions
+        Map<String, Set<String>> questionsPerLabel = this.getQuestionsPerLabel();
+
+        Set<String> warnings = new HashSet<>();
+        for(Map.Entry<String, Set<String>> entry : questionsPerLabel.entrySet()) {
+            if(entry.getValue().size() == 1) {
+                continue;
+            }
+
+            warnings.add("Label + \"" + entry.getKey() + "\" used for multiple questions: " + entry.getValue());
+        }
+
+        return warnings;
+    }
+
+    private Map<String, Set<String>> getQuestionsPerLabel() {
+        Map<String, Set<String>> questionsPerLabel = new HashMap<>();
+
+        for(Question question : form.questions) {
+            Set<String> questionIdentifiers = new HashSet<>();
+            if(questionsPerLabel.containsKey(question.text)) {
+                questionIdentifiers = questionsPerLabel.get(question.text);
+            }
+
+            // Save question identifiers with their location, for reporting purposes
+            questionIdentifiers.add(question.name + " " + question.getLocation());
+            questionsPerLabel.put(question.text, questionIdentifiers);
+        }
+
+        return questionsPerLabel;
+    }
+}

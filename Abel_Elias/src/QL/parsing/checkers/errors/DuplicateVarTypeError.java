cf2440d0bbf3a435d803121ce673310948c4c41b
Elias <elias.el.khaldi@gmail.com>
2018-03-21 14:37:32 +0100

    typo

--- Abel_Elias/src/QL/parsing/checkers/errors/DuplicateVarTypeError.java
+++ Abel_Elias/src/QL/parsing/checkers/errors/DuplicateVarTypeError.java
@@ -2,6 +2,6 @@ package QL.parsing.checkers.errors;
 
 public class DuplicateVarTypeError extends Error {
     public DuplicateVarTypeError(String variableID){
-        super("The variable " + variableID + " was declared more than once with deifferent types");
+        super("The variable " + variableID + " was declared more than once with different types");
     }
 }

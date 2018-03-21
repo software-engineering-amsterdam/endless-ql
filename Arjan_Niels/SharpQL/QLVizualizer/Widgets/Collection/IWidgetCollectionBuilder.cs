75bbfd97e88865aa74af1e492115769c5a41ea1a
GrimGerbil <meijerarjan@live.nl>
2018-03-21 17:19:07 +0100

    Re-implemented widgetBuilder base

---
+++ Arjan_Niels/SharpQL/QLVizualizer/Widgets/Collection/IWidgetCollectionBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLVisualizer.Widgets.Collection
+{
+    public interface IWidgetCollectionBuilder<T> : IWidgetBuilder<T>
+    {
+        void AddChild(WidgetBuilder<T> builder);
+    }
+}
---
+++ Arjan_Niels/SharpQL/QLVizualizer/Widgets/IWidgetBuilder.cs
@@ -0,0 +1,15 @@
+﻿using QLParser.AST.QLS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLVisualizer.Widgets
+{
+    public interface IWidgetBuilder<T>
+    {
+        T Create();
+        void SetParentStyle(List<QLSValue> elements);
+    }
+}

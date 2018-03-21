7e517cd6adb0a1f78a73e792cfa783e10fbc9889
Nico Tromp <nico.tromp@gmail.com>
2018-03-21 07:53:13 +0100

    Fix type-o.

--- niro/src/main/scala/nl/uva/se/sc/niro/model/gui/Styling.scala
+++ niro/src/main/scala/nl/uva/se/sc/niro/model/gui/Styling.scala
@@ -5,14 +5,14 @@ import nl.uva.se.sc.niro.model.qls._
 class Styling
 
 case class Default() extends Styling
-case class SpingBox() extends Styling
+case class SpinBox() extends Styling
 case class ComboBox(trueLabel: String, falseLabel: String) extends Styling
 case class Radio(trueLabel: String, falseLabel: String) extends Styling
 
 object Styling {
   def apply(widgetType: QLSWidgetType): Styling = {
     widgetType match {
-      case QLSSpinBox()                       => SpingBox()
+      case QLSSpinBox()                       => SpinBox()
       case QLSComboBox(trueValue, falseValue) => ComboBox(trueValue, falseValue)
       case QLSRadio(trueValue, falseValue)    => Radio(trueValue, falseValue)
       case _                                  => Default()

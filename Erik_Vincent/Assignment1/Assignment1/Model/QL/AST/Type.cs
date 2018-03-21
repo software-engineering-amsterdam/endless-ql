9e43c133b4a65b815cfc436a2d369d13403af115
V-Jong <vince-ny@hotmail.com>
2018-03-21 10:43:59 +0100

    Type checking refactoring

--- Erik_Vincent/Assignment1/Assignment1/Model/QL/AST/Type.cs
+++ Erik_Vincent/Assignment1/Assignment1/Model/QL/AST/Type.cs
@@ -10,4 +10,36 @@
         Money,
         Undefined
     }
+
+    public static class TypeMethods
+    {
+        public static bool IsNumeric(this Type at) => at == Type.Integer || at == Type.Money || at == Type.Decimal;
+
+        public static Type InferArithmeticType(Type leftType, Type rightType)
+        {
+            if (leftType == Type.Integer)
+            {
+                switch (rightType)
+                {
+                    case Type.Integer: return Type.Integer;
+                    case Type.Money: return Type.Money;
+                    case Type.Decimal: return Type.Decimal;
+                }
+            }
+            if (leftType == Type.Decimal)
+            {
+                switch (rightType)
+                {
+                    case Type.Integer: return Type.Decimal;
+                    case Type.Decimal: return Type.Decimal;
+                    case Type.Money: return Type.Money;
+                }
+            }
+            if (leftType == Type.Money)
+            {
+                return Type.Money;
+            }
+            return Type.Undefined;
+        }
+    }
 }

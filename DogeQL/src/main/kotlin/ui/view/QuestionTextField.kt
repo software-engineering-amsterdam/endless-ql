95f07b58ebe479b6d99c303c6d9feaff685aa91c
sangamm <sangamgupta10@gmail.com>
2018-03-21 20:04:17 +0100

    Allow negative integers in UI and fix readonly update

--- DogeQL/src/main/kotlin/ui/view/QuestionTextField.kt
+++ DogeQL/src/main/kotlin/ui/view/QuestionTextField.kt
@@ -2,6 +2,7 @@ package ui.view
 
 import data.question.SymbolType
 import javafx.scene.control.TextField
+import javafx.scene.control.TextInputControl
 import javafx.util.converter.BigDecimalStringConverter
 import tornadofx.*
 import ui.model.QuestionModel
@@ -30,8 +31,17 @@ class QuestionTextField(question: QuestionModel) : TextField() {
             else -> throw IllegalArgumentException("${question.item.value.type} unsupported type")
         }
 
-        textProperty().addListener({ _, _, _ ->
-            question.update()
+        textProperty().addListener({ _, _, newValue ->
+            if (newValue.isNotBlank()) {
+                question.update()
+            }
         })
     }
+
+    private fun TextInputControl.stripNonNumeric(vararg allowedChars: String = arrayOf(".", ",", "-")) = textProperty().mutateOnChange {
+        it?.replace(Regex("[^0-9${allowedChars.joinToString("")}]"), "")
+    }
+
+    private fun TextInputControl.stripNonInteger() = textProperty().mutateOnChange { it?.replace(Regex("[^0-9\\-]"), "") }
+
 }

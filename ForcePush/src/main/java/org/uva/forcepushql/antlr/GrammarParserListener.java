7c2bdcf82b2cbb3cf643251e42ffb3f29349709e
Joana Correia Magalhães Sousa <jsousa039@gmail.com>
2018-03-21 12:07:05 +0100

    Fixed ANTLR problem
    Deleted duplicate token file which confused compiler;
    Refactored some classes;
    
    Co-Authored-By: George Vletsas <georgevle93@gmail.com>

--- ForcePush/src/main/java/org/uva/forcepushql/antlr/GrammarParserListener.java
+++ ForcePush/src/main/java/org/uva/forcepushql/antlr/GrammarParserListener.java
@@ -1,4 +1,4 @@
-// Generated from C:/Users/georg/Documents/GitHub/endless-ql/ForcePush/src/main/resources/antlr\GrammarParser.g4 by ANTLR 4.7
+// Generated from C:/Users/Joana Magalhães/Documents/GitHub/endless-ql/ForcePush/src/main/resources/antlr\GrammarParser.g4 by ANTLR 4.7
 package org.uva.forcepushql.antlr;
 import org.antlr.v4.runtime.tree.ParseTreeListener;
 

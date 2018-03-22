ad3538ee27b4943891c66b01b84f32785321a78d
GrimGerbil <meijerarjan@live.nl>
2018-03-22 15:38:44 +0100

    Added windowsBuilders

---
+++ Arjan_Niels/SharpQL/QLVizualizer/Widgets/Windows/Leaf/DoubleBuilderWindows.cs
@@ -0,0 +1,24 @@
+﻿using QLParser.AST.QLS;
+using QLVisualizer.Elements.Managers.LeafTypes;
+using QLVisualizer.Widgets.Collection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLVisualizer.Widgets.Windows.Leaf
+{
+    public class DoubleBuilderWindows : WidgetLeafBuilderWindows<DoubleQuestionManager>
+    {
+        public DoubleBuilderWindows(List<QLSValue> qlsElements, QLSWidgetSpecification widgetSpecification, DoubleQuestionManager elementManagerLeaf, IWidgetCollectionBuilder<Control> parent) : base(qlsElements, widgetSpecification, elementManagerLeaf, parent)
+        {
+        }
+
+        public override Control Create()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

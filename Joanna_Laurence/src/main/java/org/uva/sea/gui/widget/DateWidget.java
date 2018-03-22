0098ee7c99f3c37f1a586a0b919e9c41b39b8dc7
Joanna Roczniak <roczniak.joanna@gmail.com>
2018-03-22 17:29:58 +0100

    update questioner after setting a date from a date picker

--- Joanna_Laurence/src/main/java/org/uva/sea/gui/widget/DateWidget.java
+++ Joanna_Laurence/src/main/java/org/uva/sea/gui/widget/DateWidget.java
@@ -4,11 +4,13 @@ import javafx.scene.Node;
 import javafx.scene.control.DatePicker;
 import org.uva.sea.languages.ql.interpreter.dataObject.questionData.QuestionData;
 import org.uva.sea.languages.ql.interpreter.evaluate.valueTypes.DateValue;
+import org.uva.sea.languages.ql.interpreter.evaluate.valueTypes.Value;
 
 import java.time.LocalDate;
 import java.util.Calendar;
 
 public class DateWidget extends Widget {
+
     private DateValue widgetValue = new DateValue(Calendar.getInstance());
 
     public DateWidget(QuestionData questionData) {
@@ -25,9 +27,16 @@ public class DateWidget extends Widget {
     public Node convertToGuiNode() {
         DatePicker datePicker = new DatePicker();
 
+        datePicker.getEditor().setText(this.widgetValue.toString());
+
         datePicker.setOnAction(event -> {
-            LocalDate date = datePicker.getValue();
-            System.out.println("Selected date: " + date);
+            LocalDate localDate = datePicker.getValue();
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.DAY_OF_MONTH, localDate.getDayOfMonth());
+            calendar.set(Calendar.MONTH, localDate.getMonthValue());
+            calendar.set(Calendar.YEAR, localDate.getYear());
+            Value newValue = new DateValue(calendar);
+            this.sendUpdateValueEvent(this.questionData.getQuestionName(), newValue);
         });
 
         return datePicker;

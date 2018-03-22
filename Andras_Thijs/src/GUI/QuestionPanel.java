4250efb2f2c72612cf66b3cf1565a45297d27d23
AHerczeg <h.herczeg.andras@gmail.com>
2018-03-22 11:27:19 +0100

    QuestionPanel now can update its term

--- Andras_Thijs/src/GUI/QuestionPanel.java
+++ Andras_Thijs/src/GUI/QuestionPanel.java
@@ -2,25 +2,37 @@ package GUI;
 
 import GUI.Listeners.RefreshListener;
 import Nodes.Question;
+import Nodes.Term.TermFactory;
+import QLExceptions.SyntaxException;
+import QLExceptions.TypeException;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class QuestionPanel extends JPanel{
     private Question question;
     private boolean isAvailable;
+    private Widget widget;
 
     public QuestionPanel (Question question, RefreshListener listener){
         this.question = question;
         this.setName(question.getName());
         this.isAvailable = question.isAvailable();
         this.add(new JLabel(question.getLabel()));
-        Component component = new ComponentFactory().getComponent(this.question.getType(), listener);
-        this.add(component);
+        this.widget = new WidgetFactory().getWidget(this.question.getType(), listener);
+        this.add(widget.getComponent());
 
+    }
+
+
+    public void updateTerm() throws SyntaxException, TypeException {
+        this.question.updateTerm(new TermFactory().getTerm(this.widget.getValue()));
     }
 
     public void setVisibility(){
         this.isAvailable = question.isAvailable();
         this.setVisible(this.isAvailable);
     }
+
+
 }

f7b58eb43291c333cc989e40645e9472a365d258
rmathijn <rocco.mathijnandela@dhl.com>
2018-03-22 13:54:52 +0100

    Let money arithmetics accept generic answer type

--- niro/src/main/scala/nl/uva/se/sc/niro/model/ql/expressions/MoneyArithmetics.scala
+++ niro/src/main/scala/nl/uva/se/sc/niro/model/ql/expressions/MoneyArithmetics.scala
@@ -1,19 +1,23 @@
 package nl.uva.se.sc.niro.model.ql.expressions
 
-import nl.uva.se.sc.niro.model.ql.expressions.answers.{ DecimalAnswer, IntegerAnswer, MoneyAnswer }
+import nl.uva.se.sc.niro.model.ql.expressions.answers.{ Answer, DecimalAnswer, IntegerAnswer, MoneyAnswer }
 
 import scala.language.implicitConversions
 
 trait MoneyCanDoArithmetics {
-  def plus(x: MoneyAnswer, y: MoneyAnswer): MoneyAnswer = MoneyAnswer(x.value + y.value)
-  def minus(x: MoneyAnswer, y: MoneyAnswer): MoneyAnswer = MoneyAnswer(x.value - y.value)
-  def times(x: MoneyAnswer, y: IntegerAnswer): MoneyAnswer = MoneyAnswer(x.value * y.value)
-  def times(x: IntegerAnswer, y: MoneyAnswer): MoneyAnswer = times(y, x)
-  def times(x: MoneyAnswer, y: DecimalAnswer): MoneyAnswer = MoneyAnswer(x.value * y.value)
-  def times(x: DecimalAnswer, y: MoneyAnswer): MoneyAnswer = times(y, x)
-  def div(x: MoneyAnswer, y: MoneyAnswer): DecimalAnswer = DecimalAnswer(x.value / y.value)
-  def div(x: MoneyAnswer, y: IntegerAnswer): MoneyAnswer = MoneyAnswer(x.value / y.value)
-  def div(x: MoneyAnswer, y: DecimalAnswer): MoneyAnswer = MoneyAnswer(x.value / y.value)
+  def plus(x: MoneyAnswer, y: Answer): MoneyAnswer = y match { case m: MoneyAnswer  => MoneyAnswer(x.value + m.value) }
+  def minus(x: MoneyAnswer, y: Answer): MoneyAnswer = y match { case m: MoneyAnswer => MoneyAnswer(x.value - m.value) }
+  def times(x: MoneyAnswer, y: Answer): MoneyAnswer = y match {
+    case i: IntegerAnswer => MoneyAnswer(x.value * i.value)
+    case d: DecimalAnswer => MoneyAnswer(x.value * d.value)
+  }
+  def times(x: IntegerAnswer, y: Answer): MoneyAnswer = y match { case m: MoneyAnswer => times(m, x) }
+  def times(x: DecimalAnswer, y: Answer): MoneyAnswer = y match { case m: MoneyAnswer => times(m, x) }
+  def div(x: MoneyAnswer, y: Answer): Answer = y match {
+    case m: MoneyAnswer   => DecimalAnswer(x.value / m.value)
+    case i: IntegerAnswer => MoneyAnswer(x.value / i.value)
+    case d: DecimalAnswer => MoneyAnswer(x.value / d.value)
+  }
   def negate(x: MoneyAnswer): MoneyAnswer = MoneyAnswer(-x.value)
 }
 

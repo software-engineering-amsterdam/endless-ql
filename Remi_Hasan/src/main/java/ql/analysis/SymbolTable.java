522257cdb3d057b2edb1d76567998fdf466b3a1c
Remi van Veen <remivveen@gmail.com>
2018-03-22 20:50:30 +0100

    removed symbol table listener

--- Remi_Hasan/src/main/java/ql/analysis/SymbolTable.java
+++ Remi_Hasan/src/main/java/ql/analysis/SymbolTable.java
@@ -1,6 +1,5 @@
 package ql.analysis;
 
-import javafx.util.Pair;
 import ql.evaluation.ExpressionEvaluator;
 import ql.evaluation.value.Value;
 import ql.model.Form;
@@ -8,25 +7,20 @@ import ql.model.Question;
 import ql.model.expression.Expression;
 import ql.model.expression.variable.ExpressionVariableUndefined;
 
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class SymbolTable {
     private Map<String, Expression> table;
-    private List<ChangeListener> listeners;
 
     public SymbolTable() {
         this.table = new HashMap<>();
-        this.listeners = new ArrayList<>();
     }
 
     public void buildTable(Form form) {
         for (Question question : form.questions) {
-            if(question.isComputed()) {
+            if (question.isComputed()) {
                 table.put(question.identifier, question.computedAnswer);
             } else {
                 table.put(question.identifier, new ExpressionVariableUndefined(question.getToken(), question.type));
@@ -42,23 +36,14 @@ public class SymbolTable {
         }
     }
 
-    public void addListener(ChangeListener listener){
-        this.listeners.add(listener);
-    }
-
     public void setExpression(String identifier, Expression value) {
         this.table.put(identifier, value);
 
-        // TODO remove all below
-        // Notify listener
-        for(ChangeListener listener : listeners){
-            listener.stateChanged(new ChangeEvent(new Pair(identifier, value)));
-        }
-
+        // TODO remove debugging info below
         ExpressionEvaluator expressionEvaluator = new ExpressionEvaluator(this);
-        for(Map.Entry<String, Expression> entry : table.entrySet()){
+        for (Map.Entry<String, Expression> entry : table.entrySet()) {
             Value evaluatedValue = expressionEvaluator.visit(entry.getValue());
-//            System.out.println(entry.getKey() + " " + evaluatedValue.toString());
+            System.out.println(entry.getKey() + " " + evaluatedValue.toString());
         }
     }
 }

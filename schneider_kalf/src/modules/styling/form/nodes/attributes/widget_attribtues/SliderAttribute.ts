914a485e2cbe9ee86a3e78f216242e2d4e985eab
Meess <abcmaster8@hotmail.com>
2018-03-22 18:47:17 +0100

    ADD slider widget

--- schneider_kalf/src/modules/styling/form/nodes/attributes/widget_attribtues/SliderAttribute.ts
+++ schneider_kalf/src/modules/styling/form/nodes/attributes/widget_attribtues/SliderAttribute.ts
@@ -1,16 +1,25 @@
 import WidgetAttribute from "../WidgetAttribute";
 import { SliderWidgetComponent } from "../../../../rendering/components/widgets/SliderWidgetComponent";
+import NumberValue from "../../../../../../form/values/NumberValue";
 
 export default class SliderAttribute extends WidgetAttribute {
-  getRenderComponent() {
-    return SliderWidgetComponent;
-  }
-
   getValue(): string {
     return "slider";
   }
 
   getStringValue(): string {
-    return "NOT IMPLEMENTED YET";
+    return "";
+  }
+
+  getRenderComponent() {
+    return SliderWidgetComponent;
+  }
+
+  getMinimum(): number {
+    return (this.options && this.options.length === 2) ? parseInt(this.options[0], 10) : 0;
+  }
+
+  getMaximum(): number {
+    return (this.options && this.options.length === 2) ? parseInt(this.options[1], 10) : 9001;
   }
 }

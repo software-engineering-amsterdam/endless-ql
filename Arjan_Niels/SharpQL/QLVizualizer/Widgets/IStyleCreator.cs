8361fb3271fb25fe4b588d71cd4365662f0e5c03
GrimGerbil <meijerarjan@live.nl>
2018-03-22 09:23:00 +0100

    Auto stash before merge of "Arjan_Niels" and "origin/Arjan_Niels"

---
+++ Arjan_Niels/SharpQL/QLVizualizer/Widgets/IStyleCreator.cs
@@ -0,0 +1,15 @@
+﻿using QLParser.AST.Nodes;
+using QLParser.AST.QLS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLVisualizer.Widgets
+{
+    public interface IStyleCreator
+    {
+        //Dictionary<string, QValueType>
+    }
+}

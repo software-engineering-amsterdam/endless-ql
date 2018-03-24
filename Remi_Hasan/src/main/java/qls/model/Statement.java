b6aa1e5044d823edff995c014114f1c7f073ec02
Remi van Veen <remivveen@gmail.com>
2018-03-24 21:41:59 +0100

    made QLS statements class abstract

--- Remi_Hasan/src/main/java/qls/model/Statement.java
+++ Remi_Hasan/src/main/java/qls/model/Statement.java
@@ -1,15 +1,10 @@
 package qls.model;
 
 import org.antlr.v4.runtime.Token;
-import qls.IQLSVisitor;
 
-public class Statement extends QLSNode {
+public abstract class Statement extends QLSNode {
+
     protected Statement(Token start) {
         super(start);
-    }
-
-    @Override
-    public <T> T accept(IQLSVisitor<T> visitor) {
-        return visitor.visit(this);
     }
 }

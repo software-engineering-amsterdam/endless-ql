aa5cddade1a3a397eb9aac20b9147a59f8525a6a
Metchu <ahmednassir@hotmail.com>
2018-03-25 12:09:34 +0200

    add FormTest empty form

---
+++ Ioana_Hisham/src/test/java/ql/ast/FormTest.java
@@ -0,0 +1,32 @@
+package ql.ast;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.junit.Test;
+import ql.ASTBuilder;
+import ql.antlr.QLLexer;
+import ql.antlr.QLParser;
+import ql.antlr.QLVisitor;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class FormTest {
+    private final QLVisitor<Node> visitor = new ASTBuilder();
+    @Test
+    public void emptyForm() throws IOException {
+        String input = "form f1 {}";
+        String expected = "(form form (id f1) { })";
+
+        QLParser parser = initQLParser(input);
+        ParseTree tree = parser.form();
+
+        assertEquals(expected, tree.toStringTree(parser));
+    }
+
+    private QLParser initQLParser(String code) {
+        return new QLParser(new CommonTokenStream(new QLLexer(new ANTLRInputStream(code))));
+    }
+}

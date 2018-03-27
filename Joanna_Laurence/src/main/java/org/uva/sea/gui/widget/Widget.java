1f3413f3e5016fc7b2ceeedb80b1a12055c71491
Joanna Roczniak <roczniak.joanna@gmail.com>
2018-03-27 12:26:44 +0200

    set visibility package, so that widget can be only in widget package

--- Joanna_Laurence/src/main/java/org/uva/sea/gui/widget/Widget.java
+++ Joanna_Laurence/src/main/java/org/uva/sea/gui/widget/Widget.java
@@ -2,7 +2,7 @@ package org.uva.sea.gui.widget;
 
 import org.uva.sea.languages.ql.interpreter.dataObject.questionData.QuestionData;
 
-public abstract class Widget extends BaseWidget {
+abstract class Widget extends BaseWidget {
     Widget(QuestionData questionData) {
         super(questionData);
     }

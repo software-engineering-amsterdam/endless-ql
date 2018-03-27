65d539c048b9de3ad97ada86747ff9ec4c6ba012
Metchu <ahmednassir@hotmail.com>
2018-03-27 06:10:20 +0200

    add integer literal

---
+++ Ioana_Hisham/src/main/java/ql/ast/expressions/literals/IntegerLiteral.java
@@ -0,0 +1,21 @@
+package ql.ast.expressions.literals;
+
+import ql.ast.expressions.Expression;
+import ql.visitors.ExpressionVisitor;
+
+public class IntegerLiteral extends Expression {
+    private final int value;
+    public IntegerLiteral(int lineNumber, int value) {
+        super(lineNumber);
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public <T> T accept(ExpressionVisitor<T> expressionVisitor) {
+        return null;
+    }
+}

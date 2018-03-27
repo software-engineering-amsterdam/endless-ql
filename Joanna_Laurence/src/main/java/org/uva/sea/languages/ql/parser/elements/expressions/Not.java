4834352a0f89bd12eb9115080515fda9e8ca25cb
Joanna Roczniak <roczniak.joanna@gmail.com>
2018-03-27 10:53:42 +0200

    Single node should be an operator.

--- Joanna_Laurence/src/main/java/org/uva/sea/languages/ql/parser/elements/expressions/Not.java
+++ Joanna_Laurence/src/main/java/org/uva/sea/languages/ql/parser/elements/expressions/Not.java
@@ -3,10 +3,10 @@ package org.uva.sea.languages.ql.parser.elements.expressions;
 import org.antlr.v4.runtime.Token;
 import org.uva.sea.languages.ql.parser.NodeType;
 import org.uva.sea.languages.ql.parser.elements.expressions.types.Type;
-import org.uva.sea.languages.ql.parser.nodeTypes.SingleNode;
+import org.uva.sea.languages.ql.parser.nodeTypes.SingleOperator;
 import org.uva.sea.languages.ql.parser.visitor.IASTVisitor;
 
-public class Not extends SingleNode {
+public class Not extends SingleOperator {
     public Not(Token token, Expression value) {
         super(token, value);
     }
--- Joanna_Laurence/src/main/java/org/uva/sea/languages/ql/parser/nodeTypes/SingleOperator.java
+++ Joanna_Laurence/src/main/java/org/uva/sea/languages/ql/parser/nodeTypes/SingleOperator.java
@@ -4,10 +4,10 @@ import org.antlr.v4.runtime.Token;
 import org.uva.sea.languages.ql.parser.elements.expressions.Expression;
 import org.uva.sea.languages.ql.parser.visitor.IASTVisitor;
 
-public abstract class SingleNode extends Expression {
+public abstract class SingleOperator extends Expression {
     private final Expression value;
 
-    protected SingleNode(Token token, Expression value) {
+    protected SingleOperator(Token token, Expression value) {
         super(token);
         this.value = value;
     }

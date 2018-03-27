6e83a461394636babd60ff5f2f90010a6bbced66
AHerczeg <h.herczeg.andras@gmail.com>
2018-03-27 19:37:51 +0200

    Form can collect results of all visible questions and return

--- Andras_Thijs/src/GUI/QLGUI.java
+++ Andras_Thijs/src/GUI/QLGUI.java
@@ -1,101 +1,69 @@
 package GUI;
 
-import GUI.Controller.RefreshListener;
+import GUI.Controller.FormController;
 import GUI.View.QuestionPanel;
 import Nodes.QLForm;
-import Nodes.Question;
 import QLExceptions.SyntaxException;
 import QLExceptions.TypeException;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.*;
-import java.util.List;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
-public class QLGUI implements RefreshListener{
+public class QLGUI {
 
     private QLForm form;
-    private List<QuestionPanel> questionPanels;
 
     public QLGUI(QLForm form){
         this.form = form;
-        questionPanels = new ArrayList<>();
     }
 
     public void renderForm() throws SyntaxException, TypeException {
 
         JFrame frame = new JFrame(form.getName());
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        FormController controller = new FormController(form, frame);
 
         JPanel panel = new JPanel();
-
         panel.setLayout(new GridLayout(0, 1));
 
-        //panel.setPreferredSize(new Dimension(400, 600));
-
-        List<Question> questions = form.getAllQuestions();
-        for (Question question : questions) {
-            QuestionPanel questionPanel = new QuestionPanel(question, this);
-            questionPanels.add(questionPanel);
+        for(QuestionPanel questionPanel : controller.getQuestionPanels()){
             panel.add(questionPanel);
         }
 
         JButton button = new JButton("Submit");
-
-        button.addActionListener(e -> {
+        button.addActionListener((ActionEvent e) -> {
             try {
-                System.out.println(getResults());
+                controller.getResults();
             } catch (SyntaxException e1) {
                 e1.printStackTrace();
             } catch (TypeException e1) {
+                e1.printStackTrace();
+            } catch (IOException e1) {
                 e1.printStackTrace();
             }
         });
 
         JPanel buttonPanel = new JPanel();
-
         buttonPanel.add(button);
-
         panel.add(buttonPanel);
 
-
         panel.setMaximumSize(new Dimension(400, 600));
-
         JScrollPane scrPane = new JScrollPane(panel);
-
         scrPane.setMaximumSize(new Dimension(400, 600));
 
-
         frame.getContentPane().add(scrPane);
-
         frame.setResizable(false);
-
         frame.pack();
 
-        refreshQuestions();
+
+
+        controller.refreshQuestions();
 
         frame.setVisible(true);
 
-    }
-
-
-    @Override
-    public void refreshQuestions() throws SyntaxException, TypeException {
-        for (QuestionPanel questionPanel : questionPanels) {
-            questionPanel.refreshPanel();
-        }
-    }
-
-    public String getResults() throws SyntaxException, TypeException {
-        String results = "{";
-        for (QuestionPanel questionPanel : questionPanels) {
-            results += questionPanel.getResult() + ",";
-        }
-        if(results.length() > 1)
-            results = results.substring(0, results.length() - 1);
-        results += "}";
-        return results;
     }
 }
--- Andras_Thijs/src/Main/Main.java
+++ Andras_Thijs/src/Main/Main.java
@@ -1,22 +1,25 @@
 package Main;
 
 import AST.FormReader;
-import GUI.FormTemplate;
+import GUI.QLGUI;
 import Nodes.Condition;
 import Nodes.QLForm;
 import QLExceptions.*;
 import org.antlr.v4.runtime.CharStreams;
 
 import java.io.IOException;
+import java.nio.file.AccessDeniedException;
 import java.util.List;
 
 class Main {
     public static void main (String[] args){
+
         FormReader formReader = new FormReader();
 
         try {
+
             // Read the form from a file and set the parent structure.
-            QLForm form = formReader.parseCharStream(CharStreams.fromFileName("resources/test_grammar.txt"));
+            QLForm form = formReader.parseCharStream(CharStreams.fromFileName(new PathChooser().getFilePath()));
             form.setParents();
 
             // Check for duplicate names and labels.
@@ -34,7 +37,7 @@ class Main {
             for(String label : checker.getDuplicateLabels())
                 System.out.println("WARNING: DUPLICATE LABEL " + label);
 
-            FormTemplate formGUI = new FormTemplate(form);
+            QLGUI formGUI = new QLGUI(form);
             formGUI.renderForm();
 
         } catch (IOException e) {

ce7a6e761b7aefd6cb5fd853335d5fe745c6ef73
Nico Tromp <nico.tromp@gmail.com>
2018-03-28 16:33:22 +0200

    Apply standard formatting.

--- niro/src/main/scala/nl/uva/se/sc/niro/model/qls/Styling.scala
+++ niro/src/main/scala/nl/uva/se/sc/niro/model/qls/Styling.scala
@@ -3,11 +3,11 @@ package nl.uva.se.sc.niro.model.qls
 import nl.uva.se.sc.niro.model.qls.style._
 
 case class Styling(
-                    widgetType: Option[WidgetType] = None,
-                    width: Option[Width] = None,
-                    color: Option[Color] = None,
-                    fontType: Option[FontType] = None,
-                    fontSize: Option[FontSize] = None) {
+    widgetType: Option[WidgetType] = None,
+    width: Option[Width] = None,
+    color: Option[Color] = None,
+    fontType: Option[FontType] = None,
+    fontSize: Option[FontSize] = None) {
 
   def ++(that: Styling): Styling = {
     Styling(
--- niro/src/main/scala/nl/uva/se/sc/niro/model/qls/style/WidgetType.scala
+++ niro/src/main/scala/nl/uva/se/sc/niro/model/qls/style/WidgetType.scala
@@ -5,4 +5,3 @@ import nl.uva.se.sc.niro.model.ql.AnswerType
 abstract class WidgetType extends Style {
   def isCompatibleWith(answerType: AnswerType): Boolean
 }
-

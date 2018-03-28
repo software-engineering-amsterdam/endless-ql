37ee31985385a619103235fc611c1049cd30b2c4
jewelEarthDeveloper <paul_spencer85@hotmail.com>
2018-03-28 20:01:32 +0200

    added some widgets to grammar

--- YouDontMakeFriendsWithDotNet/NoRightAnswersToWrongQuestions/QLS.UnitTests/ParserTests/TestQlsData.cs
+++ YouDontMakeFriendsWithDotNet/NoRightAnswersToWrongQuestions/QLS.UnitTests/ParserTests/TestQlsData.cs
@@ -37,14 +37,37 @@ namespace QLS.UnitTests.ParserTests
             }
         }
 
-
         public static IEnumerable StyleSheetWithMultiplePages
         {
             get
             {
                 yield return new TestCaseData(
+                    @"stylesheet ss1 { page p1 { }}",
+                    1);
+                yield return new TestCaseData(
                     @"stylesheet ss1 { page p1 { } page p2 { }}",
                     2);
+                yield return new TestCaseData(
+                    @"stylesheet ss1 { page p1 { } page p2 { } page p3 { }}",
+                    3);
+            }
+        }
+
+        public static IEnumerable StyleSheetWithDefaultStyle
+        {
+            get
+            {
+                yield return new TestCaseData(
+                    @"stylesheet ss1 { page p1 { } default boolean widget checkbox}");
+                yield return new TestCaseData(
+                    @"stylesheet ss1 { page p1 { } default boolean widget radio}");
+                yield return new TestCaseData(
+                    @"stylesheet ss1 { page p1 { } default boolean widget radio(""Ay"", ""Nee"") }");
+                yield return new TestCaseData(
+                    @"stylesheet ss1 { page p1 { } default boolean widget dropdown}");
+                yield return new TestCaseData(
+                    @"stylesheet ss1 { page p1 { } default boolean widget dropdown(""darntootin"", ""nonononono"") }");
+
             }
         }
     }

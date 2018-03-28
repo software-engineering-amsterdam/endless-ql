2813cbaea1293e98502e32841ec026a517c8bf6a
AHerczeg <h.herczeg.andras@gmail.com>
2018-03-28 13:48:39 +0200

    Added comments

--- Andras_Thijs/src/GUI/Controller/FormController.java
+++ Andras_Thijs/src/GUI/Controller/FormController.java
@@ -15,10 +15,20 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ *  FormController is responsible for updating questions and getting the results when a form is submitted
+ */
 public class FormController implements RefreshListener {
     private static JFrame frame;
     private final List<QuestionPanel> questionPanels = new ArrayList<>();
 
+    /**
+     * Creates a FormController instance
+     * @param form The Model containing the parsed questions
+     * @param frame The frame that displays the form
+     * @throws SyntaxException
+     * @throws TypeException
+     */
     public FormController(QLForm form, JFrame frame) throws SyntaxException, TypeException {
         FormController.frame = frame;
 
@@ -29,15 +39,29 @@ public class FormController implements RefreshListener {
         }
     }
 
+    /**
+     * Returns the list of QuestionPanels converted from the QLForm's Questions
+     * @return The list of QuestionPanels
+     */
     public  List<QuestionPanel> getQuestionPanels() {
         return questionPanels;
     }
 
+    /**
+     * Refreshes the visibility and values of the QuestionPanels
+     * @throws SyntaxException
+     * @throws TypeException
+     */
     public void refreshQuestions() throws SyntaxException, TypeException {
         for (QuestionPanel questionPanel : questionPanels)
             questionPanel.refreshPanel();
     }
 
+    /**
+     * Extracts the results from the form (in JSON format) and closes the window displaying it. The user needs to specify a destination for the results or the methods finishes without changing anything. Unset values are returned as "undefined".
+     * @throws SyntaxException
+     * @throws TypeException
+     */
     public void getResults() throws SyntaxException, TypeException {
 
         StringBuilder results = new StringBuilder("{");

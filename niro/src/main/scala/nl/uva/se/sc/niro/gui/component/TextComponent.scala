73b9cd11624f043ab87dda98d8f48f05771ed068
rmathijn <rocco.mathijnandela@dhl.com>
2018-03-29 16:14:39 +0200

    Convert anonymous function

--- niro/src/main/scala/nl/uva/se/sc/niro/gui/component/TextComponent.scala
+++ niro/src/main/scala/nl/uva/se/sc/niro/gui/component/TextComponent.scala
@@ -8,7 +8,7 @@ import scala.collection.mutable
 
 case class TextComponent(id: String, label: Label, control: QLWidget[String])
     extends Component[String](id, label, control) {
-  override def getValue: Option[StringAnswer] = Option(control.value).map(StringAnswer(_))
+  override def getValue: Option[StringAnswer] = Option(control.value).map(StringAnswer)
   override def setValue(value: Option[String]): Unit = control.value(fromOption(value))
   override def updateValue(dictionary: mutable.Map[String, Answer]): Unit =
     setValue(dictionary.get(id).flatMap(answer => Option(answer).map(_.value.toString).filter(_.nonEmpty)))

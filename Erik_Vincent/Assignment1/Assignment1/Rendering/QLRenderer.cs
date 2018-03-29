1a8f941f4e0d28608a6079ee1991bc4f13397104
evanscharrenburg <evanscharrenburg@users.noreply.github.com>
2018-03-29 12:54:56 +0200

    Add new QL renderer.

---
+++ Erik_Vincent/Assignment1/Assignment1/Rendering/QLRenderer.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows.Forms;
+using Assignment1.Execution;
+using Assignment1.Model.QL.AST;
+using Assignment1.Model.QL.AST.Value;
+using Assignment1.Rendering.Widget;
+
+namespace Assignment1.Rendering
+{
+    public class QLRenderer : IQuestionFormRenderer
+    {
+        private readonly QLExecutor _executor;
+        private readonly Panel _controlContainer = new FlowLayoutPanel
+        {
+            AutoSize = true,
+            AutoSizeMode = AutoSizeMode.GrowAndShrink,
+            FlowDirection = FlowDirection.TopDown
+        };
+
+        public QLRenderer(QuestionForm questionForm)
+        {
+            _executor = new QLExecutor(questionForm);
+        }
+
+        public Control Render()
+        {
+            foreach (var question in _executor.VisibleQuestions)
+            {
+                RenderQuestion(question);
+            }
+            return _controlContainer;
+        }
+
+        private void AddControl(Control control) => _controlContainer.Controls.Add(control);
+
+        private void ClearControls() => _controlContainer.Controls.Clear();
+
+        private void SetAnswer(string questionId, IValue value)
+        {
+            _executor.SetAnswer(questionId, value);
+            ClearControls();
+            Render();
+        }
+
+        private void RenderQuestion(Question question)
+        {
+            AddControl(QLWidgetFactory.GetWidget(
+                    question.Label,
+                    _executor.GetAnswer(question.Id),
+                    _executor.IsReadOnly(question.Id),
+                    value => SetAnswer(question.Id, value)
+                ).Render());
+        }
+    }
+}
---
+++ Erik_Vincent/Assignment1/Assignment1/Rendering/Widget/CheckBox.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+using Assignment1.Model.QL.AST.Value;
+
+namespace Assignment1.Rendering.Widget
+{
+    public class CheckBox : IWidget
+    {
+        private readonly System.Windows.Forms.CheckBox _checkBox;
+        private readonly Action<IValue> _valueChangedHandler;
+
+        public CheckBox(string label, bool value, bool readOnly, Action<IValue> valueChangedHandler)
+        {
+            _valueChangedHandler = valueChangedHandler;
+            _checkBox = new System.Windows.Forms.CheckBox
+            {
+                AutoSize = true,
+                Text = label,
+                Checked = value,
+                Enabled = !readOnly
+            };
+            _checkBox.CheckedChanged += _checkBox_CheckedChanged;
+        }
+
+        private void _checkBox_CheckedChanged(object sender, EventArgs e)
+        {
+            _valueChangedHandler(new QLBoolean(_checkBox.Checked));
+        }
+
+        public Control Render()
+        {
+            return _checkBox;
+        }
+    }
+}

f67bb55609a71747ef98182974ce40663139257f
Nico Tromp <nico.tromp@gmail.com>
2018-03-29 07:31:23 +0200

    Split model bridge into two separate brdiges.

--- niro/src/main/scala/nl/uva/se/sc/niro/gui/converter/QLSToGUIModelBridge.scala
+++ niro/src/main/scala/nl/uva/se/sc/niro/gui/converter/QLSToGUIModelBridge.scala
@@ -16,21 +16,25 @@ import nl.uva.se.sc.niro.util.StringUtil
   * visibility property of GUI question will consist of all intermediate expressions logical 'and'ed to ensure the
   * desired behaviour.
   */
-object QLToGUIModelBridge {
-  def convertForm(form: QLForm): GUIForm = {
-    ql.GUIForm(StringUtil.addSpaceOnCaseChange(form.formName), convertStatements(BooleanAnswer(true), form.statements))
+object QLSToGUIModelBridge {
+  def convertStylesheet(stylesheet: QLStylesheet): GUIStylesheet = {
+    val defaultStyles = stylesheet.defaultStyles.mapValues(GUIStyling(_))
+    qls.GUIStylesheet(
+      StringUtil.addSpaceOnCaseChange(stylesheet.name),
+      stylesheet.pages.map(convertPage),
+      defaultStyles)
   }
 
-  def convertStatements(visible: Expression, statements: Seq[Statement]): Seq[GUIQuestion] = {
-    statements.flatMap(statement =>
-      statement match {
-        case question: nl.uva.se.sc.niro.model.ql.Question =>
-          Seq(GUIQuestionFactory.makeGUIQuestion(visible, question))
-        case conditional: Conditional =>
-          GUIConditionalFactory.makeGUIConditional(visible, conditional)
-        case _ =>
-          Seq.empty
-    })
+  def convertPage(page: Page): GUIPage = {
+    val defaultStyles = page.defaultStyles.mapValues(GUIStyling(_))
+    qls.GUIPage(StringUtil.addSpaceOnCaseChange(page.name), page.sections.map(convertSection), defaultStyles)
   }
 
+  def convertSection(section: Section): GUISection = {
+    val defaultStyles = section.defaultStyles.mapValues(GUIStyling(_))
+    qls.GUISection(section.name, section.questions.map(convertQuestion), defaultStyles)
+  }
+
+  def convertQuestion(question: Question): GUIQuestionStyling =
+    GUIQuestionStyling(question.name, GUIStyling(question.widgetType))
 }

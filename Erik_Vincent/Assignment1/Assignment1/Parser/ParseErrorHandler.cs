1fdf6b3c5e8dbcca391cb9241dbb0351a37ebb72
V-Jong <vince-ny@hotmail.com>
2018-03-30 22:23:13 +0200

    Modified tests

--- Erik_Vincent/Assignment1/Assignment1/Parser/ParseErrorHandler.cs
+++ Erik_Vincent/Assignment1/Assignment1/Parser/ParseErrorHandler.cs
@@ -10,6 +10,8 @@ namespace Assignment1.Parser
     {
         private readonly List<string> _errors = new List<string>();
 
+        public IEnumerable<string> Errors => _errors;
+
         public bool HasErrors => _errors.Count > 0;
 
         public void AddError(int lineNumber, string message)
--- Erik_Vincent/Assignment1/Assignment1Tests/QLFormsTests.cs
+++ Erik_Vincent/Assignment1/Assignment1Tests/QLFormsTests.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Assignment1;
+using Assignment1.Converters;
 using Assignment1.Model.QL.RenderTree;
 using Assignment1.Model.QL.RenderTree.QLExpression;
+using Assignment1.Parser;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Assignment1Tests
@@ -11,33 +16,63 @@ namespace Assignment1Tests
     [TestClass]
     public class QLFormsTests
     {
+        private static string _pathToNonParsableForms = "../../NonParsable/";
         private static string _pathToInvalidForms = "../../InvalidForms/";
         private static string _pathToValidForms = "../../ValidForms/";
+        private static Form1 _form = new Form1();
+
+        [TestMethod]
+        public void TestNonParsableorms()
+        {
+            string[] nonParsableForms = Directory.GetFiles(_pathToNonParsableForms);
+
+            foreach (string nonParsableForm in nonParsableForms)
+            {
+                // Call parser and assert that exception is thrown
+                Exception expectedException = null;
+                string nonParsableFormContent = File.ReadAllText(nonParsableForm);
+                try
+                {
+                    TextToQLAST.ParseString(nonParsableFormContent);
+                }
+                catch (QLParseException exception)
+                {
+                    expectedException = exception;
+                }
+                Assert.IsNotNull(expectedException);
+            }
+        }
 
         [TestMethod]
         public void TestInvalidForms()
         {
+            var presenter = new MainPresenter(_form);
             string[] invalidForms = Directory.GetFiles(_pathToInvalidForms);
             
             foreach (string invalidForm in invalidForms)
             {
                 // Call checker and assert errors
+                string invalidFormContent = File.ReadAllText(invalidForm);
+                var astForm = TextToQLAST.ParseString(invalidFormContent);
+                var messages = presenter.ValidateForm(astForm);
+                Assert.IsTrue(messages.Errors.Any());
             }
-
-            Assert.AreEqual(7, invalidForms.Length);
         }
 
         [TestMethod]
         public void TestValidForms()
         {
+            var presenter = new MainPresenter(_form);
             string[] validForms = Directory.GetFiles(_pathToValidForms);
 
             foreach (string validForm in validForms)
             {
                 // Call checker and assert no errors
+                string validFormContent = File.ReadAllText(validForm);
+                var astForm = TextToQLAST.ParseString(validFormContent);
+                var messages = presenter.ValidateForm(astForm);
+                Assert.IsTrue(!messages.Errors.Any());
             }
-
-            Assert.AreEqual(1, validForms.Length);
         }
 
         [TestMethod]

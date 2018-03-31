93a6eef757f46c72d56de69f0d1e8d1f8a868d4f
jewelEarthDeveloper <paul_spencer85@hotmail.com>
2018-03-31 12:32:21 +0200

    make all controls different fro QL

--- YouDontMakeFriendsWithDotNet/NoRightAnswersToWrongQuestions/SimpleWPFApp/ObjectToIntegerConverter.cs
+++ YouDontMakeFriendsWithDotNet/NoRightAnswersToWrongQuestions/SimpleWPFApp/ObjectToIntegerConverter.cs
@@ -16,4 +16,16 @@ namespace SimpleWPFApp
             return value;
         }
     }
+    internal class ObjectToDecimalConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return decimal.Parse(value.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
 }

5ed7c3999dc7a80dae14827c3316711290aaa20d
GrimGerbil <meijerarjan@live.nl>
2018-03-31 12:17:40 +0200

    Implemented HexQuestionManager

--- Arjan_Niels/SharpQL/QLVizualizer/Elements/Managers/LeafTypes/HexQuestionManager.cs
+++ Arjan_Niels/SharpQL/QLVizualizer/Elements/Managers/LeafTypes/HexQuestionManager.cs
@@ -1,13 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using QLVisualizer.Controllers;
+using QLVisualizer.Expression.Types;
 
 namespace QLVisualizer.Elements.Managers.LeafTypes
 {
-    // TODO: IMPLEMENT
-    class HexQuestionManager
+    public class HexQuestionManager : QuestionElementManager<Hexadecimal>
     {
+        public HexQuestionManager(string identifyer, string text, ElementManagerCollection parent, ElementManagerController controller, ExpressionBool activationExpression = null, TypedExpressionValue<Hexadecimal> answerExpression = null) 
+            : base(identifyer, text, parent, controller, activationExpression, answerExpression)
+        {
+        }
+
+        protected override QuestionElementValue<Hexadecimal> ParseInput(string input)
+        {
+            Hexadecimal value = new Hexadecimal(0);
+            bool isValid = true;
+            try
+            {
+                value = new Hexadecimal(input);
+            }
+            catch
+            {
+                // TODO: WHAT TO DO? throw error or return value with 'valid = false'
+                isValid = false;
+            }
+            return new QuestionElementValue<Hexadecimal>(value, isValid);
+        }
     }
 }

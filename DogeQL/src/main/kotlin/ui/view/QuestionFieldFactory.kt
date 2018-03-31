b1c2d073b7bc76b6f425fecedd3387f06e0ef0e8
Sangam <sangamgupta10@gmail.com>
2018-03-31 15:12:27 +0200

    Add date field, Move listener to parent

--- DogeQL/src/main/kotlin/ui/view/QuestionFieldFactory.kt
+++ DogeQL/src/main/kotlin/ui/view/QuestionFieldFactory.kt
@@ -12,6 +12,7 @@ class QuestionFieldFactory {
             is IntegerViewModel -> IntegerField(question)
             is DecimalViewModel -> DecimalField(question)
             is BooleanViewModel -> CheckBox(question)
+            is DateViewModel -> DateField(question)
             else -> throw IllegalArgumentException("${question.item.value.type} unsupported type")
         }
     }

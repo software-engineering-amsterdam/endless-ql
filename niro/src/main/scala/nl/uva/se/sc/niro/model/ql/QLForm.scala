c3bd624e14937920d169df7cc0d5620314a8cc61
rmathijn <rocco.mathijnandela@dhl.com>
2018-03-31 16:41:37 +0200

    Use symboltable type in qlform

--- niro/src/main/scala/nl/uva/se/sc/niro/model/ql/QLForm.scala
+++ niro/src/main/scala/nl/uva/se/sc/niro/model/ql/QLForm.scala
@@ -1,7 +1,8 @@
 package nl.uva.se.sc.niro.model.ql
 
 import nl.uva.se.sc.niro.errors.Warning
+import nl.uva.se.sc.niro.model.ql.SymbolTable.SymbolTable
 
 case class QLForm(formName: String, statements: Seq[Statement], warnings: Seq[Warning] = Seq.empty) {
-  val symbolTable: Map[String, Symbol] = SymbolTable.createSymbolTable(this)
+  val symbolTable: SymbolTable = SymbolTable.createSymbolTable(this)
 }

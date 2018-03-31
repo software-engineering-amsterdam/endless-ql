c23b21414da019f52009695663857ba84c2b5fe2
bicker <bickerkards@tuta.io>
2018-03-31 12:19:46 +0200

    Add expression evaluation tester

--- PyQuest/src/test/test_pyquest.py
+++ PyQuest/src/test/test_pyquest.py
@@ -4,6 +4,7 @@ from ql.parser.parser import QLParser
 from src.test.lexing.test_lexing import TestLexing
 from src.test.parsing.test_parsing import TestParsing
 from src.test.semantic_analysis.test_semantic_analysis import TestSemanticAnalysis
+from src.test.expression_evaluation.test_expression_evaluation import TestExpressionEvaluation
 
 
 if __name__ == '__main__':
@@ -13,8 +14,9 @@ if __name__ == '__main__':
     test_lexer = TestLexing('lexing/', ql_lexer)
     test_parser = TestParsing('parsing/', ql_lexer, ql_parser)
     test_semantic_analyzer = TestSemanticAnalysis('semantic_analysis/', ql_lexer, ql_parser)
+    test_evaluation = TestExpressionEvaluation('expression_evaluation/', ql_lexer, ql_parser)
 
     test_lexer.test()
     test_parser.test()
     test_semantic_analyzer.test()
-
+    test_evaluation.test()

76843aebece65019d446251163dee7c9532d1c58
evanscharrenburg <evanscharrenburg@users.noreply.github.com>
2018-04-01 11:58:46 +0200

    Remove QLBaseListener dependency, disable ANTLR listener generation.

--- Erik_Vincent/Assignment1/Assignment1/Parser/QLParser.cs
+++ Erik_Vincent/Assignment1/Assignment1/Parser/QLParser.cs
@@ -1,23 +1,14 @@
 ﻿using Antlr4.Runtime;
-using Antlr4.Runtime.Tree;
 using Assignment1.Model.QL.AST;
 
 namespace Assignment1.Parser
 {
-    public class QLParser : QLBaseListener
+    public static class QLParser
     {
-        private QuestionForm _form;
-        private readonly ParseErrorHandler _errorHandler = new ParseErrorHandler();
-
-        public override void ExitForm(QL.FormContext context)
-        {
-            _form = context.result;
-        }
-
         public static QuestionForm ParseString(string input)
         {
-            var listener = new QLParser();
-            var errorListener = new QLErrorListener(listener._errorHandler);
+            var errorHandler = new ParseErrorHandler();
+            var errorListener = new QLErrorListener(errorHandler);
 
             var stream = CharStreams.fromstring(input);
             var lexer = new QLLexer(stream);
@@ -27,14 +18,11 @@ namespace Assignment1.Parser
             var parser = new QL(tokens);
             parser.RemoveErrorListeners();
             parser.AddErrorListener(errorListener);
-            var context = parser.form();
+            var form = parser.form().result;
 
-            var walker = new ParseTreeWalker();
-            walker.Walk(listener, context);
-
-            if (listener._errorHandler.HasErrors)
-                listener._errorHandler.ThrowParseException();
-            return listener._form;
+            if (errorHandler.HasErrors)
+                errorHandler.ThrowParseException();
+            return form;
         }
     }
 }

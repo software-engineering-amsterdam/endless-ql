45221e18212a6d5648b8b78e8ff98a0f2727633a
jewelEarthDeveloper <paul_spencer85@hotmail.com>
2018-04-01 17:55:33 +0200

    panicing to get QLS functionality in rather than refactoring - damnit!

--- YouDontMakeFriendsWithDotNet/NoRightAnswersToWrongQuestions/StyledWpfApp/ViewModels/IStyledQuestionnaireViewModel.cs
+++ YouDontMakeFriendsWithDotNet/NoRightAnswersToWrongQuestions/StyledWpfApp/ViewModels/IStyledQuestionnaireViewModel.cs
@@ -5,6 +5,8 @@ namespace StyledWpfApp.ViewModels
     public interface IStyledQuestionnaireViewModel
     {
         void Load();
+        int SelectedPageNumber { get; set; }
         ObservableCollection<IPageViewModel> Pages { get; }
+        string Name { get; }
     }
 }

e1b1abe5245b62729e715689a1a26119626adef7
Metchu <ahmednassir@hotmail.com>
2018-04-02 13:24:03 +0200

    add messages

--- Ioana_Hisham/src/main/java/ql/gui/Form.java
+++ Ioana_Hisham/src/main/java/ql/gui/Form.java
@@ -11,6 +11,10 @@ import ql.ast.statements.Question;
 import ql.evaluator.Evaluator;
 import ql.gui.controls.ControlVisitor;
 import ql.gui.controls.QLControl;
+import ql.typechecker.SemanticAnalyzer;
+import ql.typechecker.messages.Messages;
+import ql.typechecker.messages.error.Error;
+import ql.typechecker.messages.warning.Warning;
 import ql.values.Value;
 
 import java.io.IOException;
@@ -30,6 +34,7 @@ public class Form extends Application implements OnValueChange{
 
     @Override
     public void start(Stage stage) {
+        checkForMessages();
         formPane(stage);
         initializeForm();
         stage.show();
@@ -39,9 +44,26 @@ public class Form extends Application implements OnValueChange{
         ASTBuilder astBuilder = new ASTBuilder();
         InputStream input = Form.class.getResourceAsStream("/form.ql");
         form = astBuilder.build(input);
-        //todo add typechecker
-        //todo print errors/warnings
-        //visitor.visit(parseTree);
+    }
+
+    private void checkForMessages() {
+        SemanticAnalyzer semanticAnalyzer = new SemanticAnalyzer();
+        Messages messages = semanticAnalyzer.checkForm(form);
+
+        if (messages.hasErrors()) {
+            messages.errors().forEach(this::printError);
+            System.exit(1);
+        }
+
+        messages.warnings().forEach(this::printWarning);
+    }
+
+    private void printError(Error error) {
+        System.out.println(error.message());
+    }
+
+    private void printWarning(Warning warning) {
+        System.out.println(warning.message());
     }
 
     public void initializeForm() {
@@ -71,9 +93,7 @@ public class Form extends Application implements OnValueChange{
     }
 
     private void add(QLControl qlControl, Question question) {
-        //if (evaluator.isQuestionModified(question.getIdentifier())) {
-            qlControl.setValue(evaluator.valueTable().find(question.getIdentifier()));
-        //}
+        qlControl.setValue(evaluator.valueTable().find(question.getIdentifier()));
         formPane.add(qlControl.gridPane(), 0, row++);
     }
 
